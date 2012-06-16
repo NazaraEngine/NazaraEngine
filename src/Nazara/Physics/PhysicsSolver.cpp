3ea57f3f5d010ccca69a3eadaa0eb411f0c7da76
Remi Beges <remi.beges@gmail.com>
2012-06-16 13:33:31 +0200

    Forgot to commit src files and Newton headers in previous commit

---
+++ src/Nazara/Physics/PhysicsSolver.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2012 Jérôme Leclercq / Rémi Bèges
+// This file is part of the "Nazara Engine".
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include "PhysicsWorld.hpp"
+//#include <Nazara/Physics/PhysicsWorld.hpp>
+#include "PhysicsSolver.hpp"
+//#include <Nazara/Physics/PhysicsSolver.hpp>
+#include <Newton/Newton.h>
+
+#include <Nazara/Core/Debug.hpp>
+
+NzPhysicsSolver::NzPhysicsSolver(nzSolverMode mode, unsigned int numberOfPassesLinearMode)
+{
+    m_mode = mode;
+    m_numberOfPassesLinearMode = numberOfPassesLinearMode;
+}
+
+void NzPhysicsSolver::Configure(nzSolverMode mode, unsigned int numberOfPassesLinearMode)
+{
+    m_mode = mode;
+    m_numberOfPassesLinearMode = numberOfPassesLinearMode;
+}
+
+void NzPhysicsSolver::Set(NzPhysicsWorld* world)
+{
+    switch(m_mode)
+    {
+        case nzExact:
+            NewtonSetSolverModel(world, 0);
+        break;
+
+        case nzAdaptative:
+            NewtonSetSolverModel(world, 1);
+        break;
+
+        case nzLinear:
+            NewtonSetSolverModel(world, m_numberOfPassesLinearMode);
+
+    }
+}
+
+NzPhysicsSolver::~NzPhysicsSolver()
+{
+    //dtor
+}
