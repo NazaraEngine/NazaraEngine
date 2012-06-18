01a9f6d52fcd711319e81dddf14d19e9f8a61cb9
Remi Beges <remi.beges@gmail.com>
2012-06-18 10:24:06 +0200

    Sligthly changed interface & added functionnalities NzPhysicsWorld has now a public pointer to NewtonWorld NzCollisionShape added, can be constructed from a NzCube for now NzPhysicsEntity added Minor fix + typo

--- include/Nazara/Physics/Debug.hpp
+++ include/Nazara/Physics/Debug.hpp
@@ -2,7 +2,7 @@
 // This file is part of the "Nazara Engine".
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
-#include <Nazara/ModuleName/Config.hpp>
+#include <Nazara/Physics/Config.hpp>
 #if NAZARA_PHYSICS_MEMORYLEAKTRACKER || defined(NAZARA_DEBUG)
 	#include <Nazara/Core/Debug/MemoryLeakTracker.hpp>
 
