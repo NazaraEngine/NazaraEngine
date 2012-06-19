9996a7169a133955b7eba25dd2d3959dc9696c9e
Remi Beges <remi.beges@gmail.com>
2012-06-19 18:31:42 +0200

    Removed Physics module

--- include/Nazara/Physics/DebugOff.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2012 AUTHORS
-// This file is part of the "Nazara Engine".
-// For conditions of distribution and use, see copyright notice in Config.hpp
-
-#if NAZARA_PHYSICS_MEMORYLEAKTRACKER || defined(NAZARA_DEBUG)
-	#undef delete
-	#undef new
-#endif
