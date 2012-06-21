8e04e71c14bbbb12931e17cb38bea70e61f52147
Lynix <Lynix680@gmail.com>
2012-06-21 16:50:17 +0200

    Added class template

---
+++ NazaraModuleTemplate/include/Nazara/ModuleName/ClassName.hpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2012 AUTHORS
+// This file is part of the "Nazara Engine".
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_CLASSNAME_HPP
+#define NAZARA_CLASSNAME_HPP
+
+#include <Nazara/Prerequesites.hpp>
+
+class NAZARA_API NzClassName
+{
+	public:
+		NzClassName();
+
+		int GetAttribute() const;
+
+		void SetAttribute(int attribute);
+
+	private:
+		int m_attribute;
+};
+
+#endif // NAZARA_CLASSNAME_HPP
