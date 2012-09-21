da780f3334d375bff481051f97ce591eb8474417
Jérôme Leclercq <Lynix@digitalpulsesoftware.com>
2012-09-21 12:39:40 +0200

    Added InputStream::GetLine(size);
    Former-commit-id: c571015023a05b37aff41b7b7b067ce287f5f175

--- src/Nazara/Core/InputStream.cpp
+++ src/Nazara/Core/InputStream.cpp
@@ -3,6 +3,47 @@
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
 #include <Nazara/Core/InputStream.hpp>
+#include <Nazara/Core/String.hpp>
 #include <Nazara/Core/Debug.hpp>
 
 NzInputStream::~NzInputStream() = default;
+
+NzString NzInputStream::GetLine(unsigned int lineSize)
+{
+	NzString line;
+	if (lineSize == 0) // Taille maximale indéterminée
+	{
+		while (!EndOfStream())
+		{
+			char c;
+			if (Read(&c, sizeof(char)) == sizeof(char))
+			{
+				if (c == '\n')
+					break;
+
+				line += c;
+			}
+			else
+				break;
+		}
+	}
+	else
+	{
+		line.Reserve(lineSize);
+		for (unsigned int i = 0; i < lineSize; ++i)
+		{
+			char c;
+			if (Read(&c, sizeof(char)) == sizeof(char))
+			{
+				if (c == '\n')
+					break;
+
+				line += c;
+			}
+			else
+				break;
+		}
+	}
+
+	return line;
+}
