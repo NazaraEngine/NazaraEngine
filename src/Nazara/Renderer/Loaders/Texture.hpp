32fa2f0c74ee30049d2c0c351ea23d4f05680dfb
Lynix <lynix680@gmail.com>
2012-11-25 19:33:58 +0100

    Added Texture-to-Material loader
    Former-commit-id: cc5e7c0d6e8fd3e620f2db320c4dd502f37fb3e1

---
+++ src/Nazara/Renderer/Loaders/Texture.hpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Renderer module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_LOADERS_TEXTURE_HPP
+#define NAZARA_LOADERS_TEXTURE_HPP
+
+#include <Nazara/Prerequesites.hpp>
+
+void NzLoaders_Texture_Register();
+void NzLoaders_Texture_Unregister();
+
+#endif // NAZARA_LOADERS_TEXTURE_HPP
