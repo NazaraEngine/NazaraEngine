4f894cb46496a2f339120a59b0d3aa6cbf3ddbf1
Lynix <lynix680@gmail.com>
2012-11-29 16:51:01 +0100

    Made NzThread interface mimic std::thread one
    Hopefully fixed the threaded window bug

--- include/Nazara/Core/Thread.hpp
+++ include/Nazara/Core/Thread.hpp
@@ -2,8 +2,6 @@
 // This file is part of the "Nazara Engine - Core module"
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
-// Inspiré du code de la SFML par Laurent Gomila
-
 #pragma once
 
 #ifndef NAZARA_THREAD_HPP
@@ -12,53 +10,56 @@
 #include <Nazara/Prerequesites.hpp>
 #include <Nazara/Core/Functor.hpp>
 #include <Nazara/Core/NonCopyable.hpp>
+#include <ostream>
 
 class NzThreadImpl;
 
 class NAZARA_API NzThread : NzNonCopyable
 {
-	friend NzThreadImpl;
-
 	public:
 		class NAZARA_API Id
 		{
 			friend NzThread;
-			friend NzThreadImpl;
 
 			public:
 				Id() = default;
-				Id(Id&& rhs) = default;
-				~Id();
 
-				Id& operator=(Id&& rhs) = default;
-				bool operator==(const Id& rhs) const;
-				bool operator!=(const Id& rhs) const;
+				NAZARA_API friend bool operator==(const Id& lhs, const Id& rhs);
+				NAZARA_API friend bool operator!=(const Id& lhs, const Id& rhs);
+				NAZARA_API friend bool operator<(const Id& lhs, const Id& rhs);
+				NAZARA_API friend bool operator<=(const Id& lhs, const Id& rhs);
+				NAZARA_API friend bool operator>(const Id& lhs, const Id& rhs);
+				NAZARA_API friend bool operator>=(const Id& lhs, const Id& rhs);
+
+				NAZARA_API friend bool operator<<(std::ostream& o, const Id& id);
 
 			private:
-				Id(void* handle) : m_handle(handle) {}
-				Id(const NzThreadImpl* thread);
+				Id(NzThreadImpl* thread);
 
-				void* m_handle = nullptr;
+				NzThreadImpl* m_id = nullptr;
 		};
 
+		NzThread() = default;
+		NzThread(NzThread&& other);
 		template<typename F> NzThread(F function);
 		template<typename F, typename... Args> NzThread(F function, Args... args);
 		template<typename C> NzThread(void (C::*function)(), C* object);
 		~NzThread();
 
+		void Detach();
 		Id GetId() const;
-		bool IsCurrent() const;
-		void Launch(bool independent = false);
+		bool IsJoinable() const;
 		void Join();
-		void Terminate();
 
-		static Id GetCurrentId();
-		static void Sleep(nzUInt32 time);
+		NzThread& operator=(NzThread&& thread);
+
+		static unsigned int HardwareConcurrency();
+		static void Sleep(nzUInt32 milliseconds);
 
 	private:
-		NzFunctor* m_func;
+		void CreateImpl(NzFunctor* functor);
+
 		NzThreadImpl* m_impl = nullptr;
-		bool m_independent;
 };
 
 #include <Nazara/Core/Thread.inl>
