c2425cce4ca5bd37962e5b76f4b79cc847bcbebe
Lynix <lynix680@gmail.com>
2012-12-02 23:05:28 +0100

    Moved constructor prototype
    Former-commit-id: 1a8903bfd55bc6d50e8639f90d813343ede1f683

--- include/Nazara/Core/Thread.hpp
+++ include/Nazara/Core/Thread.hpp
@@ -40,10 +40,10 @@ class NAZARA_API NzThread : NzNonCopyable
 		};
 
 		NzThread() = default;
-		NzThread(NzThread&& other);
 		template<typename F> NzThread(F function);
 		template<typename F, typename... Args> NzThread(F function, Args... args);
 		template<typename C> NzThread(void (C::*function)(), C* object);
+		NzThread(NzThread&& other);
 		~NzThread();
 
 		void Detach();
