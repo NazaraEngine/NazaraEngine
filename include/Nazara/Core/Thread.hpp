d273b20373ac559893f5cec33e094c6500e10b47
Lynix <lynix680@gmail.com>
2012-12-02 23:43:08 +0100

    Simplified Thread interface

--- include/Nazara/Core/Thread.hpp
+++ include/Nazara/Core/Thread.hpp
@@ -40,7 +40,6 @@ class NAZARA_API NzThread : NzNonCopyable
 		};
 
 		NzThread() = default;
-		template<typename F> NzThread(F function);
 		template<typename F, typename... Args> NzThread(F function, Args... args);
 		template<typename C> NzThread(void (C::*function)(), C* object);
 		NzThread(NzThread&& other);
