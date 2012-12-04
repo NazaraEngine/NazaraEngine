da9f398999ac2569b498019b643d869de46131fa
Lynix <lynix680@gmail.com>
2012-12-04 10:35:34 +0100

    Removed blanks
    Former-commit-id: 0ee96417a37683d5e7fbc4d9314d7218f931b4c6

--- src/Nazara/Core/TaskScheduler.cpp
+++ src/Nazara/Core/TaskScheduler.cpp
@@ -2,8 +2,6 @@
 // This file is part of the "Nazara Engine - Core module"
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
-
-
 #include <Nazara/Core/TaskScheduler.hpp>
 #include <Nazara/Core/ConditionVariable.hpp>
 #include <Nazara/Core/Error.hpp>
