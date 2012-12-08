ac0fcee53b974abfb10a64b3d63c465375e0bf40
Lynix <lynix680@gmail.com>
2012-12-08 22:21:20 +0100

    Module can now initialize optionnal classes

--- include/Nazara/Renderer/Renderer.hpp
+++ include/Nazara/Renderer/Renderer.hpp
@@ -52,7 +52,7 @@ class NAZARA_API NzRenderer
 
 		static bool HasCapability(nzRendererCap capability);
 
-		static bool Initialize();
+		static bool Initialize(bool initializeDebugDrawer = false);
 
 		static bool IsEnabled(nzRendererParameter parameter);
 		static bool IsInitialized();
