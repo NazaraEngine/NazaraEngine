c9f81ad4b6a3ebf1e38c1521ef840ad49832567a
Lynix <lynix680@gmail.com>
2012-12-08 21:42:54 +0100

    Fixed typo

--- NazaraModuleTemplate/include/Nazara/ModuleName/ModuleName.hpp
+++ NazaraModuleTemplate/include/Nazara/ModuleName/ModuleName.hpp
@@ -23,7 +23,7 @@ class NAZARA_API NzModuleName
 		static void Uninitialize();
 
 	private:
-		static unsigned int s_moduleReferenceCouter;
+		static unsigned int s_moduleReferenceCounter;
 };
 
 #endif // NAZARA_MODULENAME_HPP
