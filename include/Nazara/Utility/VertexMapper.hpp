46fb2e74c1e277c79cebfe591ae39288ac15a7f3
Lynix <lynix680@gmail.com>
2013-01-03 19:39:52 +0100

    Added VertexMapper
    Former-commit-id: 07ae37e56b0f243b609bf713f9f612ecfba914df

---
+++ include/Nazara/Utility/VertexMapper.hpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Utility module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_VERTEXITERATOR_HPP
+#define NAZARA_VERTEXITERATOR_HPP
+
+#include <Nazara/Prerequesites.hpp>
+#include <Nazara/Math/Vector2.hpp>
+#include <Nazara/Math/Vector3.hpp>
+#include <Nazara/Utility/Enums.hpp>
+
+class NzSubMesh;
+class NzVertexBuffer;
+
+class NzVertexMapperImpl;
+
+class NAZARA_API NzVertexMapper
+{
+	public:
+		NzVertexMapper(NzSubMesh* subMesh);
+		~NzVertexMapper();
+
+		NzVector3f GetNormal(unsigned int i) const;
+		NzVector3f GetPosition(unsigned int i) const;
+		NzVector3f GetTangent(unsigned int i) const;
+		NzVector2f GetTexCoords(unsigned int i) const;
+		unsigned int GetTotalVertexCount();
+
+		void SetNormal(unsigned int i, const NzVector3f& normal);
+		void SetPosition(unsigned int i, const NzVector3f& position);
+		void SetTangent(unsigned int i, const NzVector3f& tangent);
+		void SetTexCoords(unsigned int i, const NzVector2f& texCoords);
+
+	private:
+		NzVertexMapperImpl* m_impl;
+};
+
+#endif // NAZARA_VERTEXITERATOR_HPP
