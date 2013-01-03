db86cc4dc393a3bf91567367869604530f8f2c4e
Lynix <lynix680@gmail.com>
2013-01-03 16:24:59 +0100

    Added BufferMapper class
    Used to map/unmap a buffer automatically and provide a RAII
    exception-safe class
    
    
    Former-commit-id: 2a4fd2e19ce3531639a86520876a3615846b27f4

---
+++ include/Nazara/Utility/BufferMapper.hpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Utility module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_BUFFERMAPPER_HPP
+#define NAZARA_BUFFERMAPPER_HPP
+
+#include <Nazara/Utility/Enums.hpp>
+
+template<class T>
+class NzBufferMapper
+{
+	public:
+		NzBufferMapper(T* buffer, nzBufferAccess access, unsigned int offset = 0, unsigned int length = 0);
+		NzBufferMapper(T& buffer, nzBufferAccess access, unsigned int offset = 0, unsigned int length = 0);
+		NzBufferMapper(const T* buffer, nzBufferAccess access, unsigned int offset = 0, unsigned int length = 0);
+		NzBufferMapper(const T& buffer, nzBufferAccess access, unsigned int offset = 0, unsigned int length = 0);
+		~NzBufferMapper();
+
+		const T* GetBuffer() const;
+		void* GetPointer() const;
+
+		void Unmap();
+
+	private:
+		const T* m_buffer;
+		void* m_ptr;
+};
+
+#include <Nazara/Utility/BufferMapper.inl>
+
+#endif // NAZARA_BUFFERMAPPER_HPP
