5e529cfffaf7f31082a10a42993a93da8b9fa86a
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2013-01-04 18:21:45 +0100

    Add Posix semaphore support.

---
+++ src/Nazara/Core/Posix/SemaphoreImpl.cpp
@@ -0,0 +1,68 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/Core/Posix/SemaphoreImpl.hpp>
+#include <Nazara/Core/Config.hpp>
+#include <Nazara/Core/Error.hpp>
+#include <limits>
+#include <Nazara/Core/Debug.hpp>
+
+NzSemaphoreImpl::NzSemaphoreImpl(unsigned int count)
+{
+	m_semaphore = sem_init(&m_semaphore, 0, count);
+	if (!m_semaphore)
+		NazaraError("Failed to create semaphore: " + NzGetLastSystemError());
+}
+
+NzSemaphoreImpl::~NzSemaphoreImpl()
+{
+	sem_destroy(&m_semaphore);
+}
+
+unsigned int NzSemaphoreImpl::GetCount() const
+{
+	int count;
+	sem_getvalue(&m_semaphore, &count);
+	return static_cast<unsigned int>(count);
+}
+
+void NzSemaphoreImpl::Post()
+{
+	#if NAZARA_CORE_SAFE
+	if (sem_post(&m_semaphore)==-1)
+		NazaraError("Failed to release semaphore: " + NzGetLastSystemError());
+	#else
+	sem_post(&m_semaphore);
+	#endif
+}
+
+void NzSemaphoreImpl::Wait()
+{
+	#if NAZARA_CORE_SAFE
+	if (sem_wait(&m_semaphore) == -1 )
+		NazaraError("Failed to wait for semaphore: " + NzGetLastSystemError());
+	#else
+	sem_wait(&m_semaphore);
+	#endif
+}
+
+bool NzSemaphoreImpl::Wait(nzUInt32 timeout)
+{
+    timespec ti;
+    ti.tv_nsec = (tv.tv_usec + (timeout % 1000)) * 1000000;
+    ti.tv_sec = tv.tv_sec + (timeout / 1000) + (ti.tv_nsec / 1000000000);
+    ti.tv_nsec %= 1000000000;
+
+	#if NAZARA_CORE_SAFE
+	if (sem_timedwait(m_semaphore, timeout) == -1)
+	{
+		NazaraError("Failed to wait for semaphore: " + NzGetLastSystemError());
+		return false;
+	}
+
+    return true;
+	#else
+        return sem_timedwait(&m_semaphore, ti) != -1;
+	#endif
+}
