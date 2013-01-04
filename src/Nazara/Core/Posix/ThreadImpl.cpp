e00779ebe0c93b9d409254418ce0f7f92752150f
Lynix <lynix680@gmail.com>
2013-01-04 22:01:06 +0100

    [Core] Added POSIX support (Experimental)
    Thanks to Danman
    I don't think it actually compile although tried my best to correct
    code.
    
    
    Former-commit-id: c3366c2c924f7392f4d46824dafca63ecf7f1fea

---
+++ src/Nazara/Core/Posix/ThreadImpl.cpp
@@ -0,0 +1,73 @@
+// Copyright (C) 2012 Alexandre Janniaux
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/Core/Posix/ThreadImpl.hpp>
+#include <Nazara/Core/Error.hpp>
+#include <Nazara/Core/Functor.hpp>
+#include <process.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <Nazara/Core/Debug.hpp>
+
+NzThreadImpl::NzThreadImpl(NzFunctor* functor)
+{
+    int error = pthread_create(&m_handle, nullptr, &NzThreadImpl::ThreadProc, functor);
+	if (error != 0)
+		NazaraInternalError("Failed to create thread: " + NzGetLastSystemError());
+}
+
+void NzThreadImpl::Detach()
+{
+	pthread_detach(m_handle);
+}
+
+void NzThreadImpl::Join()
+{
+	pthread_join(m_handle, nullptr);
+}
+
+unsigned int NzThreadImpl::ThreadProc(void* userdata)
+{
+	NzFunctor* func = static_cast<NzFunctor*>(userdata);
+	func->Run();
+	delete func;
+
+	return 0;
+}
+
+void NzThreadImpl::Sleep(nzUInt32 time)
+{
+    // code from SFML2 Unix SleepImpl.cpp source https://github.com/LaurentGomila/SFML/blob/master/src/SFML/System/Unix/SleepImpl.cpp
+
+    // usleep is not reliable enough (it might block the
+    // whole process instead of just the current thread)
+    // so we must use pthread_cond_timedwait instead
+
+    // this implementation is inspired from Qt
+
+    // get the current time
+    timeval tv;
+    gettimeofday(&tv, NULL);
+
+    // construct the time limit (current time + time to wait)
+    timespec ti;
+    ti.tv_nsec = (tv.tv_usec + (time % 1000)) * 1000;
+    ti.tv_sec = tv.tv_sec + (time / 1000) + (ti.tv_nsec / 1000000000);
+    ti.tv_nsec %= 1000000000;
+
+    // create a mutex and thread condition
+    pthread_mutex_t mutex;
+    pthread_mutex_init(&mutex, 0);
+    pthread_cond_t condition;
+    pthread_cond_init(&condition, 0);
+
+    // wait...
+    pthread_mutex_lock(&mutex);
+    pthread_cond_timedwait(&condition, &mutex, &ti);
+    pthread_mutex_unlock(&mutex);
+
+    // destroy the mutex and condition
+    pthread_cond_destroy(&condition);
+    pthread_mutex_destroy(&mutex);
+}
