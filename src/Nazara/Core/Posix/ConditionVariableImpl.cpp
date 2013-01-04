d6fe2d3ac595d6292cde9ce078cb72e051e464c4
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2013-01-04 18:20:42 +0100

    Add Posix condition variable support.

---
+++ src/Nazara/Core/Posix/ConditionVariableImpl.cpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+// Source: http://www.cs.wustl.edu/~schmidt/win32-cv-1.html
+
+#include <Nazara/Core/Posix/ConditionVariableImpl.hpp>
+#include <Nazara/Core/Posix/MutexImpl.hpp>
+#include <Nazara/Core/Debug.hpp>
+
+NzConditionVariableImpl::NzConditionVariableImpl()
+{
+	pthread_cond_init(&m_cv, nullptr);
+}
+
+NzConditionVariableImpl::~NzConditionVariableImpl()
+{
+    pthread_cond_destroy(&m_cv);
+}
+
+void NzConditionVariableImpl::Signal()
+{
+    pthread_cond_signal(&m_cv);
+}
+
+void NzConditionVariableImpl::SignalAll()
+{
+    pthread_cond_broadcast(&m_cv);
+}
+
+void NzConditionVariableImpl::Wait(NzMutexImpl* mutex)
+{
+	pthread_cond_wait(&m_cv, mutex);
+}
+
+bool NzConditionVariableImpl::Wait(NzMutexImpl* mutex, nzUInt32 timeout)
+{
+
+
+    // get the current time
+    timeval tv;
+    gettimeofday(&tv, NULL);
+
+    // construct the time limit (current time + time to wait)
+    timespec ti;
+    ti.tv_nsec = (tv.tv_usec + (timeout % 1000)) * 1000000;
+    ti.tv_sec = tv.tv_sec + (timeout / 1000) + (ti.tv_nsec / 1000000000);
+    ti.tv_nsec %= 1000000000;
+
+    pthread_cond_timedwait(&m_cv,mutex, &tv);
+
+}
