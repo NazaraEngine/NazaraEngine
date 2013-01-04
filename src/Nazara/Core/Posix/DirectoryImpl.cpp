cfa7704f00d290347421f1a60d17b2a584e58a7a
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2013-01-04 18:22:13 +0100

    Add Posix directory support.

---
+++ src/Nazara/Core/Posix/DirectoryImpl.cpp
@@ -0,0 +1,101 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/Core/Posix/DirectoryImpl.hpp>
+#include <Nazara/Core/Error.hpp>
+#include <Nazara/Core/Debug.hpp>
+
+
+NzDirectoryImpl::NzDirectoryImpl(const NzDirectory* parent)
+{
+	NazaraUnused(parent);
+}
+
+void NzDirectoryImpl::Close()
+{
+	closedir(m_handle);
+}
+
+NzString NzDirectoryImpl::GetResultName() const
+{
+	return NzString::Unicode(m_result.d_name);
+}
+
+nzUInt64 NzDirectoryImpl::GetResultSize() const
+{
+    struct stat64 resulststat;
+	stat64(m_result.d_name, &resulststat);
+
+    return static_cast<nzUInt64>(resulststat.st_size);
+}
+
+bool NzDirectoryImpl::IsResultDirectory() const
+{
+	return S_ISDIR(m_result.d_name);
+}
+
+bool NzDirectoryImpl::NextResult()
+{
+
+	if (m_result = readdir64(m_handle))
+		return true;
+	else
+	{
+		if (errno != ENOENT)
+			NazaraError("Unable to get next result: " + NzGetLastSystemError());
+
+		return false;
+	}
+}
+
+bool NzDirectoryImpl::Open(const NzString& dirPath)
+{
+    m_handle = opendir(dirPath.GetConstBuffer());
+
+	if (m_handle == NULL)
+	{
+		NazaraError("Unable to open directory: " + NzGetLastSystemError());
+		return false;
+	}
+
+	return true;
+}
+
+bool NzDirectoryImpl::Create(const NzString& dirPath)
+{
+    mode_t permissions; // TODO: check permissions
+    bool success = mkdir(dirPath.GetConstBuffer(), permissions) != -1;
+
+	return success;
+}
+
+bool NzDirectoryImpl::Exists(const NzString& dirPath)
+{
+    if (S_ISDIR(dirPath.GetConstBuffer()))
+        return true;
+    return false;
+}
+
+NzString NzDirectoryImpl::GetCurrent()
+{
+	NzString currentPath;
+	char* path = new char[_PC_PATH_MAX];
+
+    if (getcwd(path, _PC_PATH_MAX))
+        currentPath = NzString::Unicode(path);
+    else
+		NazaraError("Unable to get current directory: " + NzGetLastSystemError());
+
+	delete[] path;
+
+	return currentPath;
+}
+
+bool NzDirectoryImpl::Remove(const NzString& dirPath)
+{
+
+	bool success = rmdir(dirPath.GetConstBuffer()) != -1;
+
+	return success;
+}
