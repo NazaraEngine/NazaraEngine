77a87ce2c9ed8225dae772dd0c854630224da6ee
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2013-01-04 18:30:10 +0100

    Add Posix hardware infos support.
    Former-commit-id: adbdeb13349dfea4d962f0df6668f51937803fc4

---
+++ src/Nazara/Core/Posix/HardwareInfoImpl.hpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_HARDWAREINFOIMPL_POSIX_HPP
+#define NAZARA_HARDWAREINFOIMPL_POSIX_HPP
+
+#include <Nazara/Prerequesites.hpp>
+#include <unistd.h>
+
+class NzHardwareInfoImpl
+{
+	public:
+		static void Cpuid(nzUInt32 code, nzUInt32 result[4]);
+		static unsigned int GetProcessorCount();
+		static bool IsCpuidSupported();
+};
+
+#endif // NAZARA_HARDWAREINFOIMPL_POSIX_HPP
