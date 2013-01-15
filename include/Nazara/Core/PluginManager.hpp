3e13a345cb726f79e2430ad4524a47d6365bbd71
Lynix <lynix680@gmail.com>
2013-01-15 16:54:41 +0100

    Added experimental PluginManager

---
+++ include/Nazara/Core/PluginManager.hpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_PLUGINMANAGER_HPP
+#define NAZARA_PLUGINMANAGER_HPP
+
+#include <Nazara/Prerequesites.hpp>
+#include <Nazara/Core/Enums.hpp>
+#include <Nazara/Core/String.hpp>
+
+class NzPluginManager
+{
+	public:
+		NzPluginManager() = delete;
+		~NzPluginManager() = delete;
+
+		static bool AddDirectory(const NzString& directoryPath);
+
+		static bool Initialize();
+
+		static bool Mount(nzPlugin plugin);
+		static bool Mount(const NzString& pluginPath);
+
+		static void RemoveDirectory(const NzString& directoryPath);
+
+		static void Unmount(nzPlugin plugin);
+		static void Unmount(const NzString& pluginPath);
+
+		static void Uninitialize();
+};
+
+#endif // NAZARA_PLUGINMANAGER_HPP
