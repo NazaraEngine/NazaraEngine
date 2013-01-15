37e42db4d3b7ef155b863e7e753a4e5b4bd4d553
Lynix <lynix680@gmail.com>
2013-01-15 15:43:46 +0100

    Added NAZARA_DYNLIB_EXTENSION macro
    Former-commit-id: 2be41352286994e4b17ec94b11282e35e9acd995

--- include/Nazara/Core/DynLib.hpp
+++ include/Nazara/Core/DynLib.hpp
@@ -11,14 +11,25 @@
 #include <Nazara/Core/NonCopyable.hpp>
 #include <Nazara/Core/String.hpp>
 
+#if defined(NAZARA_PLATFORM_WINDOWS)
+	#define NAZARA_DYNLIB_EXTENSION ".dll"
+#elif defined(NAZARA_PLATFORM_LINUX)
+	#define NAZARA_DYNLIB_EXTENSION ".so"
+#elif defined(NAZARA_PLATFORM_MACOSX)
+	#define NAZARA_DYNLIB_EXTENSION ".dynlib"
+#else
+	#error OS not handled
+#endif
+
 #if NAZARA_CORE_THREADSAFE && NAZARA_THREADSAFETY_DYNLIB
 #include <Nazara/Core/ThreadSafety.hpp>
 #else
 #include <Nazara/Core/ThreadSafetyOff.hpp>
 #endif
 
+using NzDynLibFunc =  int (*)(); // Type "générique" de pointeur sur fonction
+
 class NzDynLibImpl;
-typedef int (*NzDynLibFunc)(); // Type "générique" de pointeur sur fonction
 
 class NzDynLib : NzNonCopyable
 {
