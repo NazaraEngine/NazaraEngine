3495cdbb0d0c26d063cc65cd40a3b3f59c45781f
Lynix <lynix680@gmail.com>
2013-03-02 01:03:20 +0100

    Added interfaces

---
+++ include/Nazara/2D/Drawable.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - 2D Module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_DRAWABLE_HPP
+#define NAZARA_DRAWABLE_HPP
+
+#include <Nazara/Prerequesites.hpp>
+
+class NAZARA_API NzDrawable
+{
+	public:
+		virtual void Draw() const = 0;
+};
+
+#endif // NAZARA_DRAWABLE_HPP
---
+++ include/Nazara/Core/Updatable.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core Module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_UPDATABLE_HPP
+#define NAZARA_UPDATABLE_HPP
+
+#include <Nazara/Prerequesites.hpp>
+
+class NzUpdatable
+{
+	public:
+		virtual void Update() = 0;
+};
+
+#endif // NAZARA_UPDATABLE_HPP
