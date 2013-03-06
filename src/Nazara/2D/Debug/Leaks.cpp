603613da190b5e6cfbc269c65dc1e04757fd2222
Lynix <lynix680@gmail.com>
2013-03-06 12:57:15 +0100

    Fixed missing Leaks.cpp in 2D/3D modules

---
+++ src/Nazara/2D/Debug/Leaks.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - 2D Module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/2D/Config.hpp>
+#if NAZARA_2D_MEMORYLEAKTRACKER || defined(NAZARA_DEBUG)
+#include <Nazara/Core/Debug/MemoryLeakTracker.hpp>
+#include <new>
+
+void* operator new(std::size_t size)
+{
+	return NzMemoryManager::Allocate(size, false);
+}
+
+void* operator new[](std::size_t size)
+{
+	return NzMemoryManager::Allocate(size, true);
+}
+
+void operator delete(void* pointer) noexcept
+{
+	NzMemoryManager::Free(pointer, false);
+}
+
+void operator delete[](void* pointer) noexcept
+{
+	NzMemoryManager::Free(pointer, true);
+}
+#endif
