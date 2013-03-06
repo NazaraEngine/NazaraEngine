95c51b38ae9d607e3a9614ba40dc9efbdee40b8f
Lynix <lynix680@gmail.com>
2013-03-06 13:42:09 +0100

    Added missing Leaks.cpp in Audio module

---
+++ src/Nazara/Audio/Debug/Leaks.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2012 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Audio module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/Audio/Config.hpp>
+#if NAZARA_AUDIO_MEMORYLEAKTRACKER || defined(NAZARA_DEBUG)
+#include <Nazara/Core/Debug/MemoryLeakTracker.hpp>
+#include <new>
+
+void* operator new(std::size_t size)
+{
+	return NzMemoryManager::Allocate(size, false);
+}
+
+void* operator new[](std::size_t size)
+{
+	return NzMemoryManager::Allocate(size, true);
+}
+
+void operator delete(void* pointer) noexcept
+{
+	NzMemoryManager::Free(pointer, false);
+}
+
+void operator delete[](void* pointer) noexcept
+{
+	NzMemoryManager::Free(pointer, true);
+}
+#endif
