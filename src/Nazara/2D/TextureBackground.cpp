8e3207c0fe8ac9bb8e0f57a1e7d077d7dfd77e37
Lynix <lynix680@gmail.com>
2013-03-22 11:15:17 +0100

    Added Texture backgrounds

---
+++ src/Nazara/2D/TextureBackground.cpp
@@ -0,0 +1,44 @@
+// Copyright (C) 2013 Jérôme Leclercq
+// This file is part of the "Nazara Engine - 2D Module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/2D/TextureBackground.hpp>
+#include <Nazara/Renderer/Renderer.hpp>
+#include <Nazara/Renderer/RenderTarget.hpp>
+#include <Nazara/2D/Debug.hpp>
+
+NzTextureBackground::NzTextureBackground(NzTexture* texture) :
+m_texture(texture)
+{
+}
+
+void NzTextureBackground::Draw() const
+{
+	const NzRenderTarget* target = NzRenderer::GetTarget();
+	NzRectui viewport = NzRenderer::GetViewport();
+
+	// Sous forme de flottants pour la division flottante
+	float width = target->GetWidth();
+	float height = target->GetHeight();
+
+	NzVector2f uv0(viewport.x/width, viewport.y/height);
+	NzVector2f uv1((viewport.x+viewport.width)/width, (viewport.y+viewport.height)/height);
+
+	NzRenderer::SetTexture(0, m_texture);
+	NzRenderer::DrawTexture(0, NzRectf(0.f, 0.f, width, height), uv0, uv1, 1.f);
+}
+
+nzBackgroundType NzTextureBackground::GetBackgroundType() const
+{
+	return nzBackgroundType_Texture;
+}
+
+NzTexture* NzTextureBackground::GetTexture() const
+{
+	return m_texture;
+}
+
+void NzTextureBackground::SetTexture(NzTexture* texture)
+{
+	m_texture = texture;
+}
