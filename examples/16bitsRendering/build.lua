aaab082a4cf1380496b48e2623e263a3ddbf63f9
Lynix <lynix680@gmail.com>
2013-03-22 18:16:01 +0100

    Added 16bits demo

---
+++ examples/16bitsRendering/build.lua
@@ -0,0 +1,45 @@
+kind "ConsoleApp"
+
+files "main.cpp"
+
+if (_OPTIONS["united"]) then
+	configuration "DebugStatic"
+		links "NazaraEngine-s-d"
+
+	configuration "ReleaseStatic"
+		links "NazaraEngine-s"
+
+	configuration "DebugDLL"
+		links "NazaraEngine-d"
+
+	configuration "ReleaseDLL"
+		links "NazaraEngine"
+else
+	configuration "DebugStatic"
+		links "Nazara3D-s-d"
+		links "Nazara2D-s-d"
+		links "NazaraRenderer-s-d"
+		links "NazaraUtility-s-d"
+		links "NazaraCore-s-d"
+
+	configuration "ReleaseStatic"
+		links "Nazara3D-s"
+		links "Nazara2D-s"
+		links "NazaraRenderer-s"
+		links "NazaraUtility-s"
+		links "NazaraCore-s"
+
+	configuration "DebugDLL"
+		links "Nazara3D-d"
+		links "Nazara2D-d"
+		links "NazaraRenderer-d"
+		links "NazaraUtility-d"
+		links "NazaraCore-d"
+
+	configuration "ReleaseDLL"
+		links "Nazara3D"
+		links "Nazara2D"
+		links "NazaraRenderer"
+		links "NazaraUtility"
+		links "NazaraCore"
+end
