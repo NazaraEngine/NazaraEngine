8c0867d8781291c1465f5db696ce43f5362d7829
Lynix <lynix680@gmail.com>
2013-03-24 01:03:21 +0100

    Added ModelLoader
    Changed Model::SetMesh behavior

---
+++ src/Nazara/3D/Loaders/Mesh/Loader.cpp
@@ -0,0 +1,89 @@
+// Copyright (C) 2013 Jérôme Leclercq
+// This file is part of the "Nazara Engine - 3D module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/3D/Loaders/Mesh.hpp>
+#include <Nazara/3D/Model.hpp>
+#include <Nazara/Renderer/Material.hpp>
+#include <Nazara/Utility/Mesh.hpp>
+#include <memory>
+#include <Nazara/3D/Debug.hpp>
+
+namespace
+{
+	bool Check(NzInputStream& stream, const NzModelParameters& parameters)
+	{
+		NazaraUnused(stream);
+		NazaraUnused(parameters);
+
+		return true; ///FIXME: Pas bon
+	}
+
+	bool Load(NzModel* model, NzInputStream& stream, const NzModelParameters& parameters)
+	{
+		NazaraUnused(parameters);
+
+		std::unique_ptr<NzMesh> mesh(new NzMesh);
+		mesh->SetPersistent(false, false);
+		if (!mesh->LoadFromStream(stream))
+		{
+			NazaraError("Failed to load model mesh");
+			return false;
+		}
+
+		// Nous ne pouvons plus avoir recours au smart pointeur à partir d'ici si nous voulons être exception-safe
+		NzMesh* meshPtr = mesh.get();
+
+		model->Reset();
+		model->SetMesh(meshPtr);
+		mesh.release();
+
+		if (parameters.loadAnimation && meshPtr->IsAnimable())
+		{
+			NzString animationPath = meshPtr->GetAnimation();
+			if (!animationPath.IsEmpty())
+			{
+				std::unique_ptr<NzAnimation> animation(new NzAnimation);
+				animation->SetPersistent(false, false);
+				if (animation->LoadFromFile(animationPath, parameters.animation) && model->SetAnimation(animation.get()))
+					animation.release();
+				else
+					NazaraWarning("Failed to load animation");
+			}
+		}
+
+		if (parameters.loadMaterials)
+		{
+			unsigned int matCount = model->GetMaterialCount();
+
+			for (unsigned int i = 0; i < matCount; ++i)
+			{
+				NzString mat = meshPtr->GetMaterial(i);
+				if (!mat.IsEmpty())
+				{
+					std::unique_ptr<NzMaterial> material(new NzMaterial);
+					material->SetPersistent(false, false);
+					if (material->LoadFromFile(mat, parameters.material))
+					{
+						model->SetMaterial(i, material.get());
+						material.release();
+					}
+					else
+						NazaraWarning("Failed to load material #" + NzString::Number(i));
+				}
+			}
+		}
+
+		return true;
+	}
+}
+
+void NzLoaders_Mesh_Register()
+{
+	NzModelLoader::RegisterLoader(NzMeshLoader::IsExtensionSupported, Check, Load);
+}
+
+void NzLoaders_Mesh_Unregister()
+{
+	NzModelLoader::UnregisterLoader(NzMeshLoader::IsExtensionSupported, Check, Load);
+}
