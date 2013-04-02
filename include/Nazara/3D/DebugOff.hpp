cfbe388d22c8daaf700786c984b76ba7b2b6309c
Lynix <lynix680@gmail.com>
2013-04-03 01:14:55 +0200

    Merged 2D and 3D modules into Graphics module
    Former-commit-id: cf33c6999cb08e42220507f5583b965d888274c8

--- include/Nazara/3D/DebugOff.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2013 Jérôme Leclercq
-// This file is part of the "Nazara Engine - 3D Module"
-// For conditions of distribution and use, see copyright notice in Config.hpp
-
-#if NAZARA_3D_MEMORYLEAKTRACKER || defined(NAZARA_DEBUG)
-	#undef delete
-	#undef new
-#endif
