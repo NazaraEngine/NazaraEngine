619a6e2db66b67d02fe491b644bb869d1208305e
Lynix <lynix680@gmail.com>
2013-05-11 14:04:08 +0200

    Improved Windows ClockGetMicroseconds precision

--- src/Nazara/Core/Win32/ClockImpl.cpp
+++ src/Nazara/Core/Win32/ClockImpl.cpp
@@ -21,13 +21,13 @@ bool NzClockImplInitializeHighPrecision()
 nzUInt64 NzClockImplGetMicroseconds()
 {
 	// http://msdn.microsoft.com/en-us/library/windows/desktop/ms644904(v=vs.85).aspx
-	HANDLE thread = GetCurrentThread();
-	DWORD oldMask = SetThreadAffinityMask(thread, 1);
+	//HANDLE thread = GetCurrentThread();
+	//DWORD oldMask = SetThreadAffinityMask(thread, 1);
 
 	LARGE_INTEGER time;
 	QueryPerformanceCounter(&time);
 
-	SetThreadAffinityMask(thread, oldMask);
+	//SetThreadAffinityMask(thread, oldMask);
 
 	return time.QuadPart*1000000ULL / frequency.QuadPart;
 }
