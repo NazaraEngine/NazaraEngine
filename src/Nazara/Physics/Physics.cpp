a742e4c6e0f4dad090e7c5ca893fd5794a23edd4
Lynix <lynix680@gmail.com>
2013-05-23 23:16:36 +0200

    Added Physics module

---
+++ src/Nazara/Physics/Physics.cpp
@@ -0,0 +1,64 @@
+// Copyright (C) 2013 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Physics module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/Physics/Physics.hpp>
+#include <Nazara/Core/Core.hpp>
+#include <Nazara/Core/Error.hpp>
+#include <Nazara/Core/Log.hpp>
+#include <Nazara/Physics/Config.hpp>
+#include <Newton/Newton.h>
+#include <Nazara/Physics/Debug.hpp>
+
+unsigned int NzPhysics::GetMemoryUsed()
+{
+	return NewtonGetMemoryUsed();
+}
+
+bool NzPhysics::Initialize()
+{
+	if (s_moduleReferenceCounter++ != 0)
+		return true; // Déjà initialisé
+
+	// Initialisation des dépendances
+	if (!NzCore::Initialize())
+	{
+		NazaraError("Failed to initialize core module");
+		Uninitialize();
+
+		return false;
+	}
+
+	// Initialisation du module
+
+	NazaraNotice("Initialized: Physics module");
+
+	return true;
+}
+
+bool NzPhysics::IsInitialized()
+{
+	return s_moduleReferenceCounter != 0;
+}
+
+void NzPhysics::Uninitialize()
+{
+	if (s_moduleReferenceCounter != 1)
+	{
+		// Le module est soit encore utilisé, soit pas initialisé
+		if (s_moduleReferenceCounter > 1)
+			s_moduleReferenceCounter--;
+
+		return;
+	}
+
+	// Libération du module
+	s_moduleReferenceCounter = 0;
+
+	NazaraNotice("Uninitialized: Physics module");
+
+	// Libération des dépendances
+	NzCore::Uninitialize();
+}
+
+unsigned int NzPhysics::s_moduleReferenceCounter = 0;
