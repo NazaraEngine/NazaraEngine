844ed35fda81fc3872d63a521951bbec22d00f2d
Lynix <lynix680@gmail.com>
2013-08-01 23:21:08 +0200

    Moved OpenGL header include

--- src/Nazara/Renderer/RenderWindow.cpp
+++ src/Nazara/Renderer/RenderWindow.cpp
@@ -2,11 +2,11 @@
 // This file is part of the "Nazara Engine - Renderer module"
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
-#include <Nazara/Renderer/OpenGL.hpp>
 #include <Nazara/Renderer/RenderWindow.hpp>
 #include <Nazara/Core/Error.hpp>
 #include <Nazara/Core/Thread.hpp>
 #include <Nazara/Renderer/Context.hpp>
+#include <Nazara/Renderer/OpenGL.hpp>
 #include <Nazara/Renderer/Texture.hpp>
 #include <stdexcept>
 #include <Nazara/Renderer/Debug.hpp>
