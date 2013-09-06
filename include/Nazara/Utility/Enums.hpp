0fd535a9e81ed2495cc4178e67b29cf37c33763a
Lynix <lynix680@gmail.com>
2013-09-06 12:07:38 +0200

    Added a lot more texture formats
    Former-commit-id: b3a553ea39a174d84c5cd6fcca853ee83c1e6670

--- include/Nazara/Utility/Enums.hpp
+++ include/Nazara/Utility/Enums.hpp
@@ -152,25 +152,49 @@ enum nzPixelFormat
 {
 	nzPixelFormat_Undefined = -1,
 
-	nzPixelFormat_BGR8,            // 3*nzUInt8
-	nzPixelFormat_BGRA8,           // 4*nzUInt8
+	nzPixelFormat_BGR8,            // 3*uint8
+	nzPixelFormat_BGRA8,           // 4*uint8
 	nzPixelFormat_DXT1,
 	nzPixelFormat_DXT3,
 	nzPixelFormat_DXT5,
-	nzPixelFormat_L8,              // 1*nzUInt8
-	nzPixelFormat_LA8,             // 2*nzUInt8
-	nzPixelFormat_RGB5A1,          // 1*nzUInt16
-	nzPixelFormat_RGB8,            // 3*nzUInt8
-	nzPixelFormat_RGB16F,
-	nzPixelFormat_RGB16I,          // 4*nzUInt16
+	nzPixelFormat_L8,              // 1*uint8
+	nzPixelFormat_LA8,             // 2*uint8
+	nzPixelFormat_R8,              // 1*uint8
+	nzPixelFormat_R8I,             // 1*int8
+	nzPixelFormat_R8UI,            // 1*uint8
+	nzPixelFormat_R16,             // 1*uint16
+	nzPixelFormat_R16F,            // 1*half
+	nzPixelFormat_R16I,            // 1*int16
+	nzPixelFormat_R16UI,           // 1*uint16
+	nzPixelFormat_R32F,            // 1*float
+	nzPixelFormat_R32I,            // 1*uint16
+	nzPixelFormat_R32UI,           // 1*uint32
+	nzPixelFormat_RG8,             // 2*int8
+	nzPixelFormat_RG8I,            // 2*int8
+	nzPixelFormat_RG8UI,           // 2*uint8
+	nzPixelFormat_RG16,            // 2*uint16
+	nzPixelFormat_RG16F,           // 2*half
+	nzPixelFormat_RG16I,           // 2*int16
+	nzPixelFormat_RG16UI,          // 2*uint16
+	nzPixelFormat_RG32F,           // 2*float
+	nzPixelFormat_RG32I,           // 2*uint16
+	nzPixelFormat_RG32UI,          // 2*uint32
+	nzPixelFormat_RGB5A1,          // 3*uint5 + alpha bit
+	nzPixelFormat_RGB8,            // 3*uint8
+	nzPixelFormat_RGB16F,          // 3*half
+	nzPixelFormat_RGB16I,          // 4*int16
+	nzPixelFormat_RGB16UI,         // 4*uint16
 	nzPixelFormat_RGB32F,          // 3*float
-	nzPixelFormat_RGB32I,          // 4*nzUInt32
-	nzPixelFormat_RGBA4,           // 1*nzUInt16
-	nzPixelFormat_RGBA8,           // 4*nzUInt8
-	nzPixelFormat_RGBA16F,
-	nzPixelFormat_RGBA16I,         // 4*nzUInt16
+	nzPixelFormat_RGB32I,          // 4*int32
+	nzPixelFormat_RGB32UI,         // 4*uint32
+	nzPixelFormat_RGBA4,           // 4*uint4
+	nzPixelFormat_RGBA8,           // 4*uint8
+	nzPixelFormat_RGBA16F,         // 4*half
+	nzPixelFormat_RGBA16I,         // 4*int16
+	nzPixelFormat_RGBA16UI,        // 4*uint16
 	nzPixelFormat_RGBA32F,         // 4*float
-	nzPixelFormat_RGBA32I,         // 4*nzUInt32
+	nzPixelFormat_RGBA32I,         // 4*int32
+	nzPixelFormat_RGBA32UI,        // 4*uint32
 	nzPixelFormat_Depth16,
 	nzPixelFormat_Depth24,
 	nzPixelFormat_Depth24Stencil8,
