8a30c80444afc43d38935a8473d34d3ff2319934
Lynix <lynix680@gmail.com>
2013-09-20 21:47:35 +0200

    Fixed missing file from commit b373633

--- src/Nazara/Renderer/AbstractShaderProgram.hpp
+++ src/Nazara/Renderer/AbstractShaderProgram.hpp
@@ -41,17 +41,32 @@ class NzAbstractShaderProgram
 		virtual bool SendBoolean(int location, bool value) = 0;
 		virtual bool SendColor(int location, const NzColor& color) = 0;
 		virtual bool SendDouble(int location, double value) = 0;
+		virtual bool SendDoubleArray(int location, const double* values, unsigned int count) = 0;
 		virtual bool SendFloat(int location, float value) = 0;
+		virtual bool SendFloatArray(int location, const float* values, unsigned int count) = 0;
 		virtual bool SendInteger(int location, int value) = 0;
+		virtual bool SendIntegerArray(int location, const int* value, unsigned int count) = 0;
 		virtual bool SendMatrix(int location, const NzMatrix4d& matrix) = 0;
 		virtual bool SendMatrix(int location, const NzMatrix4f& matrix) = 0;
 		virtual bool SendTexture(int location, const NzTexture* texture, nzUInt8* textureUnit = nullptr) = 0;
 		virtual bool SendVector(int location, const NzVector2d& vector) = 0;
 		virtual bool SendVector(int location, const NzVector2f& vector) = 0;
+		virtual bool SendVector(int location, const NzVector2i& vector) = 0;
 		virtual bool SendVector(int location, const NzVector3d& vector) = 0;
 		virtual bool SendVector(int location, const NzVector3f& vector) = 0;
+		virtual bool SendVector(int location, const NzVector3i& vector) = 0;
 		virtual bool SendVector(int location, const NzVector4d& vector) = 0;
 		virtual bool SendVector(int location, const NzVector4f& vector) = 0;
+		virtual bool SendVector(int location, const NzVector4i& vector) = 0;
+		virtual bool SendVectorArray(int location, const NzVector2d* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector2f* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector2i* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector3d* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector3f* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector3i* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector4d* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector4f* vectors, unsigned int count) = 0;
+		virtual bool SendVectorArray(int location, const NzVector4i* vectors, unsigned int count) = 0;
 };
 
 #endif // NAZARA_ABSTRACTSHADERPROGRAM_HPP
