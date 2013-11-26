a5230281b99ccb6737d8f6786cf59c0f67761def
Jérôme Leclercq <lynix680@gmail.com>
2013-11-26 15:59:49 +0100

    Optimized resource encoder
    Is now three times faster

--- build/scripts/actions/encodesresources.lua
+++ build/scripts/actions/encodesresources.lua
@@ -1,30 +1,28 @@
 function encodeResources()
 	print("Encoding resources ...")
+	local startClock = os.clock()
 	local modules = os.matchdirs("../src/Nazara/*")
 	for k, modulePath in pairs(modules) do
-		local file
-		local headerContent
 		local moduleName = modulePath:sub(15, -1)
-		local resourceContent
 		local files = os.matchfiles(modulePath .. "/Resources/**")
 		for k, filePath in pairs(files) do
 			if (filePath:sub(-2) ~= ".h") then
-				file = filePath:sub(modulePath:len() + 12, -1)
+				local file = filePath:sub(modulePath:len() + 12, -1)
 				local resource, err = io.open(filePath, "rb")
 				if (not resource) then
 					error("Failed to read resource file " .. file .. ": " .. err)
 				end
 
-				resourceContent = resource:read("*a")
+				local resourceContent = resource:read("*a")
 				resource:close()
 
 				local contentLength = resourceContent:len()
 
-				headerContent = ""
+				local headerContentTable = {}
 				for i = 1, contentLength do
-					local b = resourceContent:sub(i, i):byte()
-					headerContent = headerContent .. string.format("%d,", b)
+					table.insert(headerContentTable, string.format("%d,", resourceContent:byte(i)))
 				end
+				local headerContent = table.concat(headerContentTable)
 
 				local header, err = io.open(filePath .. ".h", "w+")
 				if (not header) then
@@ -33,11 +31,12 @@ function encodeResources()
 
 				header:write(headerContent)
 				header:close()
-				
+
 				print(string.format("%s: %s (raw: %.3g kB, header: %.3g kB)", moduleName, file, contentLength/1024, string.format("%.3g", headerContent:len()/1024)))
 			end
 		end
 	end
+	print("Finished (took " .. os.clock() - startClock .. "s)")
 end
 
 newaction
