c42eff2db0cfcb4b9d3f9b34aa3f63e45fd1b485
Lynix <lynix680@gmail.com>
2014-02-18 01:01:30 +0100

    Added CallOnExit class
    Former-commit-id: d4b6bb17a8183f46e4631d727fc8056eb2fcb254

---
+++ include/Nazara/Core/CallOnExit.hpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2014 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_CALLONEXIT_HPP
+#define NAZARA_CALLONEXIT_HPP
+
+#include <Nazara/Prerequesites.hpp>
+
+class NzCallOnExit
+{
+	using Func = void (*)();
+
+	public:
+		NzCallOnExit(Func func = nullptr);
+		~NzCallOnExit();
+
+		void Reset(Func func = nullptr);
+
+	private:
+		Func m_func;
+};
+
+#include <Nazara/Core/CallOnExit.inl>
+
+#endif // NAZARA_CALLONEXIT_HPP
