5b523a4e02831abbba238ab164f313ac21da86bb
Jérôme Leclercq <lynix680@gmail.com>
2014-03-01 12:20:37 +0100

    Fixed compilation error

--- include/Nazara/Renderer.hpp
+++ include/Nazara/Renderer.hpp
@@ -53,7 +53,5 @@
 #include <Nazara/Renderer/UberShaderInstancePreprocessor.hpp>
 #include <Nazara/Renderer/UberShaderLibrary.hpp>
 #include <Nazara/Renderer/UberShaderPreprocessor.hpp>
-#include <Nazara/Renderer/UberShaderPUniform.hpp>
-#include <Nazara/Renderer/UberShaderPUSubroutine.hpp>
 
 #endif // NAZARA_GLOBAL_RENDERER_HPP
