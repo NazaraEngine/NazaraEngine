a2cc3e00c8eb5ac89d205f9785a9c376d6a67fb0
Jérôme Leclercq <lynix680@gmail.com>
2014-06-21 16:22:34 +0200

    Cached skinning matrix

--- src/Nazara/Utility/Joint.cpp
+++ src/Nazara/Utility/Joint.cpp
@@ -7,7 +7,8 @@
 #include <Nazara/Utility/Debug.hpp>
 
 NzJoint::NzJoint(NzSkeleton* skeleton) :
-m_skeleton(skeleton)
+m_skeleton(skeleton),
+m_skinningMatrixUpdated(false)
 {
 }
 
@@ -15,11 +16,18 @@ NzJoint::NzJoint(const NzJoint& joint) :
 NzNode(joint),
 m_inverseBindMatrix(joint.m_inverseBindMatrix),
 m_name(joint.m_name),
-m_skeleton(joint.m_skeleton)
+m_skeleton(joint.m_skeleton),
+m_skinningMatrixUpdated(false)
 {
 }
 
-NzMatrix4f NzJoint::GetInverseBindMatrix() const
+void NzJoint::EnsureSkinningMatrixUpdate() const
+{
+	if (!m_skinningMatrixUpdated)
+		UpdateSkinningMatrix();
+}
+
+const NzMatrix4f& NzJoint::GetInverseBindMatrix() const
 {
 	return m_inverseBindMatrix;
 }
@@ -39,9 +47,18 @@ const NzSkeleton* NzJoint::GetSkeleton() const
 	return m_skeleton;
 }
 
+const NzMatrix4f& NzJoint::GetSkinningMatrix() const
+{
+	if (!m_skinningMatrixUpdated)
+		UpdateSkinningMatrix();
+
+	return m_skinningMatrix;
+}
+
 void NzJoint::SetInverseBindMatrix(const NzMatrix4f& matrix)
 {
 	m_inverseBindMatrix = matrix;
+	m_skinningMatrixUpdated = false;
 }
 
 void NzJoint::SetName(const NzString& name)
@@ -49,4 +66,21 @@ void NzJoint::SetName(const NzString& name)
 	m_name = name;
 
 	m_skeleton->InvalidateJointMap();
+}
+
+void NzJoint::InvalidateNode()
+{
+	NzNode::InvalidateNode();
+
+	m_skinningMatrixUpdated = false;
+}
+
+void NzJoint::UpdateSkinningMatrix() const
+{
+	if (!m_transformMatrixUpdated)
+		UpdateTransformMatrix();
+
+	m_skinningMatrix.Set(m_inverseBindMatrix);
+	m_skinningMatrix.ConcatenateAffine(m_transformMatrix);
+	m_skinningMatrixUpdated = true;
 }
