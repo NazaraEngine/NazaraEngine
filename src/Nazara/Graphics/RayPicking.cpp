86fc724dfea259fc58dbe9f3a5e863995a79203d
Gawaboumga <yourihubaut@homail.com>
2014-08-05 15:53:51 +0200

    UTF-8

--- src/Nazara/Graphics/RayPicking.cpp
+++ src/Nazara/Graphics/RayPicking.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2014 Gawaboumga (Youri Hubaut) - Jérôme Leclercq
+// Copyright (C) 2014 Gawaboumga (Youri Hubaut) - JÃ©rÃ´me Leclercq
 // This file is part of the "Nazara Engine - Graphics module"
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
