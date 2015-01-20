934131ff40d7e19df9b50c6faedbc3cf52209c2f
Jérôme Leclercq <lynix680@gmail.com>
2015-01-20 23:37:49 +0100

    Added experimental new build system

--- build/scripts/module/utility.lua
+++ build/scripts/module/utility.lua
@@ -1,42 +1,22 @@
-if (not _OPTIONS["united"]) then
-	project "NazaraUtility"
-end
+MODULE.Name = "Utility"
 
-files
-{
-	"../include/Nazara/Utility/**.hpp",
-	"../include/Nazara/Utility/**.inl",
-	"../src/Nazara/Utility/**.hpp",
-	"../src/Nazara/Utility/**.cpp"
+MODULE.Libraries = {
+	"freetype-s",
+	"NazaraCore",
+	"stb_image"
 }
 
-links "freetype-s"
+MODULE.OsFiles.Windows = {
+	"../src/Nazara/Utility/Win32/**.hpp",
+	"../src/Nazara/Utility/Win32/**.cpp"
+}
 
-if (os.is("windows")) then
-	excludes { "../src/Nazara/Utility/Posix/*.hpp", "../src/Nazara/Utility/Posix/*.cpp" }
-	links "gdi32"
-else
-	excludes { "../src/Nazara/Utility/Win32/*.hpp", "../src/Nazara/Utility/Win32/*.cpp" }
-end
+MODULE.OsFiles.Posix = {
+	"../src/Nazara/Utility/Posix/**.hpp",
+	"../src/Nazara/Utility/Posix/**.cpp"
+}
 
-if (_OPTIONS["united"]) then
-	excludes "../src/Nazara/Utility/Debug/NewOverload.cpp"
-else
-	configuration "DebugStatic"
-		links "NazaraCore-s-d"
+MODULE.OsLibraries.Windows = {
+	"gdi32"
+}
 
-	configuration "ReleaseStatic"
-		links "NazaraCore-s"
-
-	configuration "DebugDLL"
-		links "NazaraCore-d"
-
-	configuration "ReleaseDLL"
-		links "NazaraCore"
-end
-
-configuration "Debug*"
-	links "stb_image-s-d"
-
-configuration "Release*"
-	links "stb_image-s"
