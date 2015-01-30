7c8a2e4f79c517145f527fef4db5df9f4341d814
Lynix <lynix680@gmail.com>
2015-01-30 22:33:41 +0100

    Regenerated global headers (and updated copyright year, again)
    Former-commit-id: db92457e50d5590d4f7ffea279d7286b2dc55d1e

--- include/Nazara/Renderer.hpp
+++ include/Nazara/Renderer.hpp
@@ -1,9 +1,9 @@
-// This file was automatically generated on 21 Feb 2014 at 18:17:18
+// This file was automatically generated on 30 Jan 2015 at 22:28:30
 
 /*
 	Nazara Engine - Renderer module
 
-	Copyright (C) 2014 Jérôme "Lynix" Leclercq (Lynix680@gmail.com)
+	Copyright (C) 2015 Jérôme "Lynix" Leclercq (Lynix680@gmail.com)
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy of
 	this software and associated documentation files (the "Software"), to deal in
