4efd73eff4e23963972cd4733eb5d48e22f8c25a
Jérôme Leclercq <lynix680@gmail.com>
2015-02-04 21:59:45 +0100

    Fixed potential name conflict with user's code

--- include/Nazara/Graphics/ConfigCheck.hpp
+++ include/Nazara/Graphics/ConfigCheck.hpp
@@ -10,7 +10,7 @@
 /// Ce fichier sert à vérifier la valeur des constantes du fichier Config.hpp
 
 #include <type_traits>
-#define CheckTypeAndVal(name, type, op, val, err) static_assert(std::is_ ##type <decltype(name)>::value && name op val, #type err)
+#define NazaraCheckTypeAndVal(name, type, op, val, err) static_assert(std::is_ ##type <decltype(name)>::value && name op val, #type err)
 
 // On force la valeur de MANAGE_MEMORY en mode debug
 #if defined(NAZARA_DEBUG) && !NAZARA_GRAPHICS_MANAGE_MEMORY
@@ -18,7 +18,9 @@
 	#define NAZARA_GRAPHICS_MANAGE_MEMORY 1
 #endif
 
-CheckTypeAndVal(NAZARA_GRAPHICS_INSTANCING_MIN_INSTANCES_COUNT, integral, >, 0, " shall be a strictly positive integer");
-CheckTypeAndVal(NAZARA_GRAPHICS_MAX_LIGHT_PER_PASS, integral, >, 0, " shall be a strictly positive integer");
+NazaraCheckTypeAndVal(NAZARA_GRAPHICS_INSTANCING_MIN_INSTANCES_COUNT, integral, >, 0, " shall be a strictly positive integer");
+NazaraCheckTypeAndVal(NAZARA_GRAPHICS_MAX_LIGHT_PER_PASS, integral, >, 0, " shall be a strictly positive integer");
+
+#undef NazaraCheckTypeAndVal
 
 #endif // NAZARA_CONFIG_CHECK_GRAPHICS_HPP
