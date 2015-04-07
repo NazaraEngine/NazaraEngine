df22c6f6769ffc25846b1b453a1a06b1d72133a0
Lynix <lynix680@gmail.com>
2015-04-07 17:41:17 +0200

    (NDK) Added Application class
    Let's try something
    
    
    Former-commit-id: addb5cb3861ffcd9f01d7a8a0d8d36ed3eca907a

---
+++ SDK/include/NDK/Application.hpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequesites.hpp
+
+#pragma once
+
+#ifndef NDK_APPLICATION_HPP
+#define NDK_APPLICATION_HPP
+
+#include <NDK/Prerequesites.hpp>
+
+namespace Ndk
+{
+	class NDK_API Application
+	{
+		public:
+			Application();
+			~Application();
+	};
+}
+
+#include <NDK/Application.inl>
+
+#endif // NDK_APPLICATION_HPP
