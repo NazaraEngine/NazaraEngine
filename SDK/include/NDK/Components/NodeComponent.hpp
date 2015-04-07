9919c6b7593f4b445c48d5f30ec5989ef26ecbd9
Lynix <lynix680@gmail.com>
2015-04-07 17:39:45 +0200

    (NDK) Added NodeComponent
    Former-commit-id: 9a0a4d9a7604e9ebb6d7d3aeb3d97c334b670e1b

---
+++ SDK/include/NDK/Components/NodeComponent.hpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequesites.hpp
+
+#pragma once
+
+#ifndef NDK_COMPONENTS_NODECOMPONENT_HPP
+#define NDK_COMPONENTS_NODECOMPONENT_HPP
+
+#include <Nazara/Utility/Node.hpp>
+#include <NDK/Component.hpp>
+
+namespace Ndk
+{
+	class Entity;
+
+	class NDK_API NodeComponent : public Component<NodeComponent>, public NzNode
+	{
+		public:
+			NodeComponent() = default;
+			~NodeComponent() = default;
+
+			void SetParent(Entity* entity, bool keepDerived = false);
+
+			static ComponentIndex componentIndex;
+	};
+}
+
+#include <NDK/Components/NodeComponent.inl>
+
+#endif // NDK_COMPONENTS_NODECOMPONENT_HPP
---
+++ SDK/include/NDK/Components/NodeComponent.inl
@@ -0,0 +1,21 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequesites.hpp
+
+#include <Nazara/Core/Error.hpp>
+#include <NDK/Entity.hpp>
+
+namespace Ndk
+{
+	inline void NodeComponent::SetParent(Entity* entity, bool keepDerived)
+	{
+		if (entity)
+		{
+			NazaraAssert(entity->HasComponent<NodeComponent>(), "Entity must have a NodeComponent");
+
+			NzNode::SetParent(entity->GetComponent<NodeComponent>(), keepDerived);
+		}
+		else
+			SetParent(nullptr, keepDerived);
+	}
+}
