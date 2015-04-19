5a146e588e1bcc4a653fec8a26c516a26f7a5a88
Lynix <lynix680@gmail.com>
2015-04-20 01:54:09 +0200

    Ndk/Components: Added OnComponent[Attached|Detacted] events
    And m_entity member variable
    
    
    Former-commit-id: ceabc55e435c41e485668bbc1c91b1d1bed6344e

--- SDK/src/NDK/BaseComponent.cpp
+++ SDK/src/NDK/BaseComponent.cpp
@@ -8,6 +8,16 @@ namespace Ndk
 {
 	BaseComponent::~BaseComponent() = default;
 
+	void BaseComponent::OnComponentAttached(BaseComponent& component)
+	{
+		NazaraUnused(component);
+	}
+
+	void BaseComponent::OnComponentDetached(BaseComponent& component)
+	{
+		NazaraUnused(component);
+	}
+
 	std::vector<BaseComponent::ComponentEntry> BaseComponent::s_entries;
 	std::unordered_map<ComponentId, ComponentIndex> BaseComponent::s_idToIndex;
 }
