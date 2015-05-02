5feccffaab097af352d981664fafe3296fad719f
Lynix <lynix680@gmail.com>
2015-05-02 10:00:07 +0200

    Ndk/Physics: Added first physics components/systems
    Former-commit-id: 6e1c25c7a7cabcd1d23adf84b808336659a3f55c

---
+++ SDK/include/NDK/Systems/PhysicsSystem.hpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequesites.hpp
+
+#pragma once
+
+#ifndef NDK_SYSTEMS_PHYSICSSYSTEM_HPP
+#define NDK_SYSTEMS_PHYSICSSYSTEM_HPP
+
+#include <Nazara/Physics/PhysWorld.hpp>
+#include <NDK/System.hpp>
+
+namespace Ndk
+{
+	class NDK_API PhysicsSystem : public System<PhysicsSystem>
+	{
+		public:
+			PhysicsSystem();
+			PhysicsSystem(const PhysicsSystem& system);
+			~PhysicsSystem() = default;
+
+			NzPhysWorld& GetWorld();
+			const NzPhysWorld& GetWorld() const;
+
+			void Update(float elapsedTime);
+
+			static SystemIndex systemIndex;
+
+		private:
+			NzPhysWorld m_world;
+	};
+}
+
+#include <NDK/Systems/PhysicsSystem.inl>
+
+#endif // NDK_SYSTEMS_PHYSICSSYSTEM_HPP
