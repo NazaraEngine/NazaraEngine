9395bb3994785a84276f77f1c25d698e2700af8b
Jérôme Leclercq <lynix680@gmail.com>
2015-05-19 01:03:11 +0200

    Ndk: Add RenderSystem (experimental)

---
+++ SDK/src/NDK/Systems/RenderSystem.cpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequesites.hpp
+
+#include <NDK/Systems/RenderSystem.hpp>
+#include <NDK/Components/CameraComponent.hpp>
+#include <NDK/Components/GraphicsComponent.hpp>
+
+namespace Ndk
+{
+	RenderSystem::RenderSystem()
+	{
+	}
+
+	void RenderSystem::Update(float elapsedTime)
+	{
+	}
+
+	void RenderSystem::OnEntityValidation(Entity* entity, bool justAdded)
+	{
+		if (entity->HasComponent<CameraComponent>())
+		{
+			m_cameras.Insert(entity);
+			std::sort(m_cameras.begin(), m_cameras.end(), [](const EntityHandle& handle1, const EntityHandle& handle2)
+			{
+				return handle1->GetComponent<CameraComponent>().GetLayer() < handle2->GetComponent<CameraComponent>().GetLayer();
+			});
+		}
+		else
+			m_cameras.Remove(entity);
+
+		if (entity->HasComponent<GraphicsComponent>())
+			m_drawables.Insert(entity);
+		else
+			m_drawables.Remove(entity);
+
+	}
+
+	SystemIndex RenderSystem::systemIndex;
+}
