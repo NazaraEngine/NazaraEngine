3c840c70c064eeec5b2f687dcca1a97ffce6a9e5
Lynix <lynix680@gmail.com>
2015-05-26 23:38:06 +0200

    Math/Matrix4: Add GetSquaredScale() method

--- include/Nazara/Math/Matrix4.hpp
+++ include/Nazara/Math/Matrix4.hpp
@@ -49,6 +49,7 @@ class NzMatrix4
 		//NzMatrix3 GetRotationMatrix() const;
 		NzVector4<T> GetRow(unsigned int row) const;
 		NzVector3<T> GetScale() const;
+		NzVector3<T> GetSquaredScale() const;
 		NzVector3<T> GetTranslation() const;
 		void GetTransposed(NzMatrix4* dest) const;
 
