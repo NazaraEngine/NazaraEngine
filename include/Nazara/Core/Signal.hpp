0f27930467377a6aad0009ff7b249082dcbf6aa6
Lynix <lynix680@gmail.com>
2015-06-04 15:20:16 +0200

    Core/Signal: Add Signals (WIP)
    Former-commit-id: eee55aa563f29c5604437f3c1a2b172af1a6410d

---
+++ include/Nazara/Core/Signal.hpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_SIGNAL_HPP
+#define NAZARA_SIGNAL_HPP
+
+#include <functional>
+#include <vector>
+
+template<typename... Args>
+class NzSignal
+{
+	public:
+		using Callback = std::function<void(Args...)>;
+
+		NzSignal() = default;
+		~NzSignal() = default;
+
+		void Connect(const Callback& func);
+
+		void operator()(Args&&... args);
+
+	private:
+		std::vector<Callback> m_callbacks;
+};
+
+#include <Nazara/Core/Signal.inl>
+
+#endif // NAZARA_SIGNAL_HPP
