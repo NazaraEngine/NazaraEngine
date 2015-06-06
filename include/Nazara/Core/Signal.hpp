34c22f525a4650e6bad421d1bf115792962cd339
Lynix <lynix680@gmail.com>
2015-06-07 00:51:43 +0200

    Core/Signal: Add Connection object and Disconnection
    Former-commit-id: 28f6ed4df8e85ade0be1f3e20fb8f426d2eeed3c

--- include/Nazara/Core/Signal.hpp
+++ include/Nazara/Core/Signal.hpp
@@ -8,26 +8,97 @@
 #define NAZARA_SIGNAL_HPP
 
 #include <functional>
-#include <vector>
+#include <memory>
+#include <list>
 
 template<typename... Args>
 class NzSignal
 {
 	public:
 		using Callback = std::function<void(Args...)>;
+		class Connection;
+		class ConnectionGuard;
 
 		NzSignal() = default;
+		NzSignal(const NzSignal&) = delete;
+		NzSignal(NzSignal&& signal);
 		~NzSignal() = default;
 
-		void Connect(const Callback& func);
-		void Connect(Callback&& func);
-		template<typename O> void Connect(O& object, void (O::*method)(Args...));
-		template<typename O> void Connect(O* object, void (O::*method)(Args...));
+		void Clear();
+
+		Connection&& Connect(const Callback& func);
+		Connection&& Connect(Callback&& func);
+		template<typename O> Connection&& Connect(O& object, void (O::*method)(Args...));
+		template<typename O> Connection&& Connect(O* object, void (O::*method)(Args...));
 
 		void operator()(Args&&... args);
 
+		NzSignal& operator=(const NzSignal&) = delete;
+		NzSignal& operator=(NzSignal&& signal);
+
 	private:
-		std::vector<Callback> m_callbacks;
+		struct Slot;
+
+		using SlotPtr = std::shared_ptr<Slot>;
+		using SlotList = std::list<SlotPtr>;
+
+		struct Slot
+		{
+			Slot(NzSignal* me) :
+			signal(me)
+			{
+			}
+
+			Callback callback;
+			NzSignal* signal;
+			typename SlotList::iterator it;
+		};
+
+		void Disconnect(const SlotPtr& slot);
+
+		SlotList m_slots;
+};
+
+template<typename... Args>
+class NzSignal<Args...>::Connection
+{
+	using BaseClass = NzSignal<Args...>;
+	friend BaseClass;
+
+	public:
+		Connection(const Connection& connection) = default;
+		Connection(Connection&& connection) = default;
+		~Connection() = default;
+
+		void Disconnect();
+
+		bool IsConnected() const;
+
+		Connection& operator=(const Connection& connection) = default;
+		Connection& operator=(Connection&& connection) = default;
+
+	private:
+		Connection(const SlotPtr& slot);
+
+		std::weak_ptr<Slot> m_ptr;
+};
+
+template<typename... Args>
+class NzSignal<Args...>::ConnectionGuard
+{
+	using BaseClass = NzSignal<Args...>;
+	using Connection = BaseClass::Connection;
+
+	public:
+		ConnectionGuard(const Connection& connection);
+		ConnectionGuard(Connection&& connection);
+		~ConnectionGuard();
+
+		Connection& operator=(const Connection& connection) = delete;
+		Connection& operator=(Connection&& connection) = delete;
+
+	private:
+		Connection m_connection;
 };
 
 #include <Nazara/Core/Signal.inl>
