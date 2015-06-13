ab4e851627a5caec904e93a77a48db1f311a3b66
Lynix <lynix680@gmail.com>
2015-06-13 23:28:01 +0200

    Build/Graphics: Fix conflict
    Former-commit-id: 2bb37286260f27886f7b40901cff770f8ff6a045

--- build/scripts/module/graphics.lua
+++ build/scripts/module/graphics.lua
@@ -1,18 +1,7 @@
 MODULE.Name = "Graphics"
 
-<<<<<<< HEAD
-defines "NAZARA_GRAPHICS_BUILD"
-
-files
-{
-	"../include/Nazara/Graphics/**.hpp",
-	"../include/Nazara/Graphics/**.inl",
-	"../src/Nazara/Graphics/**.hpp",
-	"../src/Nazara/Graphics/**.cpp"
-=======
 MODULE.Libraries = {
 	"NazaraCore",
 	"NazaraUtility",
 	"NazaraRenderer"
->>>>>>> origin/BuildSystem-Update
 }
