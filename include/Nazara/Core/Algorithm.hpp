3d9da38e450e7f5418b1855fee087800a6555e2c
Lynix <lynix680@gmail.com>
2015-09-11 14:09:59 +0200

    Core/Algorithm: Add Apply variant for methods
    Former-commit-id: 6e34c37c56d916c040ef501190f02f35e9c734df

--- include/Nazara/Core/Algorithm.hpp
+++ include/Nazara/Core/Algorithm.hpp
@@ -12,6 +12,7 @@
 #include <tuple>
 
 template<typename F, typename Tuple> auto NzApply(F&& fn, Tuple&& t);
+template<typename O, typename F, typename Tuple> auto NzApply(O& object, F&& fn, Tuple&& t);
 template<typename T> void NzHashCombine(std::size_t& seed, const T& v);
 
 template<typename T>
