dfa6f06337e982056b297adce5349f6c302c2103
Lynix <lynix680@gmail.com>
2015-11-03 22:34:46 +0100

    Core/Error: Make NazaraAssert blocklike (fixing integration)
    Former-commit-id: c6b7e10e30504ef14ef83d0a4d07f17adfd7b026

--- include/Nazara/Core/Error.hpp
+++ include/Nazara/Core/Error.hpp
@@ -14,9 +14,9 @@
 #include <Nazara/Core/String.hpp>
 
 #if NAZARA_CORE_ENABLE_ASSERTS || defined(NAZARA_DEBUG)
-	#define NazaraAssert(a, err) if (!(a)) Nz::Error::Trigger(Nz::ErrorType_AssertFailed, err, __LINE__, Nz::Directory::GetCurrentFileRelativeToEngine(__FILE__), NAZARA_FUNCTION)
+	#define NazaraAssert(a, err) do { if (!(a)) Nz::Error::Trigger(Nz::ErrorType_AssertFailed, err, __LINE__, Nz::Directory::GetCurrentFileRelativeToEngine(__FILE__), NAZARA_FUNCTION); } while (false)
 #else
-	#define NazaraAssert(a, err)
+	#define NazaraAssert(a, err) do {} while (false)
 #endif
 
 #define NazaraError(err) Nz::Error::Trigger(Nz::ErrorType_Normal, err, __LINE__, Nz::Directory::GetCurrentFileRelativeToEngine(__FILE__), NAZARA_FUNCTION)
