1c8a09f90c09bfa64ea6f50bf743a99204ec4e80
Lynix <lynix680@gmail.com>
2015-11-19 13:25:07 +0100

    Core: Replace serialization arguments by context structures
    Also fixed some endianness errors
    
    
    Former-commit-id: 450849e681a9b002c3d501a856a8481470d08dea

--- include/Nazara/Core/OutputStream.hpp
+++ include/Nazara/Core/OutputStream.hpp
@@ -8,6 +8,7 @@
 #define NAZARA_OUTPUTSTREAM_HPP
 
 #include <Nazara/Prerequesites.hpp>
+#include <Nazara/Core/Serialization.hpp>
 #include <Nazara/Core/Stream.hpp>
 
 namespace Nz
@@ -18,6 +19,8 @@ namespace Nz
 	class NAZARA_CORE_API OutputStream : virtual public Stream
 	{
 		public:
+			inline OutputStream(const OutputStream& stream);
+			inline OutputStream(OutputStream&& stream) noexcept;
 			virtual ~OutputStream();
 
 			virtual void Flush() = 0;
@@ -29,8 +32,13 @@ namespace Nz
 			template<typename T>
 			OutputStream& operator<<(const T& value);
 
+			inline OutputStream& operator=(const OutputStream& stream);
+			inline OutputStream& operator=(OutputStream&& stream) noexcept;
+
 		protected:
 			inline OutputStream();
+
+			SerializationContext m_serializationContext;
 	};
 }
 
