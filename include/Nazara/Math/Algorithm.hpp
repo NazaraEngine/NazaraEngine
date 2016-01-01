f2a64f82ba7d840c4b971bbf0fc97610db62ee1c
Lynix <lynix680@gmail.com>
2016-01-01 16:34:49 +0100

    Math/Algorithm: Fix compilation (C++14 constexpr is not supported by all major compilers)
    Former-commit-id: 3576f374e3f20f1dafd35695635905948e78740b

--- include/Nazara/Math/Algorithm.hpp
+++ include/Nazara/Math/Algorithm.hpp
@@ -30,30 +30,30 @@
 
 namespace Nz
 {
-	template<typename T> constexpr T Approach(T value, T objective, T increment);
+	template<typename T> /*constexpr*/ T Approach(T value, T objective, T increment);
 	template<typename T> constexpr T Clamp(T value, T min, T max);
-	template<typename T> constexpr T CountBits(T value);
+	template<typename T> /*constexpr*/ T CountBits(T value);
 	template<typename T> constexpr T FromDegrees(T degrees);
 	template<typename T> constexpr T FromRadians(T radians);
 	template<typename T> constexpr T DegreeToRadian(T degrees);
-	template<typename T> constexpr T GetNearestPowerOfTwo(T number);
-	constexpr unsigned int GetNumberLength(signed char number);
-	constexpr unsigned int GetNumberLength(unsigned char number);
+	template<typename T> /*constexpr*/ T GetNearestPowerOfTwo(T number);
+	/*constexpr*/ unsigned int GetNumberLength(signed char number);
+	/*constexpr*/ unsigned int GetNumberLength(unsigned char number);
 	unsigned int GetNumberLength(int number);
-	constexpr unsigned int GetNumberLength(unsigned int number);
+	/*constexpr*/ unsigned int GetNumberLength(unsigned int number);
 	unsigned int GetNumberLength(long long number);
-	constexpr unsigned int GetNumberLength(unsigned long long number);
+	/*constexpr*/ unsigned int GetNumberLength(unsigned long long number);
 	unsigned int GetNumberLength(float number, UInt8 precision = NAZARA_CORE_DECIMAL_DIGITS);
 	unsigned int GetNumberLength(double number, UInt8 precision = NAZARA_CORE_DECIMAL_DIGITS);
 	unsigned int GetNumberLength(long double number, UInt8 precision = NAZARA_CORE_DECIMAL_DIGITS);
-	template<typename T> constexpr unsigned int IntegralLog2(T number);
-	template<typename T> constexpr unsigned int IntegralLog2Pot(T pot);
-	constexpr unsigned int IntegralPow(unsigned int base, unsigned int exponent);
+	template<typename T> /*constexpr*/ unsigned int IntegralLog2(T number);
+	template<typename T> /*constexpr*/ unsigned int IntegralLog2Pot(T pot);
+	/*constexpr*/ unsigned int IntegralPow(unsigned int base, unsigned int exponent);
 	template<typename T, typename T2> constexpr T Lerp(const T& from, const T& to, const T2& interpolation);
 	template<typename T> constexpr T MultiplyAdd(T x, T y, T z);
-	template<typename T> constexpr T NormalizeAngle(T angle);
-	template<typename T> constexpr bool NumberEquals(T a, T b);
-	template<typename T> constexpr bool NumberEquals(T a, T b, T maxDifference);
+	template<typename T> /*constexpr*/ T NormalizeAngle(T angle);
+	template<typename T> /*constexpr*/ bool NumberEquals(T a, T b);
+	template<typename T> /*constexpr*/ bool NumberEquals(T a, T b, T maxDifference);
 	String NumberToString(long long number, UInt8 radix = 10);
 	template<typename T> constexpr T RadianToDegree(T radians);
 	long long StringToNumber(String str, UInt8 radix = 10, bool* ok = nullptr);
