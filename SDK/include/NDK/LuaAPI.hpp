3898421f2fb82cee8bfd1781162d6cd1c6d83fb7
Lynix <lynix680@gmail.com>
2016-01-20 14:08:43 +0100

    Sdk/API: Begin Lua Binding
    Former-commit-id: b9be6bf7748296c3afc6fe1f32a1fd3fad6af008

---
+++ SDK/include/NDK/LuaAPI.hpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequesites.hpp
+
+#pragma once
+
+#ifndef NDK_LUAINTERFACE_HPP
+#define NDK_LUAINTERFACE_HPP
+
+#include <NDK/Prerequesites.hpp>
+
+namespace Nz
+{
+	class LuaInstance;
+}
+
+namespace Ndk
+{
+	class NDK_API LuaAPI
+	{
+		public:
+			LuaAPI() = delete;
+			~LuaAPI() = delete;
+
+			static void Register_Audio(Nz::LuaInstance& instance);
+			static void Register_Core(Nz::LuaInstance& instance);
+			static void Register_Math(Nz::LuaInstance& instance);
+	};
+}
+
+#include <NDK/LuaAPI.inl>
+
+#endif // NDK_LUAINTERFACE_HPP
