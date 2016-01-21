04aac4363f0a0ebb1c4fc02c38fcd37421526c02
GigAnon <gig.anonymous@gmail.com>
2016-01-21 14:12:20 +0100

    Lua binding update
    - Added files & Register_X methods for modules Graphics, Renderer and
    Utility
    - Added Nz::AbstractImage binding (WIP)
    
    
    Former-commit-id: 91fd641c7676c12034e5f702bd3ba45e2a826263

---
+++ SDK/src/NDK/LuaAPI_Utility.cpp
@@ -0,0 +1,14 @@
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequesites.hpp
+
+
+#include <NDK/LuaAPI.hpp>
+#include <Nazara/Utility.hpp>
+#include <Nazara/Lua/LuaClass.hpp>
+
+namespace Ndk
+{
+	void LuaAPI::Register_Utility(Nz::LuaInstance& instance)
+	{
+	}
+}
