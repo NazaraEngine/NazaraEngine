58836c98061bfd506cb4061c2f46cec99e270cb8
Lynix <lynix680@gmail.com>
2016-01-24 15:20:14 +0100

    Fix and improve last PR

--- SDK/src/NDK/LuaAPI_Math.cpp
+++ SDK/src/NDK/LuaAPI_Math.cpp
@@ -11,115 +11,113 @@ namespace Ndk
 		/*********************************** Nz::Vector2 **********************************/
 		Nz::LuaClass<Nz::Vector2d> vector2dClass("Vector2");
 
-		vector2dClass.SetConstructor(
-			[](Nz::LuaInstance& lua) -> Nz::Vector2d* 
+		vector2dClass.SetConstructor([](Nz::LuaInstance& lua) -> Nz::Vector2d* 
+		{
+			unsigned int argCount = std::min(lua.GetStackTop(), 2U);
+			switch (argCount)
 			{
-				unsigned int argCount = std::min(lua.GetStackTop(), 2U);
-				switch (argCount)
+				case 0:
+				case 2:
+					return new Nz::Vector2d(lua.CheckNumber(1, 0.0), lua.CheckNumber(2, 0.0));
+
+				case 1:
 				{
-					case 0:
-					case 2:
-						return new Nz::Vector2d(lua.CheckNumber(1, 0.0), lua.CheckNumber(2, 0.0));
+					if (lua.IsOfType(1, Nz::LuaType_Number))
+						return new Nz::Vector2d(lua.CheckNumber(1));
+					else if (lua.IsOfType(1, "Vector2"))
+						return new Nz::Vector2d(*(*static_cast<Nz::Vector2d*>(lua.ToUserdata(1))));
 
-					case 1:
-					{
-						if (lua.IsOfType(1, Nz::LuaType_Number))
-							return new Nz::Vector2d(lua.CheckNumber(1));
-						else if (lua.IsOfType(1, "Vector2"))
-							return new Nz::Vector2d(*(*static_cast<Nz::Vector2d*>(lua.ToUserdata(1))));
-
-						break;
-					}
+					break;
 				}
+			}
 
-				lua.Error("No matching overload for Vector2 constructor");
-				return nullptr;
-			});
+			lua.Error("No matching overload for Vector2 constructor");
+			return nullptr;
+		});
 
 		vector2dClass.SetMethod("__tostring", &Nz::Vector2d::ToString);
 
-		vector2dClass.SetGetter(
-			[](Nz::LuaInstance& lua, Nz::Vector2d& instance)
+		vector2dClass.SetGetter([](Nz::LuaInstance& lua, Nz::Vector2d& instance)
+		{
+			switch (lua.GetType(1))
 			{
-				switch (lua.GetType(1))
+				case Nz::LuaType_Number:
+					lua.Push(instance[lua.CheckInteger(1)]);
+					return true;
+
+				case Nz::LuaType_String:
 				{
-					case Nz::LuaType_Number:
-						lua.Push(instance[lua.CheckInteger(1)]);
-						return true;
+					std::size_t length;
+					const char* xy = lua.CheckString(1, &length);
 
-					case Nz::LuaType_String:
-					{
-						std::size_t length;
-						const char* xy = lua.CheckString(1, &length);
-
-						if (length != 1)
-							break;
-
-						switch (xy[0])
-						{
-							case 'x':
-								lua.Push(instance.x);
-								return true;
-
-							case 'y':
-								lua.Push(instance.y);
-								return true;
-						}
+					if (length != 1)
 						break;
+
+					switch (xy[0])
+					{
+						case 'x':
+							lua.Push(instance.x);
+							return true;
+
+						case 'y':
+							lua.Push(instance.y);
+							return true;
 					}
+					break;
+				}
+			}
+
+			return false;
+		});
+
+		vector2dClass.SetSetter([](Nz::LuaInstance& lua, Nz::Vector2d& instance)
+		{
+			switch (lua.GetType(1))
+			{
+				case Nz::LuaType_Number:
+				{
+					long long index = lua.CheckInteger(1);
+					if (index < 1 || index > 2)
+						return false;
+
+					instance[index] = lua.CheckNumber(2);
+					return true;
 				}
 
-				return false;
-			});
-
-		vector2dClass.SetSetter(
-			[](Nz::LuaInstance& lua, Nz::Vector2d& instance)
-			{
-				switch (lua.GetType(1))
+				case Nz::LuaType_String:
 				{
-					case Nz::LuaType_Number:
-					{
-						long long index = lua.CheckInteger(1);
-						if (index < 1 || index > 2)
-							return false;
+					std::size_t length;
+					const char* xy = lua.CheckString(1, &length);
 
-						instance[index] = lua.CheckNumber(2);
-						return true;
-					}
-
-					case Nz::LuaType_String:
-					{
-						std::size_t length;
-						const char* xy = lua.CheckString(1, &length);
-
-						if (length != 1)
-							break;
-
-						double value = lua.CheckNumber(2);
-
-						switch (xy[0])
-						{
-							case 'x':
-								instance.x = value;
-								return true;
-
-							case 'y':
-								instance.y = value;
-								return true;
-						}
+					if (length != 1)
 						break;
-					}
-				}
 
-				return false;
-			});
+					double value = lua.CheckNumber(2);
+
+					switch (xy[0])
+					{
+						case 'x':
+							instance.x = value;
+							return true;
+
+						case 'y':
+							instance.y = value;
+							return true;
+					}
+					break;
+				}
+			}
+
+			return false;
+		});
 
 		vector2dClass.Register(instance);
 
 		/*********************************** Nz::Vector3 **********************************/
 		Nz::LuaClass<Nz::Vector3d> vector3dClass("Vector3");
 
-		vector3dClass.SetConstructor([] (Nz::LuaInstance& lua) -> Nz::Vector3d* {
+		vector3dClass.SetConstructor([] (Nz::LuaInstance& lua) -> Nz::Vector3d*
+		{
 			unsigned int argCount = std::min(lua.GetStackTop(), 3U);
 			switch (argCount)
 			{
