258868231d5673b7203d3ef647da0768f3ab9d0a
Gawaboumga <yourihubaut@hotmail.com>
2016-02-21 14:36:03 +0100

    Documentation for Synchronisation primitives
    Former-commit-id: 24b0fa7fd5e08e155476c9ffd5669759cd3ef167

--- src/Nazara/Core/ConditionVariable.cpp
+++ src/Nazara/Core/ConditionVariable.cpp
@@ -18,31 +18,83 @@
 
 namespace Nz
 {
+	/*!
+	* \class Nz::ConditionVariable
+	* \brief Core class that represents a condition variable
+	*
+	* The ConditionVariable class is a synchronization primitive that can be used to block a thread, or multiple threads at the same time, until another thread both modifies a shared variable (the condition), and notifies the ConditionVariable
+	*/
+
+	/*!
+	* \brief Constructs a ConditionVariable object by default
+	*/
+
 	ConditionVariable::ConditionVariable()
 	{
 		m_impl = new ConditionVariableImpl;
 	}
+
+	/*!
+	* \brief Destructs the object
+	*/
 
 	ConditionVariable::~ConditionVariable()
 	{
 		delete m_impl;
 	}
 
+	/*!
+	* \brief Sends a signal to one thread waiting on the condition
+	*
+	* If any threads are waiting on *this, calling Signal unblocks one of the waiting threads
+	*
+	* \see SignalAll
+	*/
+
 	void ConditionVariable::Signal()
 	{
 		m_impl->Signal();
 	}
+
+	/*!
+	* \brief Sends a signal to every threads waiting on the condition
+	*
+	* Unblocks all threads currently waiting for *this
+	*
+	* \see Signal
+	*/
 
 	void ConditionVariable::SignalAll()
 	{
 		m_impl->SignalAll();
 	}
 
+	/*!
+	* \brief Makes the thread wait on the condition
+	*
+	* Wait causes the current thread to block until the condition variable is notified or a spurious wakeup occurs
+	*
+	* \param mutex Mutex for the condition
+	*
+	* \remark Produces a NazaraAssert if mutex is invalid
+	*/
+
 	void ConditionVariable::Wait(Mutex* mutex)
 	{
 		NazaraAssert(mutex != nullptr, "Mutex must be valid");
 		m_impl->Wait(mutex->m_impl);
 	}
+
+	/*!
+	* \brief Makes the thread wait on the condition for a certain amount of time
+	*
+	* Wait causes the current thread to block until the condition variable is notified, a specific time is reached, or a spurious wakeup occurs
+	*
+	* \param mutex Mutex for the condition
+	* \param timeout Time before expiration of the waiting
+	*
+	* \remark Produces a NazaraAssert if mutex is invalid
+	*/
 
 	bool ConditionVariable::Wait(Mutex* mutex, UInt32 timeout)
 	{
