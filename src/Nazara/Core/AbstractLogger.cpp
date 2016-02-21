07547bdd9f59a70aee0ea4e03ab0fbe82c994cff
Gawaboumga <yourihubaut@hotmail.com>
2016-02-21 14:34:59 +0100

    Documentation for Logger
    Former-commit-id: b09dda8a542782667070ea8bf912b552dd6b478a

--- src/Nazara/Core/AbstractLogger.cpp
+++ src/Nazara/Core/AbstractLogger.cpp
@@ -11,16 +11,33 @@ namespace Nz
 	namespace
 	{
 		const char* errorType[] = {
-			"Assert failed: ",	// ErrorType_AssertFailed
-			"Internal error: ",	// ErrorType_Internal
-			"Error: ",			// ErrorType_Normal
-			"Warning: "			// ErrorType_Warning
+			"Assert failed: ",  // ErrorType_AssertFailed
+			"Internal error: ", // ErrorType_Internal
+			"Error: ",          // ErrorType_Normal
+			"Warning: "         // ErrorType_Warning
 		};
 
 		static_assert(sizeof(errorType) / sizeof(const char*) == ErrorType_Max + 1, "Error type array is incomplete");
 	}
 
+	/*!
+	* \class Nz::AbstractLogger<T>
+	* \brief Core class that represents the behaviour of the log classes
+	*
+	* \remark This class is abstract
+	*/
+
 	AbstractLogger::~AbstractLogger() = default;
+
+	/*!
+	* \brief Writes the error in StringStream
+	*
+	* \param type Enumeration of type ErrorType
+	* \param error String describing the error
+	* \param line Line number in the file
+	* \param file Filename
+	* \param function Name of the function throwing the error
+	*/
 
 	void AbstractLogger::WriteError(ErrorType type, const String& error, unsigned int line, const char* file, const char* function)
 	{
