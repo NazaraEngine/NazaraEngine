208d594e85222bcac26c5d9f87126555da0326db
Lynix <lynix680@gmail.com>
2016-03-03 14:10:52 +0100

    SDK: Add global headers (improved global headers script)

---
+++ SDK/include/NDK/Components.hpp
@@ -0,0 +1,17 @@
+// This file was automatically generated on 03 Mar 2016 at 14:09:12
+
+#pragma once
+
+#ifndef NDK_COMPONENTS_GLOBAL_HPP
+#define NDK_COMPONENTS_GLOBAL_HPP
+
+#include <NDK/Components/CameraComponent.hpp>
+#include <NDK/Components/CollisionComponent.hpp>
+#include <NDK/Components/GraphicsComponent.hpp>
+#include <NDK/Components/LightComponent.hpp>
+#include <NDK/Components/ListenerComponent.hpp>
+#include <NDK/Components/NodeComponent.hpp>
+#include <NDK/Components/PhysicsComponent.hpp>
+#include <NDK/Components/VelocityComponent.hpp>
+
+#endif // NDK_COMPONENTS_GLOBAL_HPP
