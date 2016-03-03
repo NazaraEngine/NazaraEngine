0a55276d13bbd406deb27c0040274dc0458b616a
Lynix <lynix680@gmail.com>
2016-03-03 14:10:52 +0100

    SDK: Add global headers (improved global headers script)
    Former-commit-id: 208d594e85222bcac26c5d9f87126555da0326db

---
+++ SDK/include/NDK/Systems.hpp
@@ -0,0 +1,13 @@
+// This file was automatically generated on 03 Mar 2016 at 14:09:12
+
+#pragma once
+
+#ifndef NDK_SYSTEMS_GLOBAL_HPP
+#define NDK_SYSTEMS_GLOBAL_HPP
+
+#include <NDK/Systems/ListenerSystem.hpp>
+#include <NDK/Systems/PhysicsSystem.hpp>
+#include <NDK/Systems/RenderSystem.hpp>
+#include <NDK/Systems/VelocitySystem.hpp>
+
+#endif // NDK_SYSTEMS_GLOBAL_HPP
