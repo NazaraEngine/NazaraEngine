883f2eca393e7d4ceeadf96f7009318f59a6cd8b
Lynix <lynix680@gmail.com>
2016-03-07 13:05:21 +0100

    Extlibs/STB: Update stb_image.h and add stb_image_write.h
    Former-commit-id: 1b567271240b0207780c2d516fad9e03575ce388

--- src/Nazara/Utility/Formats/STBLoader.cpp
+++ src/Nazara/Utility/Formats/STBLoader.cpp
@@ -3,7 +3,7 @@
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
 #include <Nazara/Utility/Formats/STBLoader.hpp>
-#include <stb_image/stb_image.h>
+#include <stb/stb_image.h>
 #include <Nazara/Core/Endianness.hpp>
 #include <Nazara/Core/Error.hpp>
 #include <Nazara/Core/File.hpp>
@@ -17,6 +17,12 @@ namespace Nz
 {
 	namespace
 	{
+		int Eof(void* userdata)
+		{
+			Stream* stream = static_cast<Stream*>(userdata);
+			return stream->GetCursorPos() >= stream->GetSize();
+		}
+
 		int Read(void* userdata, char* data, int size)
 		{
 			Stream* stream = static_cast<Stream*>(userdata);
@@ -27,12 +33,6 @@ namespace Nz
 		{
 			Stream* stream = static_cast<Stream*>(userdata);
 			stream->SetCursorPos(static_cast<Int64>(stream->GetCursorPos()) + static_cast<Int64>(size));
-		}
-
-		int Eof(void* userdata)
-		{
-			Stream* stream = static_cast<Stream*>(userdata);
-			return stream->GetCursorPos() >= stream->GetSize();
 		}
 
 		static stbi_io_callbacks callbacks = {Read, Skip, Eof};
