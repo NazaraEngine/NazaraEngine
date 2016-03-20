438c8fa8459041fc08b23ce0bd8f60ea9148f604
Ardakaniz <adrien@renaudineau.fr>
2016-03-20 16:26:59 +0100

    Changement de projet pour le concours

---
+++ examples/Tetris/build.lua
@@ -0,0 +1,15 @@
+EXAMPLE.Name = "Tetris"
+
+EXAMPLE.Console = true
+
+EXAMPLE.Files = {
+    "main.cpp"
+}
+
+EXAMPLE.Libraries = {
+    "NazaraCore",
+    "NazaraGraphics",
+    "NazaraRenderer",
+    "NazaraUtility",
+    "NazaraSDK"
+}
