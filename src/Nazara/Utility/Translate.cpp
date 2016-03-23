89d301f88f1b049698ceabaa59a46e7794832b5c
Ardakaniz <adrien@renaudineau.fr>
2016-03-23 14:46:57 +0100

    Utility/Translate: Add verifications

--- src/Nazara/Utility/Translate.cpp
+++ src/Nazara/Utility/Translate.cpp
@@ -11,8 +11,10 @@ namespace Nz
     {
         NazaraAssert(Nz::Translator::s_translatorInstance, "No Translator has been created");
 
-        auto& translations = Nz::Translator::s_translatorInstance->m_translations[Nz::Translator::s_translatorInstance->m_targetLang];
+        auto& translatorInstance = Nz::Translator::s_translatorInstance;
+        NazaraAssert(translatorInstance->m_translations.find(translatorInstance->m_targetLang) != std::end(translatorInstance->m_translations), "Translation in requested lang does not exist");
 
+        auto& translations = translatorInstance->m_translations[translatorInstance->m_targetLang];
         NazaraAssert(translations[toTranslate].is_string(), "Translations must be in string format");
 
         return translations[toTranslate];
