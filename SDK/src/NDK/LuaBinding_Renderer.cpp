b1fa937bf36db6ff474c5e123f5d42a532208efe
Lynix <lynix680@gmail.com>
2016-03-31 22:51:10 +0200

    Sdk: Remake LuaAPI internal
    Former-commit-id: 95b921282259e71256035b210ead542a911976ca

--- SDK/src/NDK/LuaBinding_Renderer.cpp
+++ SDK/src/NDK/LuaBinding_Renderer.cpp
@@ -2,14 +2,16 @@
 // This file is part of the "Nazara Development Kit"
 // For conditions of distribution and use, see copyright notice in Prerequesites.hpp
 
-
+#include <NDK/LuaBinding.hpp>
 #include <NDK/LuaAPI.hpp>
-#include <Nazara/Renderer.hpp>
-#include <Nazara/Lua/LuaClass.hpp>
 
 namespace Ndk
 {
-	void LuaAPI::Register_Renderer(Nz::LuaInstance& instance)
+	void LuaBinding::BindRenderer()
+	{
+	}
+
+	void LuaBinding::RegisterRenderer(Nz::LuaInstance& instance)
 	{
 	}
 }
