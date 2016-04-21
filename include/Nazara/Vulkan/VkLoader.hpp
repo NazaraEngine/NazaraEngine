386ae1c6e889ab7f58910f0445415a37494fe610
DPSLynix <lynix680@gmail.com>
2016-04-21 13:38:58 +0200

    Vulkan: Add empty module

---
+++ include/Nazara/Vulkan/VkLoader.hpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2016 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Vulkan"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_VULKAN_LOADER_HPP
+#define NAZARA_VULKAN_LOADER_HPP
+
+#include <Nazara/Prerequesites.hpp>
+#include <Nazara/Vulkan/Config.hpp>
+
+namespace Nz
+{
+	class NAZARA_VULKAN_API VkLoader
+	{
+	};
+}
+
+#endif // NAZARA_VULKAN_LOADER_HPP
