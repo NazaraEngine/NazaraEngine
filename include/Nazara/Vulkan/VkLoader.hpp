223e7525ff031a1be7f2490f7e86dc894769d944
DPSLynix <lynix680@gmail.com>
2016-04-21 17:27:16 +0200

    Extlibs: Add Vulkan headers

--- include/Nazara/Vulkan/VkLoader.hpp
+++ include/Nazara/Vulkan/VkLoader.hpp
@@ -8,13 +8,27 @@
 #define NAZARA_VULKAN_LOADER_HPP
 
 #include <Nazara/Prerequesites.hpp>
+#include <Nazara/Core/DynLib.hpp>
 #include <Nazara/Vulkan/Config.hpp>
+#include <vulkan/vulkan.h>
 
 namespace Nz
 {
 	class NAZARA_VULKAN_API VkLoader
 	{
+		public:
+			VkLoader() = delete;
+			~VkLoader() = delete;
+
+			static bool Initialize();
+
+		private:
+			static DynLib s_vulkanLib;
 	};
 }
+
+#define NAZARA_VULKAN_EXPORTED_FUNCTION(func) extern PFN_##func func;
+
+NAZARA_VULKAN_EXPORTED_FUNCTION(vkGetInstanceProcAddr);
 
 #endif // NAZARA_VULKAN_LOADER_HPP
