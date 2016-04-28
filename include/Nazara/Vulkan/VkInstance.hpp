e1d424e5f90967c62f1ed62913f7941a06093af7
Lynix <lynix680@gmail.com>
2016-04-28 20:37:30 +0200

    Vulkan: Add support for extensions
    Former-commit-id: ab5742edc79815dd75473bf467c7b1dd46e2fecb

--- include/Nazara/Vulkan/VkInstance.hpp
+++ include/Nazara/Vulkan/VkInstance.hpp
@@ -8,9 +8,11 @@
 #define NAZARA_VULKAN_VKINSTANCE_HPP
 
 #include <Nazara/Prerequesites.hpp>
+#include <Nazara/Core/String.hpp>
 #include <Nazara/Vulkan/Config.hpp>
 #include <Nazara/Vulkan/VkLoader.hpp>
 #include <vulkan/vulkan.h>
+#include <unordered_set>
 
 namespace Nz 
 {
@@ -25,6 +27,7 @@ namespace Nz
 				inline ~Instance();
 
 				bool Create(const VkInstanceCreateInfo& createInfo, const VkAllocationCallbacks* allocator = nullptr);
+				inline bool Create(const String& appName, UInt32 appVersion, const String& engineName, UInt32 engineVersion, const std::vector<const char*>& layers, const std::vector<const char*>& extensions, const VkAllocationCallbacks* allocator = nullptr);
 				inline void Destroy();
 
 				bool EnumeratePhysicalDevices(std::vector<VkPhysicalDevice>* physicalDevices);
@@ -40,12 +43,18 @@ namespace Nz
 
 				inline VkResult GetLastErrorCode() const;
 
+				inline bool IsExtensionLoaded(const String& extensionName);
+				inline bool IsLayerLoaded(const String& layerName);
+
 				Instance& operator=(const Instance&) = delete;
 				Instance& operator=(Instance&&) = delete;
+
+				inline operator VkInstance();
 
 				// Vulkan functions
 				#define NAZARA_VULKAN_INSTANCE_FUNCTION(func) PFN_##func func
 
+				// Vulkan core
 				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateDevice);
 				NAZARA_VULKAN_INSTANCE_FUNCTION(vkDestroyInstance);
 				NAZARA_VULKAN_INSTANCE_FUNCTION(vkEnumeratePhysicalDevices);
@@ -57,6 +66,72 @@ namespace Nz
 				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceProperties);
 				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceQueueFamilyProperties);
 
+				// VK_KHR_display
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateDisplayModeKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateDisplayPlaneSurfaceKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetDisplayModePropertiesKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetDisplayPlaneCapabilitiesKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetDisplayPlaneSupportedDisplaysKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceDisplayPlanePropertiesKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceDisplayPropertiesKHR);
+
+				// VK_KHR_display_swapchain
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateSharedSwapchainsKHR);
+
+				// VK_KHR_surface
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkDestroySurfaceKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceSurfaceCapabilitiesKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceSurfaceFormatsKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceSurfacePresentModesKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceSurfaceSupportKHR);
+
+				// VK_KHR_swapchain
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkAcquireNextImageKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateSwapchainKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkDestroySwapchainKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetSwapchainImagesKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkQueuePresentKHR);
+
+				// VK_EXT_debug_report
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateDebugReportCallbackEXT);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkDestroyDebugReportCallbackEXT);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkDebugReportMessageEXT);
+
+				#ifdef VK_USE_PLATFORM_ANDROID_KHR
+				// VK_KHR_android_surface
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateAndroidSurfaceKHR);
+				#endif
+
+				#ifdef VK_USE_PLATFORM_MIR_KHR
+				// VK_KHR_mir_surface
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateMirSurfaceKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceMirPresentationSupportKHR);
+				#endif
+
+				#ifdef VK_USE_PLATFORM_XCB_KHR
+				// VK_KHR_xcb_surface
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateXcbSurfaceKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceXcbPresentationSupportKHR);
+				#endif
+
+				#ifdef VK_USE_PLATFORM_XLIB_KHR
+				// VK_KHR_xlib_surface
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateXlibSurfaceKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceXlibPresentationSupportKHR);
+				#endif
+
+				#ifdef VK_USE_PLATFORM_WAYLAND_KHR
+				// VK_KHR_wayland_surface
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateWaylandSurfaceKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceWaylandPresentationSupportKHR);
+				#endif
+
+				#ifdef VK_USE_PLATFORM_WIN32_KHR
+				// VK_KHR_win32_surface
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkCreateWin32SurfaceKHR);
+				NAZARA_VULKAN_INSTANCE_FUNCTION(vkGetPhysicalDeviceWin32PresentationSupportKHR);
+				#endif
+
 				#undef NAZARA_VULKAN_INSTANCE_FUNCTION
 
 			private:
@@ -65,6 +140,8 @@ namespace Nz
 				VkAllocationCallbacks m_allocator;
 				VkInstance m_instance;
 				VkResult m_lastErrorCode;
+				std::unordered_set<String> m_loadedExtensions;
+				std::unordered_set<String> m_loadedLayers;
 		};
 	}
 }
