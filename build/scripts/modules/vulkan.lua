ed8828bfe2a13fa503311a16d687ec48f9469452
Jérôme Leclercq <lynix680@gmail.com>
2016-04-29 09:02:24 +0200

    Build/Vulkan: Add Win32 define

--- build/scripts/modules/vulkan.lua
+++ build/scripts/modules/vulkan.lua
@@ -9,6 +9,10 @@ MODULE.Libraries = {
 	"NazaraUtility"
 }
 
+MODULE.OsDefines.Windows = {
+	"VK_USE_PLATFORM_WIN32_KHR"
+}
+
 MODULE.OsFiles.Windows = {
 	"../src/Nazara/Vulkan/Win32/**.hpp",
 	"../src/Nazara/Vulkan/Win32/**.cpp"
