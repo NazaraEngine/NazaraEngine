a7d16f0d57e81af2d2d54a7c067f2dd0b881d03a
DPSLynix <lynix680@gmail.com>
2016-04-29 14:20:22 +0200

    Core/File: Add SetSize method (currently missing Posix implementation!)

--- src/Nazara/Core/Win32/FileImpl.cpp
+++ src/Nazara/Core/Win32/FileImpl.cpp
@@ -3,6 +3,7 @@
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
 #include <Nazara/Core/Win32/FileImpl.hpp>
+#include <Nazara/Core/CallOnExit.hpp>
 #include <Nazara/Core/Error.hpp>
 #include <Nazara/Core/Win32/Time.hpp>
 #include <memory>
@@ -156,6 +157,31 @@ namespace Nz
 		m_endOfFileUpdated = false;
 
 		return SetFilePointerEx(m_handle, distance, nullptr, moveMethod) != 0;
+	}
+
+	bool FileImpl::SetSize(UInt64 size)
+	{
+		UInt64 cursorPos = GetCursorPos();
+
+		CallOnExit resetCursor([this, cursorPos] ()
+		{
+			if (!SetCursorPos(CursorPosition_AtBegin, cursorPos))
+				NazaraWarning("Failed to reset cursor position to previous position: " + Error::GetLastSystemError());
+		});
+
+		if (!SetCursorPos(CursorPosition_AtBegin, size))
+		{
+			NazaraError("Failed to set file size: failed to move cursor position: " + Error::GetLastSystemError());
+			return false;
+		}
+
+		if (!SetEndOfFile(m_handle))
+		{
+			NazaraError("Failed to set file size: " + Error::GetLastSystemError());
+			return false;
+		}
+
+		return true;
 	}
 
 	std::size_t FileImpl::Write(const void* buffer, std::size_t size)
