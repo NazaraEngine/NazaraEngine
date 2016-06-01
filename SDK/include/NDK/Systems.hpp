180c6366aa110ec3efc8f46316bb13970fccde72
Lynix <lynix680@gmail.com>
2016-06-01 13:51:32 +0200

    Build: Add server option, allows to compile only the server-side part of Nazara

--- SDK/include/NDK/Systems.hpp
+++ SDK/include/NDK/Systems.hpp
@@ -1,16 +1,13 @@
-// This file was automatically generated on 03 Mar 2016 at 14:09:12
+// This file was automatically generated on 01 Jun 2016 at 13:11:09
 
 #pragma once
 
 #ifndef NDK_SYSTEMS_GLOBAL_HPP
 #define NDK_SYSTEMS_GLOBAL_HPP
 
-#include <NDK/Systems/PhysicsSystem.hpp>
-#include <NDK/Systems/VelocitySystem.hpp>
-
-#ifndef NDK_SERVER
 #include <NDK/Systems/ListenerSystem.hpp>
+#include <NDK/Systems/PhysicsSystem.hpp>
 #include <NDK/Systems/RenderSystem.hpp>
-#endif
+#include <NDK/Systems/VelocitySystem.hpp>
 
 #endif // NDK_SYSTEMS_GLOBAL_HPP
