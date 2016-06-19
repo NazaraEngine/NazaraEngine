68af748e972afe0e6e6a2d82c4dce045045942be
Lynix <lynix680@gmail.com>
2016-06-19 16:09:22 +0200

    Noise: Fix errors/warnings
    Former-commit-id: 37e946067a3324f102aabcad1f43bfdd775841a5 [formerly 04d3406dba7c84118888ba2cfdbf364f93860567]
    Former-commit-id: 53316c61e484c1968ddcfb7c75acc57956a054f4

--- src/Nazara/Noise/NoiseBase.cpp
+++ src/Nazara/Noise/NoiseBase.cpp
@@ -3,6 +3,7 @@
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
 #include <Nazara/Noise/NoiseBase.hpp>
+#include <numeric>
 #include <Nazara/Noise/Debug.hpp>
 
 namespace Nz
