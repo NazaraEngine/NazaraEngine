d99c6f00be5d030ca65ce67a1d93022c9fc94bef
Lynix <lynix680@gmail.com>
2016-07-07 14:09:16 +0200

    Vulkan/CommandBuffer: Add some rendering wrappers
    Former-commit-id: 762ac8aba9d0998aa5e44f8df7832258d743a95f [formerly 30a7cabf1689a0ad96cef2c4c6d632c1118bd99c]
    Former-commit-id: a25c64c88b0812a4f5d6f641ba309a642c3ce2be

--- include/Nazara/Vulkan/VkCommandBuffer.hpp
+++ include/Nazara/Vulkan/VkCommandBuffer.hpp
@@ -8,6 +8,7 @@
 #define NAZARA_VULKAN_VKCOMMANDBUFFER_HPP
 
 #include <Nazara/Prerequesites.hpp>
+#include <Nazara/Math/Rect.hpp>
 #include <Nazara/Vulkan/VkCommandPool.hpp>
 #include <vulkan/vulkan.h>
 
@@ -31,13 +32,28 @@ namespace Nz
 				inline bool Begin(VkCommandBufferUsageFlags flags, VkRenderPass renderPass, UInt32 subpass, VkFramebuffer framebuffer, bool occlusionQueryEnable, VkQueryControlFlags queryFlags, VkQueryPipelineStatisticFlags pipelineStatistics);
 				inline bool Begin(VkCommandBufferUsageFlags flags, bool occlusionQueryEnable, VkQueryControlFlags queryFlags, VkQueryPipelineStatisticFlags pipelineStatistics);
 
+				inline void BeginRenderPass(const VkRenderPassBeginInfo& beginInfo, VkSubpassContents contents = VK_SUBPASS_CONTENTS_INLINE);
+
+				inline void BindPipeline(VkPipelineBindPoint pipelineBindPoint, VkPipeline pipeline);
+
+				inline void Draw(UInt32 vertexCount, UInt32 instanceCount = 1, UInt32 firstVertex = 0, UInt32 firstInstance = 0);
+
 				inline bool End();
+
+				inline void EndRenderPass();
 
 				inline void Free();
 
 				inline void PipelineBarrier(VkPipelineStageFlags srcStageMask, VkPipelineStageFlags dstStageMask, VkDependencyFlags dependencyFlags, const VkImageMemoryBarrier& imageMemoryBarrier);
 				inline void PipelineBarrier(VkPipelineStageFlags srcStageMask, VkPipelineStageFlags dstStageMask, VkDependencyFlags dependencyFlags, const VkMemoryBarrier& memoryBarrier, const VkBufferMemoryBarrier& bufferMemoryBarrier, const VkImageMemoryBarrier& imageMemoryBarrier);
 				inline void PipelineBarrier(VkPipelineStageFlags srcStageMask, VkPipelineStageFlags dstStageMask, VkDependencyFlags dependencyFlags, UInt32 memoryBarrierCount, const VkMemoryBarrier* memoryBarriers, UInt32 bufferMemoryBarrierCount, const VkBufferMemoryBarrier* bufferMemoryBarriers, UInt32 imageMemoryBarrierCount, const VkImageMemoryBarrier* imageMemoryBarriers);
+
+				inline void SetScissor(const Recti& scissorRegion);
+				inline void SetScissor(const VkRect2D& scissorRegion);
+				inline void SetScissor(UInt32 firstScissor, UInt32 scissorCount, const VkRect2D* scissors);
+				inline void SetViewport(const Rectf& viewport, float minDepth, float maxDepth);
+				inline void SetViewport(const VkViewport& viewport);
+				inline void SetViewport(UInt32 firstViewport, UInt32 viewportCount, const VkViewport* viewports);
 
 				inline VkResult GetLastErrorCode() const;
 
