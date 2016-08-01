afe682d79981b3a6adf81bb7be8cc977d8dba557
Lynix <lynix680@gmail.com>
2016-08-01 12:04:15 +0200

    SDK/ParticleEmitterComponent: Fix missing include
    Former-commit-id: acd40e087c5626ed81b4beeae2478defadcf07fc [formerly 6cfe657fd9e154e8f77db4ffa206a8ab23e4d5c1]
    Former-commit-id: da09547499f6f12665937b7e1d7488ddc75eb3cd

--- SDK/include/NDK/Components/ParticleEmitterComponent.hpp
+++ SDK/include/NDK/Components/ParticleEmitterComponent.hpp
@@ -9,6 +9,7 @@
 #define NDK_COMPONENTS_PARTICLEEMITTERCOMPONENT_HPP
 
 #include <Nazara/Graphics/ParticleEmitter.hpp>
+#include <Nazara/Graphics/ParticleGroup.hpp>
 #include <NDK/Component.hpp>
 
 namespace Ndk
