7f707e12508d274e6054115f3389a842b982d62c
Lynix <lynix680@gmail.com>
2016-08-06 01:09:13 +0200

    Sdk/ParticleEmitterComponent: Update emitter (they are no longer attached to groups)
    They are no longer attached to groups, groups attach emitters
    
    
    Former-commit-id: fa4c2eb69147c7a19e9d77f952fdeaec0fdafc24 [formerly 981a7cfe37867842b4fe275cae6b04ab9f8a33c9] [formerly bc3c128bdfdb36eaf09f8f8a650c07e00d7e877c [formerly 1dc92772e589d80f59e8dab5d315387c7c57f886]]
    Former-commit-id: 6420a724b368ab2e72e67154e76566c7eda9f7a5 [formerly b3912e18e589bff75f8f9a1870fbebffe208f653]
    Former-commit-id: 1598b8cfb892285daee6dac7cd3283ba8447a032

--- SDK/src/NDK/Components/ParticleEmitterComponent.cpp
+++ SDK/src/NDK/Components/ParticleEmitterComponent.cpp
@@ -7,51 +7,9 @@
 
 namespace Ndk
 {
-	ParticleEmitterComponent::ParticleEmitterComponent(Nz::ParticleGroup* group) :
-	m_particleGroup(group),
-	m_isActive(true)
+	void ParticleEmitterComponent::SetupParticles(Nz::ParticleMapper& mapper, unsigned int count) const
 	{
-		if (m_particleGroup)
-			m_particleGroup->AddEmitter(this);
-	}
-
-	ParticleEmitterComponent::ParticleEmitterComponent(const ParticleEmitterComponent& emitter) :
-	m_particleGroup(emitter.m_particleGroup),
-	m_isActive(emitter.m_isActive)
-	{
-		if (m_isActive)
-			m_particleGroup->AddEmitter(this);
-	}
-
-	ParticleEmitterComponent::~ParticleEmitterComponent()
-	{
-		m_particleGroup->RemoveEmitter(this);
-	}
-
-	inline void Ndk::ParticleEmitterComponent::SetActive(bool active)
-	{
-		if (m_isActive != active)
-		{
-			if (active)
-				m_particleGroup->AddEmitter(this);
-			else
-				m_particleGroup->RemoveEmitter(this);
-		}
-	}
-
-	void ParticleEmitterComponent::SetGroup(Nz::ParticleGroup* group)
-	{
-		if (m_particleGroup)
-			m_particleGroup->RemoveEmitter(this);
-
-		m_particleGroup = group;
-		if (m_particleGroup && m_isActive)
-			m_particleGroup = group;
-	}
-
-	inline void ParticleEmitterComponent::SetupParticles(Nz::ParticleMapper& mapper, unsigned int count) const
-	{
-		if (m_setupFunc)
+		if (m_isActive && m_setupFunc)
 			m_setupFunc(m_entity, mapper, count);
 	}
 
