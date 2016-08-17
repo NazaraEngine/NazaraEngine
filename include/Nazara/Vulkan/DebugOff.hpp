14bc38e3f830a63804ea8a182e0bddd54c386ded
Lynix <lynix680@gmail.com>
2016-08-17 13:00:03 +0200

    Remove Vulkan from master branch
    Former-commit-id: 4bc44622fc88c08e4718484ee74128a4a8a326e0 [formerly 6ecba7b84dc522ace503113d60a54d3e30edb058] [formerly d80ca9f0e519522d7e0dc9ccb0e4b0a4aa1a13ab [formerly abef6064b63ddae172eb7e9c09bab1b7faf5c399]]
    Former-commit-id: f94d82bcc372d4f19f51280c25d2e98f99bfdbcf [formerly f1953666e292997824ce70da96758e3b67d4ac37]
    Former-commit-id: 1148ef1a828bb3599f41a432de67148fd4ef98f8

--- include/Nazara/Vulkan/DebugOff.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2016 Jérôme Leclercq
-// This file is part of the "Nazara Engine - Vulkan"
-// For conditions of distribution and use, see copyright notice in Config.hpp
-
-// On suppose que Debug.hpp a déjà été inclus, tout comme Config.hpp
-#if NAZARA_MODULENAME_MANAGE_MEMORY
-	#undef delete
-	#undef new
-#endif
