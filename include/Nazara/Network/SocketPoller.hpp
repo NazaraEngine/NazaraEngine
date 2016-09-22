a1597f6b30c27cf63d7aa2a941f64f316185f679
Lynix <lynix680@gmail.com>
2016-09-22 18:16:39 +0200

    Network: Add SocketPoller class
    Former-commit-id: 86b9266d904551d19e7ec8e8d6bbe5f137e8d29f [formerly 34575937d84f796cfe36a5ab97ea1f787c1506ba] [formerly 6c8c621523800958ad38eef118fcb3687c34b367 [formerly 7a3948dd21452544d30a73495269128fe2b6e4b8]]
    Former-commit-id: 6635c463a753f744b835267181b51ed89620b627 [formerly 14e84c7155cd669ac20a5492e67704059a442925]
    Former-commit-id: 7becccdf2d8aac7cdf526d855215d7d144be284c

---
+++ include/Nazara/Network/SocketPoller.hpp
@@ -0,0 +1,44 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Network module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_SOCKETPOLLER_HPP
+#define NAZARA_SOCKETPOLLER_HPP
+
+#include <Nazara/Prerequesites.hpp>
+#include <Nazara/Network/AbstractSocket.hpp>
+#include <Nazara/Network/IpAddress.hpp>
+
+namespace Nz
+{
+	class SocketPollerImpl;
+
+	class NAZARA_NETWORK_API SocketPoller
+	{
+		public:
+			SocketPoller();
+			inline SocketPoller(SocketPoller&& socketPoller);
+			~SocketPoller() = default;
+
+			void Clear();
+
+			bool IsReady(const AbstractSocket& socket) const;
+			bool IsRegistered(const AbstractSocket& socket) const;
+
+			bool RegisterSocket(AbstractSocket& socket);
+			void UnregisterSocket(AbstractSocket& socket);
+
+			bool Wait(UInt64 msTimeout);
+
+			inline SocketPoller& operator=(SocketPoller&& socketPoller);
+
+		private:
+			SocketPollerImpl* m_impl;
+	};
+}
+
+#include <Nazara/Network/SocketPoller.inl>
+
+#endif // NAZARA_SOCKETPOLLER_HPP
---
+++ src/Nazara/Network/Win32/SocketPollerImpl.cpp
@@ -0,0 +1,148 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Network module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/Network/Win32/SocketPollerImpl.hpp>
+#include <Nazara/Network/Debug.hpp>
+
+namespace Nz
+{
+	SocketPollerImpl::SocketPollerImpl()
+	{
+		#if !NAZARA_NETWORK_POLL_SUPPORT
+		FD_ZERO(&m_activeSockets);
+		FD_ZERO(&m_sockets);
+		#endif
+	}
+
+	void SocketPollerImpl::Clear()
+	{
+		#if NAZARA_NETWORK_POLL_SUPPORT
+		m_activeSockets.clear();
+		m_allSockets.clear();
+		m_sockets.clear();
+		#else
+		FD_ZERO(&m_activeSockets);
+		FD_ZERO(&m_sockets);
+		#endif
+	}
+
+	bool SocketPollerImpl::IsReady(SocketHandle socket) const
+	{
+		#if NAZARA_NETWORK_POLL_SUPPORT
+		return m_activeSockets.count(socket) != 0;
+		#else
+		return FD_ISSET(socket, &m_activeSockets) != 0;
+		#endif
+	}
+
+	bool SocketPollerImpl::IsRegistered(SocketHandle socket) const
+	{
+		#if NAZARA_NETWORK_POLL_SUPPORT
+		return m_allSockets.count(socket) != 0;
+		#else
+		return FD_ISSET(socket, &m_sockets) != 0;
+		#endif
+	}
+
+	bool SocketPollerImpl::RegisterSocket(SocketHandle socket)
+	{
+		NazaraAssert(!IsRegistered(socket), "Socket is already registered");
+
+		#if NAZARA_NETWORK_POLL_SUPPORT
+		PollSocket entry = {
+			socket,
+			POLLRDNORM,
+			0
+		};
+
+		m_allSockets[socket] = m_sockets.size();
+		m_sockets.emplace_back(entry);
+		#else
+		if (m_sockets.fd_count > FD_SETSIZE)
+		{
+			NazaraError("Socket count exceeding FD_SETSIZE (" + String::Number(FD_SETSIZE) + ")");
+			return false;
+		}
+
+		FD_SET(socket, &m_sockets);
+		#endif
+
+		return true;
+	}
+
+	void SocketPollerImpl::UnregisterSocket(SocketHandle socket)
+	{
+		NazaraAssert(IsRegistered(socket), "Socket is not registered");
+
+		#if NAZARA_NETWORK_POLL_SUPPORT
+		if (m_sockets.size() > 1U)
+		{
+			// Instead of using vector::erase, let's move the last element to the now unoccupied position
+			std::size_t entry = m_allSockets[socket];
+
+			// Get the last element and update it's position
+			const PollSocket& lastElement = m_sockets.back();
+			m_allSockets[lastElement.fd] = entry;
+
+			// Now move it properly (lastElement is invalid after the following line) and pop it
+			m_sockets[entry] = std::move(m_sockets.back());
+		}
+
+		m_sockets.pop_back();
+		m_activeSockets.erase(socket);
+		m_allSockets.erase(socket);
+		#else
+		FD_CLR(socket, &m_activeSockets);
+		FD_CLR(socket, &m_sockets);
+		#endif
+	}
+
+	int SocketPollerImpl::Wait(UInt64 msTimeout, SocketError* error)
+	{
+		int activeSockets;
+
+		#if NAZARA_NETWORK_POLL_SUPPORT
+		// Reset status of sockets
+		for (PollSocket& entry : m_sockets)
+			entry.revents = 0;
+
+		activeSockets = SocketImpl::Poll(m_sockets.data(), m_sockets.size(), static_cast<int>(msTimeout), error);
+
+		m_activeSockets.clear();
+		if (activeSockets > 0U)
+		{
+			for (PollSocket& entry : m_sockets)
+			{
+				if (entry.revents & POLLRDNORM)
+				{
+					m_activeSockets.insert(entry.fd);
+					if (--activeSockets == 0)
+						break;
+				}
+			}
+		}
+		#else
+
+		m_activeSockets = m_sockets;
+
+		timeval tv;
+		tv.tv_sec = static_cast<long>(msTimeout / 1000ULL);
+		tv.tv_usec = static_cast<long>((msTimeout % 1000ULL) * 1000ULL);
+
+		activeSockets = ::select(0xDEADBEEF, &m_activeSockets, nullptr, nullptr, (msTimeout > 0) ? &tv : nullptr); //< The first argument is ignored on Windows
+		if (activeSockets == SOCKET_ERROR)
+		{
+			if (error)
+				*error = SocketImpl::TranslateWSAErrorToSocketError(WSAGetLastError());
+
+			return 0;
+		}
+
+		if (error)
+			*error = SocketError_NoError;
+		#endif
+
+		return activeSockets;
+	}
+}
