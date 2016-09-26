35b16d23aac751387825256598c7519e39129c1f
Lynix <lynix680@gmail.com>
2016-09-26 18:34:06 +0200

    Network/SocketPoller: Switch to epoll implementation on Linux

--- build/scripts/modules/network.lua
+++ build/scripts/modules/network.lua
@@ -14,6 +14,11 @@ MODULE.OsFiles.Posix = {
 	"../src/Nazara/Network/Posix/**.cpp"
 }
 
+MODULE.OsFiles.Linux = {
+	"../src/Nazara/Network/Linux/**.hpp",
+	"../src/Nazara/Network/Linux/**.cpp"
+}
+
 MODULE.OsLibraries.Windows = {
 	"ws2_32"
 }
---
+++ src/Nazara/Network/Linux/SocketPollerImpl.cpp
@@ -0,0 +1,99 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Network module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#include <Nazara/Network/Linux/SocketPollerImpl.hpp>
+#include <cstring>
+#include <Nazara/Network/Debug.hpp>
+
+namespace Nz
+{
+	SocketPollerImpl::SocketPollerImpl()
+	{
+		m_handle = epoll_create1(0);
+	}
+
+	SocketPollerImpl::~SocketPollerImpl()
+	{
+		close(m_handle);
+	}
+
+	void SocketPollerImpl::Clear()
+	{
+		m_activeSockets.clear();
+		m_sockets.clear();
+	}
+
+	bool SocketPollerImpl::IsReady(SocketHandle socket) const
+	{
+		return m_activeSockets.count(socket) != 0;
+	}
+
+	bool SocketPollerImpl::IsRegistered(SocketHandle socket) const
+	{
+		return m_sockets.count(socket) != 0;
+	}
+
+	bool SocketPollerImpl::RegisterSocket(SocketHandle socket)
+	{
+		NazaraAssert(!IsRegistered(socket), "Socket is already registered");
+
+		epoll_event event;
+		event.events = EPOLLIN;
+		event.data.fd = socket;
+
+		if (epoll_ctl(m_handle, EPOLL_CTL_ADD, socket, &event) != 0)
+		{
+			NazaraError("Failed to add socket to epoll structure (errno " String::Number(errno) + ": " + Error::GetLastSystemError() + ')');
+			return false;
+		}
+
+		m_sockets.insert(socket);
+
+		return true;
+	}
+
+	void SocketPollerImpl::UnregisterSocket(SocketHandle socket)
+	{
+		NazaraAssert(IsRegistered(socket), "Socket is not registered");
+
+		m_activeSockets.erase(socket);
+		m_sockets.erase(socket);
+
+		if (epoll_ctl(m_handle, EPOLL_CTL_DEL, socket, nullptr) != 0)
+			NazaraWarning("An error occured while removing socket from epoll structure (errno " String::Number(errno) + ": " + Error::GetLastSystemError() + ')');
+	}
+
+	int SocketPollerImpl::Wait(UInt64 msTimeout, SocketError* error)
+	{
+		int activeSockets;
+
+		// Reset status of sockets
+		m_events.resize(m_sockets.size());
+		std::memset(m_events.data(), 0, m_events.size() * sizeof(epoll_event));
+
+		activeSockets = epoll_wait(m_handle, m_events.data(), static_cast<int>(m_events.size()), static_cast<int>(msTimeout));
+
+		m_activeSockets.clear();
+		if (activeSockets > 0U)
+		{
+			int socketCount = activeSockets;
+			for (int i = 0; i < socketCount; ++i)
+			{
+				if (m_events[i].events & (EPOLLIN | EPOLLHUP | EPOLLERR))
+				{
+					m_activeSockets.insert(m_events[i].data.fd);
+					if (m_events[i].events & EPOLLERR)
+						NazaraWarning("Descriptor " + String::Number(m_events[i].data.fd) + " was returned by epoll with EPOLLERR status");
+				}
+				else
+				{
+					NazaraWarning("Descriptor " + String::Number(m_events[i].data.fd) + " was returned by epoll without EPOLLIN (events: 0x" + String::Number(m_events[i].events, 16) + ')');
+					activeSockets--;
+				}
+			}
+		}
+
+		return activeSockets;
+	}
+}
