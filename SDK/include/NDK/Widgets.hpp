a4eccc9fac31111556bd64f712c8b08af58fd533
Lynix <lynix680@gmail.com>
2016-10-27 21:38:46 +0200

    SDK: Add widget global header

---
+++ SDK/include/NDK/Widgets.hpp
@@ -0,0 +1,11 @@
+// This file was automatically generated on 27 Oct 2016 at 18:53:37
+
+#pragma once
+
+#ifndef NDK_WIDGETS_GLOBAL_HPP
+#define NDK_WIDGETS_GLOBAL_HPP
+
+#include <NDK/Widgets/ButtonWidget.hpp>
+#include <NDK/Widgets/LabelWidget.hpp>
+
+#endif // NDK_WIDGETS_GLOBAL_HPP
