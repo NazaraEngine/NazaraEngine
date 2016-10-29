86b892c3bc1ad7969b5352f22108635e9556f1ce
Lynix <lynix680@gmail.com>
2016-10-29 02:46:59 +0200

    Regenerate Vulkan global include; add wrapper global include

---
+++ include/Nazara/VulkanRenderer/Wrapper.hpp
@@ -0,0 +1,33 @@
+// This file was automatically generated
+
+#pragma once
+
+#ifndef NAZARA_GLOBAL_VULKANRENDERER_WRAPPER_HPP
+#define NAZARA_GLOBAL_VULKANRENDERER_WRAPPER_HPP
+
+#include <Nazara/VulkanRenderer/Wrapper/Buffer.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/CommandBuffer.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/CommandPool.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/DescriptorPool.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/DescriptorSet.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/DescriptorSetLayout.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Device.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/DeviceMemory.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/DeviceObject.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Framebuffer.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Image.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/ImageView.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Instance.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Loader.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/PhysicalDevice.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Pipeline.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/PipelineCache.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/PipelineLayout.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Queue.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/RenderPass.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Semaphore.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/ShaderModule.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Surface.hpp>
+#include <Nazara/VulkanRenderer/Wrapper/Swapchain.hpp>
+
+#endif // NAZARA_GLOBAL_VULKANRENDERER_WRAPPER_HPP
