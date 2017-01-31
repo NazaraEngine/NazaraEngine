7b8100dafe2577a878357ad00ed72271976bb66e
Lynix <lynix680@gmail.com>
2017-02-01 00:20:22 +0100

    Network/ENet: Add ENet licence

--- src/Nazara/Network/ENetPacket.cpp
+++ src/Nazara/Network/ENetPacket.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2017 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Network module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
 #include <Nazara/Network/ENetPacket.hpp>
 #include <Nazara/Core/MemoryPool.hpp>
 #include <Nazara/Network/Debug.hpp>
