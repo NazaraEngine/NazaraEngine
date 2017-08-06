75d0ffa8885b798ae6ae677b7f1c310f5cffa029
Lynix <lynix680@gmail.com>
2017-08-06 12:13:39 +0200

    Add Vulkan demo

---
+++ examples/VulkanTest/build.lua
@@ -0,0 +1,19 @@
+EXAMPLE.Name = "VulkanTest"
+
+EXAMPLE.EnableConsole = true
+
+EXAMPLE.OsDefines.Windows = {
+	"VK_USE_PLATFORM_WIN32_KHR",
+	"WIN32_LEAN_AND_MEAN",
+	"NOMINMAX"
+}
+
+EXAMPLE.Files = {
+	"main.cpp"
+}
+
+EXAMPLE.Libraries = {
+	"NazaraCore",
+	"NazaraVulkanRenderer",
+	"NazaraUtility"
+}
