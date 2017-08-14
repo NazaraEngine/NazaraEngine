f1e380bb672512ae088cfb06ed3466927affc074
S6066 <kiwimeteor6066@gmail.com>
2017-08-14 21:34:43 +0200

    Probably looks like more generated now

--- SDK/include/NDK/Widgets.hpp
+++ SDK/include/NDK/Widgets.hpp
@@ -1,13 +1,13 @@
-// This file was automatically generated
+﻿// This file was automatically generated
 
 #pragma once
 
 #ifndef NDK_WIDGETS_GLOBAL_HPP
 #define NDK_WIDGETS_GLOBAL_HPP
 
+#include <NDK/Widgets/CheckboxWidget.hpp>
 #include <NDK/Widgets/ButtonWidget.hpp>
 #include <NDK/Widgets/LabelWidget.hpp>
 #include <NDK/Widgets/TextAreaWidget.hpp>
-#include <NDK/Widgets/CheckboxWidget.hpp>
 
 #endif // NDK_WIDGETS_GLOBAL_HPP
