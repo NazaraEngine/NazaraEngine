5be37315b92832414a09b32464f5073bf5538465
Faymoon <mercier.mateo@orange.fr>
2018-01-14 13:50:04 +0100

    fix indent

--- examples/FirstScene/main.cpp
+++ examples/FirstScene/main.cpp
@@ -3,13 +3,13 @@
 * Prérequis: Aucun
 * Utilisation du module utilitaire et graphique
 ** Présente :
-* -Création et gestion d'une fenêtre (Traitement des évènements clavier/souris)
-* -Gestion du clavier(Récupération de l'état d'une touche)
-* -Des outils pour afficher une scène basique via le chargement d'un modèle (et son affichage)
-* -Éclairage directionnel
-* -Gestion d'une caméra free-fly (Avec déplacement fluide)
-* -Gestion basique d'une horloge
-* -Console
+*	-Création et gestion d'une fenêtre (Traitement des évènements clavier/souris)
+*	-Gestion du clavier(Récupération de l'état d'une touche)
+*	-Des outils pour afficher une scène basique via le chargement d'un modèle (et son affichage)
+*	-Éclairage directionnel
+*	-Gestion d'une caméra free-fly (Avec déplacement fluide)
+*	-Gestion basique d'une horloge
+*	-Console
 */
 
 #include <Nazara/Core/Clock.hpp> // Horloges
