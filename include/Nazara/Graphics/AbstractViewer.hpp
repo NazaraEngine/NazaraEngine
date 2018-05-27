b448ba3576b029b2bdc7bdec38586f272486441d
Lynix <lynix680@gmail.com>
2018-05-27 21:45:06 +0200

    Graphics/AbstractViewer: Add Project/Unproject methods

--- include/Nazara/Graphics/AbstractViewer.hpp
+++ include/Nazara/Graphics/AbstractViewer.hpp
@@ -40,6 +40,9 @@ namespace Nz
 			virtual float GetZFar() const = 0;
 			virtual float GetZNear() const = 0;
 
+			Nz::Vector3f Project(const Nz::Vector3f& worldPosition) const;
+			Nz::Vector3f Unproject(const Nz::Vector3f& screenPos) const;
+
 			AbstractViewer& operator=(const AbstractViewer&) = default;
 			AbstractViewer& operator=(AbstractViewer&&) noexcept = default;
 	};
