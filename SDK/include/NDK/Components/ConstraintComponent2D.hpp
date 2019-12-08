ef030ddaac9d7c05c9744276b6268e1c23499243
Lynix <lynix680@gmail.com>
2019-12-08 18:56:21 +0100

    Rework constraint2D

--- SDK/include/NDK/Components/ConstraintComponent2D.hpp
+++ SDK/include/NDK/Components/ConstraintComponent2D.hpp
@@ -1,13 +1,18 @@
+// Copyright (C) 2019 Jérôme Leclercq
+// This file is part of the "Nazara Development Kit"
+// For conditions of distribution and use, see copyright notice in Prerequisites.hpp
+
 #pragma once
 
 #ifndef NDK_COMPONENTS_CONSTRAINTCOMPONENT2D_HPP
 #define NDK_COMPONENTS_CONSTRAINTCOMPONENT2D_HPP
 
-#include <NDK/Component.hpp>
 #include <Nazara/Physics2D/Constraint2D.hpp>
 #include <Nazara/Math/Vector3.hpp>
-#include <vector>
+#include <NDK/Component.hpp>
+#include <NDK/Entity.hpp>
 #include <memory>
+#include <vector>
 
 namespace Ndk
 {
@@ -19,16 +24,24 @@ namespace Ndk
 	{
 		public:
 			ConstraintComponent2D() = default;
-			ConstraintComponent2D(const ConstraintComponent2D& joint) = default;
+			ConstraintComponent2D(const ConstraintComponent2D& joint);
 			ConstraintComponent2D(ConstraintComponent2D&& joint) = default;
 
-			template<typename T, typename... Args> inline Nz::ObjectRef<T> CreateConstraint(const Ndk::EntityHandle& first, const Ndk::EntityHandle& second, Args&&... args);
-		
+			template<typename T, typename... Args> T* CreateConstraint(const Ndk::EntityHandle& first, const Ndk::EntityHandle& second, Args&&... args);
+			bool RemoveConstraint(Nz::Constraint2D* constraint);
+
 			static ComponentIndex componentIndex;
 
 		private:
+			struct ConstraintData
+			{
+				std::unique_ptr<Nz::Constraint2D> constraint;
 
-			std::vector<Nz::Constraint2DRef> m_constraints;
+				NazaraSlot(Ndk::Entity, OnEntityDestruction, onBodyADestruction);
+				NazaraSlot(Ndk::Entity, OnEntityDestruction, onBodyBDestruction);
+			};
+
+			std::vector<ConstraintData> m_constraints;
 	};
 
 }
