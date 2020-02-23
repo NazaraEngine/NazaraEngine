87900a78cbcd19c68c3a1d56afd09081ad1468f8
Lynix <lynix680@gmail.com>
2020-02-23 02:17:27 +0100

    DynLib: Posix fixes

--- src/Nazara/Core/Posix/DynLibImpl.hpp
+++ src/Nazara/Core/Posix/DynLibImpl.hpp
@@ -8,6 +8,8 @@
 #define NAZARA_DYNLIBIMPL_HPP
 
 #include <Nazara/Core/DynLib.hpp>
+#include <filesystem>
+#include <string>
 
 namespace Nz
 {
@@ -19,9 +21,8 @@ namespace Nz
 			DynLibImpl(DynLib* m_parent);
 			~DynLibImpl() = default;
 
-			DynLibFunc GetSymbol(const String& symbol, String* errorMessage) const;
-			bool Load(const String& libraryPath, String* errorMessage);
-			void Unload();
+			DynLibFunc GetSymbol(const char* symbol, std::string* errorMessage) const;
+			bool Load(const std::filesystem::path& libraryPath, std::string* errorMessage);
 
 		private:
 			void* m_handle;
