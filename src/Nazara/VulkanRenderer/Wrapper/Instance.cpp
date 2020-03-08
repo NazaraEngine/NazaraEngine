1e302a4f93d42712ab0354075e0c4d585031dc57
Lynix <lynix680@gmail.com>
2020-03-08 18:09:45 +0100

    Move Vulkan functions list to external files

--- src/Nazara/VulkanRenderer/Wrapper/Instance.cpp
+++ src/Nazara/VulkanRenderer/Wrapper/Instance.cpp
@@ -35,112 +35,25 @@ namespace Nz
 				m_loadedLayers.insert(createInfo.ppEnabledLayerNames[i]);
 
 			// And now load everything
-			#define NAZARA_VULKANRENDERER_LOAD_INSTANCE(func) func = reinterpret_cast<PFN_##func>(GetProcAddr(#func))
-
 			try
 			{
 				ErrorFlags flags(ErrorFlag_ThrowException, true);
 
-				// Vulkan core
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateDevice);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkDestroyInstance);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkEnumeratePhysicalDevices);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetDeviceProcAddr);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceFeatures);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceFormatProperties);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceImageFormatProperties);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceMemoryProperties);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceProperties);
-				NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceQueueFamilyProperties);
+#define NAZARA_VULKANRENDERER_INSTANCE_EXT_BEGIN(ext) if (IsExtensionLoaded(#ext)) {
+#define NAZARA_VULKANRENDERER_INSTANCE_EXT_END() }
+#define NAZARA_VULKANRENDERER_INSTANCE_FUNCTION(func) func = reinterpret_cast<PFN_##func>(GetProcAddr(#func));
 
-				// VK_KHR_display
-				if (IsExtensionLoaded("VK_KHR_display"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateDisplayModeKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateDisplayPlaneSurfaceKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetDisplayModePropertiesKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetDisplayPlaneCapabilitiesKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetDisplayPlaneSupportedDisplaysKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceDisplayPlanePropertiesKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceDisplayPropertiesKHR);
-				}
+#include <Nazara/VulkanRenderer/Wrapper/InstanceFunctions.hpp>
 
-				// VK_KHR_surface
-				if (IsExtensionLoaded("VK_KHR_surface"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkDestroySurfaceKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceSurfaceCapabilitiesKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceSurfaceFormatsKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceSurfacePresentModesKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceSurfaceSupportKHR);
-				}
-
-				// VK_EXT_debug_report
-				if (IsExtensionLoaded("VK_EXT_debug_report"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateDebugReportCallbackEXT);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkDestroyDebugReportCallbackEXT);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkDebugReportMessageEXT);
-				}
-
-				#ifdef VK_USE_PLATFORM_ANDROID_KHR
-				// VK_KHR_android_surface
-				if (IsExtensionLoaded("VK_KHR_android_surface"))
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateAndroidSurfaceKHR);
-				#endif
-
-				#ifdef VK_USE_PLATFORM_MIR_KHR
-				// VK_KHR_mir_surface
-				if (IsExtensionLoaded("VK_KHR_mir_surface"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateMirSurfaceKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceMirPresentationSupportKHR);
-				}
-				#endif
-
-				#ifdef VK_USE_PLATFORM_XCB_KHR
-				// VK_KHR_xcb_surface
-				if (IsExtensionLoaded("VK_KHR_xcb_surface"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateXcbSurfaceKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceXcbPresentationSupportKHR);
-				}
-				#endif
-
-				#ifdef VK_USE_PLATFORM_XLIB_KHR
-				// VK_KHR_xlib_surface
-				if (IsExtensionLoaded("VK_KHR_xlib_surface"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateXlibSurfaceKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceXlibPresentationSupportKHR);
-				}
-				#endif
-
-				#ifdef VK_USE_PLATFORM_WAYLAND_KHR
-				// VK_KHR_wayland_surface
-				if (IsExtensionLoaded("VK_KHR_wayland_surface"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateWaylandSurfaceKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceWaylandPresentationSupportKHR);
-				}
-				#endif
-
-				#ifdef VK_USE_PLATFORM_WIN32_KHR
-				// VK_KHR_win32_surface
-				if (IsExtensionLoaded("VK_KHR_win32_surface"))
-				{
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkCreateWin32SurfaceKHR);
-					NAZARA_VULKANRENDERER_LOAD_INSTANCE(vkGetPhysicalDeviceWin32PresentationSupportKHR);
-				}
-				#endif
+#undef NAZARA_VULKANRENDERER_INSTANCE_EXT_BEGIN
+#undef NAZARA_VULKANRENDERER_INSTANCE_EXT_END
+#undef NAZARA_VULKANRENDERER_INSTANCE_FUNCTION
 			}
 			catch (const std::exception& e)
 			{
 				NazaraError(String("Failed to query instance function: ") + e.what());
 				return false;
 			}
-
-			#undef NAZARA_VULKANRENDERER_LOAD_INSTANCE
 
 			return true;
 		}
