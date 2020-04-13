8b6940d12f058b4287e4fd3485bf86a6193e12f1
Lynix <lynix680@gmail.com>
2020-04-13 15:13:15 +0200

    Don't link VulkanTest with VulkanRenderer

--- examples/VulkanTest/build.lua
+++ examples/VulkanTest/build.lua
@@ -2,12 +2,6 @@ EXAMPLE.Name = "VulkanTest"
 
 EXAMPLE.EnableConsole = true
 
-EXAMPLE.OsDefines.Windows = {
-	"VK_USE_PLATFORM_WIN32_KHR",
-	"WIN32_LEAN_AND_MEAN",
-	"NOMINMAX"
-}
-
 EXAMPLE.Files = {
 	"main.cpp"
 }
@@ -15,6 +9,6 @@ EXAMPLE.Files = {
 EXAMPLE.Libraries = {
 	"NazaraCore",
 	"NazaraPlatform",
-	"NazaraVulkanRenderer",
+	"NazaraRenderer",
 	"NazaraUtility"
 }
