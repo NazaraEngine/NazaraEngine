6073d8f592c70c07ded73b4a755611927ee71c43
Lynix <lynix680@gmail.com>
2020-05-11 13:57:11 +0200

    OpenGL: Implement UploadPool

--- src/Nazara/OpenGLRenderer/OpenGLUploadPool.cpp
+++ src/Nazara/OpenGLRenderer/OpenGLUploadPool.cpp
@@ -11,23 +11,56 @@ namespace Nz
 {
 	auto OpenGLUploadPool::Allocate(UInt64 size) -> Allocation&
 	{
-		/*const auto& deviceProperties = m_device.GetPhysicalDeviceInfo().properties;
-		UInt64 preferredAlignement = deviceProperties.limits.optimalBufferCopyOffsetAlignment;*/
-
-		return Allocate(size, 0); //< FIXME
+		return Allocate(size, 1); //< Alignment doesn't matter
 	}
 
-	auto OpenGLUploadPool::Allocate(UInt64 size, UInt64 alignment) -> Allocation&
+	auto OpenGLUploadPool::Allocate(UInt64 size, UInt64 /*alignment*/) -> Allocation&
 	{
 		assert(size <= m_blockSize);
 
-		static Allocation dummy;
-		return dummy;
+		// Try to minimize lost space
+		struct
+		{
+			Block* block = nullptr;
+			UInt64 offset = 0;
+		} bestBlock;
+
+		for (Block& block : m_blocks)
+		{
+			UInt64 alignedOffset = block.freeOffset;
+			if (block.freeOffset + size > m_blockSize)
+				continue; //< Not enough space
+
+			if (!bestBlock.block)
+			{
+				bestBlock.block = &block;
+				bestBlock.offset = block.freeOffset;
+				break; //< Since we have no alignment constraint, the first block is good
+			}
+		}
+
+		// No block found, allocate a new one
+		if (!bestBlock.block)
+		{
+			Block newBlock;
+			newBlock.memory.resize(m_blockSize);
+
+			bestBlock.block = &m_blocks.emplace_back(std::move(newBlock));
+			bestBlock.offset = 0;
+		}
+
+		Allocation& allocationData = m_allocations.emplace_back();
+		allocationData.mappedPtr = static_cast<UInt8*>(bestBlock.block->memory.data()) + bestBlock.offset;
+		allocationData.size = size;
+
+		return allocationData;
 	}
 
 	void OpenGLUploadPool::Reset()
 	{
-		/*for (Block& block : m_blocks)
-			block.freeOffset = 0;*/
+		for (Block& block : m_blocks)
+			block.freeOffset = 0;
+
+		m_allocations.clear();
 	}
 }
