1734b8e5db030ebd53fedceded84c44f3fc06f37
Lynix <lynix680@gmail.com>
2020-05-27 20:37:20 +0200

    SDL fixes

--- build/scripts/modules/platform.lua
+++ build/scripts/modules/platform.lua
@@ -16,3 +16,12 @@ MODULE.Files = {
 	"../src/Nazara/Platform/SDL2/**.hpp",
 	"../src/Nazara/Platform/SDL2/**.cpp"
 }
+
+MODULE.OsDefines.Windows = {
+	"SDL_VIDEO_DRIVER_WINDOWS=1"
+}
+
+MODULE.OsDefines.Posix = {
+	"SDL_VIDEO_DRIVER_X11=1",
+	"SDL_VIDEO_DRIVER_WAYLAND=1",
+}
