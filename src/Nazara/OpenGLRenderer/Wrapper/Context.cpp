b01a3ee3e1999f8162bc3f08dab2c1b3cc2bee47
Jérôme Leclercq <lynix680@gmail.com>
2020-08-09 18:43:44 +0200

    Enable GL_DEBUG_OUTPUT

--- src/Nazara/OpenGLRenderer/Wrapper/Context.cpp
+++ src/Nazara/OpenGLRenderer/Wrapper/Context.cpp
@@ -247,6 +247,11 @@ namespace Nz::GL
 		// Set debug callback (if supported)
 		if (glDebugMessageCallback)
 		{
+			glEnable(GL_DEBUG_OUTPUT);
+#ifdef NAZARA_DEBUG
+			glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+#endif
+
 			glDebugMessageCallback([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
 			{
 				const Context* context = static_cast<const Context*>(userParam);
