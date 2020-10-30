ae00591a6e180b0374a64c6bce8018b0c1782717
Jérôme Leclercq <lynix680@gmail.com>
2020-10-30 23:08:55 +0100

    Add GraphicsTest demo

---
+++ examples/GraphicsTest/build.lua
@@ -0,0 +1,16 @@
+EXAMPLE.Name = "GraphicsTest"
+
+EXAMPLE.EnableConsole = true
+
+EXAMPLE.Files = {
+	"main.cpp"
+}
+
+EXAMPLE.Libraries = {
+	"NazaraCore",
+	"NazaraGraphics",
+	"NazaraPlatform",
+	"NazaraRenderer",
+	"NazaraShader",
+	"NazaraUtility"
+}
