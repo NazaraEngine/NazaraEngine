3a7f5c263092d34e17948bf66787937f2fce0536
Jérôme Leclercq <lynix680@gmail.com>
2021-04-17 14:43:29 +0200

    Shader: Fix optimization

--- src/Nazara/Shader/Ast/SanitizeVisitor.cpp
+++ src/Nazara/Shader/Ast/SanitizeVisitor.cpp
@@ -358,33 +358,7 @@ namespace Nz::ShaderAst
 	ExpressionPtr SanitizeVisitor::Clone(ConstantExpression& node)
 	{
 		auto clone = static_unique_pointer_cast<ConstantExpression>(AstCloner::Clone(node));
-		clone->cachedExpressionType = std::visit([&](auto&& arg) -> ShaderAst::ExpressionType
-		{
-			using T = std::decay_t<decltype(arg)>;
-
-			if constexpr (std::is_same_v<T, bool>)
-				return PrimitiveType::Boolean;
-			else if constexpr (std::is_same_v<T, float>)
-				return PrimitiveType::Float32;
-			else if constexpr (std::is_same_v<T, Int32>)
-				return PrimitiveType::Int32;
-			else if constexpr (std::is_same_v<T, UInt32>)
-				return PrimitiveType::UInt32;
-			else if constexpr (std::is_same_v<T, Vector2f>)
-				return VectorType{ 2, PrimitiveType::Float32 };
-			else if constexpr (std::is_same_v<T, Vector3f>)
-				return VectorType{ 3, PrimitiveType::Float32 };
-			else if constexpr (std::is_same_v<T, Vector4f>)
-				return VectorType{ 4, PrimitiveType::Float32 };
-			else if constexpr (std::is_same_v<T, Vector2i32>)
-				return VectorType{ 2, PrimitiveType::Int32 };
-			else if constexpr (std::is_same_v<T, Vector3i32>)
-				return VectorType{ 3, PrimitiveType::Int32 };
-			else if constexpr (std::is_same_v<T, Vector4i32>)
-				return VectorType{ 4, PrimitiveType::Int32 };
-			else
-				static_assert(AlwaysFalse<T>::value, "non-exhaustive visitor");
-		}, clone->value);
+		clone->cachedExpressionType = clone->GetExpressionType();
 
 		return clone;
 	}
