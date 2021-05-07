f64c65304c4fd7ebf04f64a78cdea552e1291445
Jérôme Leclercq <lynix680@gmail.com>
2021-05-07 14:15:57 +0200

    CI: Fix dependencies hash (until next xmake version)

--- xmake.lua
+++ xmake.lua
@@ -190,3 +190,27 @@ rule("build_rendererplugins")
 			end
 		end
 	end)
+
+-- Generates a hash key made of packages confs/version, for CI
+task("dephash")
+	on_run(function ()
+		import("core.project.project")
+		import("private.action.require.impl.package")
+
+		local requires, requires_extra = project.requires_str()
+
+		local key = {}
+		for _, instance in irpairs(package.load_packages(requires, {requires_extra = requires_extra})) do
+			table.insert(key, instance:name() .. "-" .. instance:version_str() .. "-" .. instance:buildhash())
+		end
+
+		table.sort(key)
+
+		key = table.concat(key, ",")
+		print(hash.uuid4(key):gsub('-', ''):lower())
+	end)
+
+	set_menu {
+		usage = "xmake dephash",
+		description = "Outputs a hash key of current dependencies version/configuration"
+	}
