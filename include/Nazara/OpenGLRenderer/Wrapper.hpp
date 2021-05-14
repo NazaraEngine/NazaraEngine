572dc56b108b292ceb4542337f272785bd599fd5
Jérôme Leclercq <lynix680@gmail.com>
2021-05-14 03:08:44 +0200

    Update global headers

---
+++ include/Nazara/OpenGLRenderer/Wrapper.hpp
@@ -0,0 +1,22 @@
+// This file was automatically generated
+
+#pragma once
+
+#ifndef NAZARA_GLOBAL_OPENGLRENDERER_WRAPPER_HPP
+#define NAZARA_GLOBAL_OPENGLRENDERER_WRAPPER_HPP
+
+#include <Nazara/OpenGLRenderer/Wrapper/Buffer.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/Context.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/ContextObject.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/CoreFunctions.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/DeviceObject.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/Framebuffer.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/Loader.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/Program.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/RenderBuffer.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/Sampler.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/Shader.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/Texture.hpp>
+#include <Nazara/OpenGLRenderer/Wrapper/VertexArray.hpp>
+
+#endif // NAZARA_GLOBAL_OPENGLRENDERER_WRAPPER_HPP
