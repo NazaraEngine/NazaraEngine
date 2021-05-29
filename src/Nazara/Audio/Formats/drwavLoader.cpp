557c23a9fac2af2222453cdd450345efa381b0f1
Jérôme Leclercq <lynix680@gmail.com>
2021-05-29 23:17:50 +0200

    Minor stuff

--- src/Nazara/Audio/Formats/drwavLoader.cpp
+++ src/Nazara/Audio/Formats/drwavLoader.cpp
@@ -2,7 +2,7 @@
 // This file is part of the "Nazara Engine - Audio module"
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
-#include <Nazara/Audio/Formats/minimp3Loader.hpp>
+#include <Nazara/Audio/Formats/drwavLoader.hpp>
 #include <Nazara/Audio/Algorithm.hpp>
 #include <Nazara/Audio/Audio.hpp>
 #include <Nazara/Audio/Config.hpp>
