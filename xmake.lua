7c9bc16535a2cf65f59699c90760b9f389c3249a
Jérôme Leclercq <lynix680@gmail.com>
2021-06-03 17:22:12 +0200

    XMake: Add asan mode

--- xmake.lua
+++ xmake.lua
@@ -105,7 +105,7 @@ add_requires("newtondynamics", { debug = is_plat("windows") and is_mode("debug")
 
 set_project("NazaraEngine")
 
-add_rules("mode.debug", "mode.releasedbg")
+add_rules("mode.asan", "mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 add_rules("build_rendererplugins")
 
