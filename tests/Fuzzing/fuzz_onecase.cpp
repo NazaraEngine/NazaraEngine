c7306e4689af00a45d537a4c26e562cb56041bcb
gogo <gogo246475@gmail.com>
2021-08-17 00:53:44 +0200

    edit CI file

--- tests/Fuzzing/fuzz_onecase.cpp
+++ tests/Fuzzing/fuzz_onecase.cpp
@@ -9,21 +9,9 @@
 #include <Nazara/Physics2D/Physics2D.hpp>
 #include <Nazara/Shader/Shader.hpp>
 
-int fuzz_one(std::string const &s)
-{
-    const char* arguments[] = { ("--input-file " + s).c_str() };
-    int result = Catch::Session().run(1, arguments);
-    return result;
-}
-
 int main(int argc, char* argv[])
 {
 	Nz::Modules<Nz::Audio, Nz::Network, Nz::Physics2D, Nz::Shader> nazaza;
-
-	std::string s((std::istreambuf_iterator<char>(std::cin)),
-	               (std::istreambuf_iterator<char>()));
-
-    int result = fuzz_one(s);
-
-	return result;
+    int result = Catch::Session().run(1, argv);
+    return 0;
 }
