0bb443b0d099009979279a8bc0193d51b68444b0
Jérôme Leclercq <lynix680@gmail.com>
2021-08-23 19:35:45 +0200

    Fix xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -116,7 +116,7 @@ add_rules("build_rendererplugins")
 
 set_allowedmodes("asan", "debug", "releasedbg")
 set_allowedplats("windows", "mingw", "linux", "macosx")
-set_allowedarchs("windows|x64", "mingw|x86_64" "linux|x86_64", "macosx|x86_64")
+set_allowedarchs("windows|x64", "mingw|x86_64", "linux|x86_64", "macosx|x86_64")
 set_defaultmode("debug")
 
 if is_mode("debug") then
