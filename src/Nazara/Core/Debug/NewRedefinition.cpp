37df122ad3971520401f563732f9a4122224fab0
Jérôme Leclercq <lynix680@gmail.com>
2021-10-27 18:09:12 +0200

    XMake: Add inclusion fix

--- src/Nazara/Core/Debug/NewRedefinition.cpp
+++ src/Nazara/Core/Debug/NewRedefinition.cpp
@@ -2,6 +2,8 @@
 // This file is part of the "Nazara Engine - Core module"
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
+// no include fix
+
 #include <Nazara/Core/Config.hpp>
 #if NAZARA_CORE_MANAGE_MEMORY
 
