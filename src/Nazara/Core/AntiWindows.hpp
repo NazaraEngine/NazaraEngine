eb4629947ef36070a471ff3490a9926d0845c53f
Jérôme Leclercq <lynix680@gmail.com>
2022-03-16 08:25:19 +0100

    Minor fixes

--- src/Nazara/Core/AntiWindows.hpp
+++ src/Nazara/Core/AntiWindows.hpp
@@ -2,6 +2,8 @@
 // This file is part of the "Nazara Engine - Core module"
 // For conditions of distribution and use, see copyright notice in Config.hpp
 
+// no header guards
+
 #ifndef _WINDOWS_
 #error This header should only be included after including windows.h directly or indirectly in a .cpp
 #endif
