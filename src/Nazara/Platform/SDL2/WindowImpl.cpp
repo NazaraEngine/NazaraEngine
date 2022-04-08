b46fc6ffce5c40be70a06cf0faa8bab4e7529af9
SirLynix <lynix680@gmail.com>
2022-04-08 08:49:36 +0200

    Fix X11 polluting namespace with unity build

--- src/Nazara/Platform/SDL2/WindowImpl.cpp
+++ src/Nazara/Platform/SDL2/WindowImpl.cpp
@@ -665,3 +665,9 @@ namespace Nz
 	   DestroyWindow(winHandle);*/
 	//}
 }
+
+#if defined(NAZARA_PLATFORM_WINDOWS)
+#include <Nazara/Core/AntiWindows.hpp>
+#elif defined(NAZARA_PLATFORM_LINUX)
+#include <Nazara/Core/AntiX11.hpp>
+#endif
