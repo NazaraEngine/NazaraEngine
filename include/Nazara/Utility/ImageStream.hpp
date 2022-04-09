2a091d25b7d81c4dade8838fa7c527b13aac814a
SirLynix <lynix680@gmail.com>
2022-04-09 18:22:13 +0200

    Utility: Add ImageStream and GIF support

---
+++ include/Nazara/Utility/ImageStream.hpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2022 Jérôme "Lynix" Leclercq (lynix680@gmail.com)
+// This file is part of the "Nazara Engine - Utility module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_UTILITY_IMAGESTREAM_HPP
+#define NAZARA_UTILITY_IMAGESTREAM_HPP
+
+#include <Nazara/Prerequisites.hpp>
+#include <Nazara/Core/Resource.hpp>
+#include <Nazara/Core/ResourceLoader.hpp>
+#include <Nazara/Core/ResourceParameters.hpp>
+#include <Nazara/Math/Vector2.hpp>
+#include <Nazara/Utility/Config.hpp>
+#include <Nazara/Utility/Enums.hpp>
+#include <mutex>
+
+namespace Nz
+{
+	struct ImageStreamParams : public ResourceParameters
+	{
+		bool IsValid() const;
+	};
+
+	class Mutex;
+	class ImageStream;
+
+	using ImageStreamLoader = ResourceLoader<ImageStream, ImageStreamParams>;
+
+	class NAZARA_UTILITY_API ImageStream : public Resource
+	{
+		public:
+			ImageStream() = default;
+			virtual ~ImageStream();
+
+			virtual bool DecodeNextFrame(void* buffer) = 0;
+
+			virtual UInt64 GetFrameCount() const = 0;
+			virtual UInt64 GetFrameDelay(std::size_t frameIndex) const = 0;
+			virtual PixelFormat GetPixelFormat() const = 0;
+			virtual Vector2ui GetSize() const = 0;
+
+			virtual bool HasConstantRate() const = 0;
+
+			virtual void Seek(UInt64 frameIndex) = 0;
+
+			virtual UInt64 Tell() = 0;
+
+			static std::shared_ptr<ImageStream> OpenFromFile(const std::filesystem::path& filePath, const ImageStreamParams& params = ImageStreamParams());
+			static std::shared_ptr<ImageStream> OpenFromMemory(const void* data, std::size_t size, const ImageStreamParams& params = ImageStreamParams());
+			static std::shared_ptr<ImageStream> OpenFromStream(Stream& stream, const ImageStreamParams& params = ImageStreamParams());
+	};
+}
+
+#include <Nazara/Utility/ImageStream.inl>
+
+#endif // NAZARA_UTILITY_IMAGESTREAM_HPP
