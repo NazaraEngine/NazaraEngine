61722d1c9b9120bc1a46481f0dce62599f1f6c9f
Lynix <lynix680@gmail.com>
2022-06-07 20:52:46 +0200

    Add support for options on task compile-shaders

--- xmake/actions/compile_shaders.lua
+++ xmake/actions/compile_shaders.lua
@@ -3,10 +3,15 @@ task("compile-shaders")
 	set_menu({
 		-- Settings menu usage
 		usage = "xmake compile-shaders [options]",
-		description = "Compile engine shaders"
+		description = "Compile engine shaders",
+		options = {
+			{nil, "measure", "k", nil, "Measure time taken for every step of the shader compilation." },
+			{nil, "benchmark-iteration", "kv", nil, "Benchmark using a number of iterations." },
+		}
 	})
 
 	on_run(function ()
+		import("core.base.option")
 		import("core.project.project")
 		local nzsl = path.join(project.required_package("nzsl"):installdir(), "bin", "nzslc")
 
@@ -17,11 +22,17 @@ task("compile-shaders")
 				envs = mingw:runenvs()
 			end
 		end
-		
+
 		print("Compiling shaders...")
 		for _, filepath in pairs(os.files("src/Nazara/*/Resources/**.nzsl")) do
 			print(" - Compiling " .. filepath)
 			local argv = {"--compile=nzslb", "--partial", "--header-file", filepath }
+			if option.get("measure") then
+				table.insert(argv, "--measure")
+			end
+			if option.get("benchmark-iteration") then
+				table.insert(argv, "--benchmark-iteration=" .. option.get("benchmark-iteration"))
+			end
 			os.execv(nzsl, argv, { envs = envs })
 		end
 	end)
