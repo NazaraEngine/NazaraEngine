ff169705428d429a3190b6896517c009928f506f
SirLynix <lynix680@gmail.com>
2023-02-26 14:19:43 +0100

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -209,7 +209,6 @@ option("unitybuild", { description = "Build the engine using unity build", defau
 -- Nazara dependencies
 
 add_repositories("nazara-engine-repo https://github.com/NazaraEngine/xmake-repo")
-add_repositories("local-repo xmake-repo")
 add_requires("nazarautils")
 add_requires("nzsl", { debug = is_mode("debug"), configs = { with_symbols = not is_mode("release"), shared = not is_plat("wasm", "android") } })
 
