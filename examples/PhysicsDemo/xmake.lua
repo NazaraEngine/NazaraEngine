9306ce246f61ee015a32ba72e10e5ec4912e5ad3
SirLynix <lynix680@gmail.com>
2023-03-07 19:28:51 +0100

    Re-enable demo using Physics3D on web

--- examples/PhysicsDemo/xmake.lua
+++ examples/PhysicsDemo/xmake.lua
@@ -1,7 +1,3 @@
-if is_plat("wasm") then
-	return -- Physics3D is not yet supported on wasm because of Newton
-end
-
 target("PhysicsDemo")
 	add_deps("NazaraGraphics", "NazaraPhysics3D")
 	add_packages("entt")
