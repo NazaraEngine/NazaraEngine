NazaraEngine/NazaraEngine

commit 97f1c2c56c6b62ee74a6c18efd1dccde865c18fd

Refactor xmake.lua and make some warnings as errors

src/Nazara/Graphics/FrameGraph.cpp
@@ -8,6 +8,7 @@
 #include <Nazara/Graphics/FrameGraph.hpp>
 #include <Nazara/Core/Algorithm.hpp>
 #include <Nazara/Graphics/Graphics.hpp>
+#include <NazaraUtils/Bitset.hpp>
 #include <NazaraUtils/StackArray.hpp>
 #include <stdexcept>
 #include <unordered_set>
@@ -1099,7 +1100,7 @@ namespace Nz
 	void FrameGraph::RemoveDuplicatePasses()
 	{
 		// A way to remove duplicates from a std::vector without sorting it
-		std::vector<bool> seen(m_framePasses.size());
+		Bitset<> seen(m_framePasses.size());
 
 		auto itRead = m_pending.passList.begin();
 		auto itWrite = m_pending.passList.begin();

include/Nazara/Core/VirtualDirectory.inl
@@ -215,7 +215,8 @@ namespace Nz
 					}
 					else
 					{
-						constexpr std::size_t blockSize = 4 * 1024;
+						// File size isn't know, read it block by block until the end of stream
+						constexpr std::size_t blockSize = 4u * 1024;
 
 						while (!stream.EndOfStream())
 						{

include/Nazara/Graphics/Tilemap.hpp
@@ -82,11 +82,11 @@ namespace Nz
 			};
 
 			mutable std::vector<VertexStruct_XYZ_Color_UV> m_vertices;
-			std::vector<Tile> m_tiles;
 			std::vector<Layer> m_layers;
-			Vector2ui m_mapSize;
+			std::vector<Tile> m_tiles;
 			Vector2f m_origin;
 			Vector2f m_tileSize;
+			Vector2ui m_mapSize;
 			bool m_isometricModeEnabled;
 			mutable bool m_shouldRebuildVertices;
 	};

src/Nazara/Graphics/Light.cpp
@@ -11,8 +11,8 @@ namespace Nz
 	Light::Light(UInt8 lightType) :
 	m_boundingVolume(BoundingVolumef::Null()),
 	m_shadowMapFormat(Graphics::Instance()->GetPreferredDepthFormat()),
-	m_shadowMapSize(512),
 	m_lightType(lightType),
+	m_shadowMapSize(512),
 	m_isShadowCaster(false)
 	{
 	}

src/Nazara/Graphics/PropertyHandler/OptionValuePropertyHandler.cpp
@@ -50,8 +50,6 @@ namespace Nz
 			{
 				constexpr MaterialPropertyType PropertyType = TypeToMaterialPropertyType_v<T>;
 
-				using BufferType = typename MaterialPropertyTypeInfo<PropertyType>::BufferType;
-
 				materialInstance.UpdateOptionValue(m_optionHash, MaterialPropertyTypeInfo<PropertyType>::EncodeToOption(arg));
 			}
 			else

src/Nazara/Graphics/Tilemap.cpp
@@ -23,11 +23,11 @@ namespace Nz
 	* \remark The default material is used for every material requested
 	*/
 	Tilemap::Tilemap(const Vector2ui& mapSize, const Vector2f& tileSize, std::size_t materialCount) :
-	m_tiles(mapSize.x* mapSize.y),
 	m_layers(materialCount),
-	m_mapSize(mapSize),
-	m_tileSize(tileSize),
+	m_tiles(mapSize.x* mapSize.y),
 	m_origin(0.f, 0.f),
+	m_tileSize(tileSize),
+	m_mapSize(mapSize),
 	m_isometricModeEnabled(false),
 	m_shouldRebuildVertices(false)
 	{

src/Nazara/Platform/SDL2/WindowImpl.cpp
@@ -117,6 +117,12 @@ namespace Nz
 				return false;
 			}
 
+			case WindowBackend::Web:
+			{
+				NazaraError("unsupported creation from a Web handle");
+				return false;
+			}
+
 			case WindowBackend::Cocoa:   systemHandle = handle.cocoa.window;       break;
 			case WindowBackend::X11:     systemHandle = reinterpret_cast<void*>(std::uintptr_t(handle.x11.window)); break;
 			case WindowBackend::Windows: systemHandle = handle.windows.window;     break;

src/Nazara/Platform/Window.cpp
@@ -20,11 +20,11 @@ namespace Nz
 	}
 
 	Window::Window(Window&& window) noexcept :
-	m_cursorController(std::move(window.m_cursorController)),
 	m_cursor(std::move(window.m_cursor)),
-	m_eventHandler(std::move(window.m_eventHandler)),
 	m_icon(std::move(window.m_icon)),
 	m_impl(std::move(window.m_impl)),
+	m_cursorController(std::move(window.m_cursorController)),
+	m_eventHandler(std::move(window.m_eventHandler)),
 	m_closed(window.m_closed),
 	m_closeOnQuit(window.m_closeOnQuit),
 	m_ownsWindow(window.m_ownsWindow),

commit a578b061b4b203a6ae7eea309ca96477b038c760

Graphics/FrameGraph: Fix bug introduced in 97f1c2c56c6b62ee74a6c18efd1dccde865c18fd

src/Nazara/Graphics/FrameGraph.cpp
@@ -1100,7 +1100,7 @@ namespace Nz
 	void FrameGraph::RemoveDuplicatePasses()
 	{
 		// A way to remove duplicates from a std::vector without sorting it
-		Bitset<> seen(m_framePasses.size());
+		Bitset<> seen(m_framePasses.size(), false);
 
 		auto itRead = m_pending.passList.begin();
 		auto itWrite = m_pending.passList.begin();