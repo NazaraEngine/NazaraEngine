4175e18b5ba080c22af8c8f0d92447577f5ba0be
SirLynix <lynix680@gmail.com>
2023-06-09 09:23:18 +0200

    Examples/WebService: Switch URL

--- examples/WebServices/main.cpp
+++ examples/WebServices/main.cpp
@@ -8,10 +8,13 @@ int main()
 
 	std::unique_ptr<Nz::WebService> webService = Nz::Network::Instance()->InstantiateWebService();
 
-	std::unique_ptr<Nz::WebRequest> webRequest = webService->CreateGetRequest("https://google.com", [&](const Nz::WebRequestResult& result)
+	std::unique_ptr<Nz::WebRequest> webRequest = webService->CreateGetRequest("https://www.perdu.com", [&](const Nz::WebRequestResult& result)
 	{
 		if (result)
-			std::cout << "Got a response (" << result.GetDownloadedSize() << " bytes, " << result.GetDownloadSpeed() << " bytes/s" << ")" << std::endl;
+		{
+			std::cout << "Got a " << result.GetReponseCode() << " response(" << result.GetDownloadedSize() << " bytes, " << result.GetDownloadSpeed() << " bytes / s" << ")" << std::endl;
+			std::cout << result.GetBody() << std::endl;
+		}
 		else
 			std::cout << "Web request failed (code " << result.GetReponseCode() << "): " << result.GetErrorMessage() << std::endl;
 
