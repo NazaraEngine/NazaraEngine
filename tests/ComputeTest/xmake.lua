64de142614bcf8f1b1da80cfdcd3d0b25ffbdb73
SirLynix <lynix680@gmail.com>
2023-07-21 08:57:24 +0200

    Add compute shaders as filegroup on VS

--- tests/ComputeTest/xmake.lua
+++ tests/ComputeTest/xmake.lua
@@ -5,3 +5,6 @@ end
 target("ComputeTest")
 	add_deps("NazaraRenderer")
 	add_files("main.cpp")
+	add_filegroups("shaders", {rootdir = "../../assets/shaders/compute"})
+	add_headerfiles("../../assets/shaders/compute/compute_edge_detection.nzsl")
+	add_headerfiles("../../assets/shaders/compute/compute_sepia.nzsl")
