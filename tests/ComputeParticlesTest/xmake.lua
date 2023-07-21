d94d5415a34e196b17d4881e458b2c49bc0fbdb2
SirLynix <lynix680@gmail.com>
2023-07-21 08:57:24 +0200

    Add compute shaders as filegroup on VS

--- tests/ComputeParticlesTest/xmake.lua
+++ tests/ComputeParticlesTest/xmake.lua
@@ -1,3 +1,10 @@
+if is_plat("wasm") then 
+	return -- Compute shaders are not supported with WebGL (but are with WebGPU)
+end
+
 target("ComputeParticlesTest")
 	add_deps("NazaraRenderer")
 	add_files("main.cpp")
+	add_filegroups("shaders", {rootdir = "../../assets/shaders/compute"})
+	add_headerfiles("../../assets/shaders/compute/compute_particle_texture.nzsl")
+	add_headerfiles("../../assets/shaders/compute/compute_particles.nzsl")
