d94f3aeb54846802be90dad2c95fed1ec0e36ef7
PoloNX <geyrecharlie@gmail.com>
2023-07-25 18:56:49 +0200

    Add Pong example

---
+++ examples/Pong/State/GameState.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <Nazara/Core.hpp>
+#include <Nazara/Platform/Window.hpp>
+
+class GameState : public Nz::State
+{
+public:
+    GameState(Nz::EnttWorld& world, Nz::Window& window);
+    void Enter(Nz::StateMachine& fsm) override;
+    void Leave(Nz::StateMachine& fsm) override;
+    bool Update(Nz::StateMachine& fsm, Nz::Time elapsedTime) override;
+
+private:
+    entt::handle CreateEntity(Nz::EnttWorld& world, const Nz::Vector2f& position, const std::string& imageName);
+    entt::handle CreateBaseEntity(Nz::EnttWorld& world, const Nz::Vector2f& position);
+    void ResetBall();
+    void checkPhysics();
+    void BounceBallY();
+    void BounceBallX();
+    void AddKeyboard(Nz::Window& window);
+    void UpdateScore();
+
+	
+    entt::handle m_ball;
+	entt::handle m_leftPad;
+	entt::handle m_rightPad;
+
+    entt::handle m_upperBord;
+    entt::handle m_lowerBord;
+    entt::handle m_leftBord;
+    entt::handle m_rightBord;
+
+    entt::handle m_score;
+
+    int PlayerBScore = 0;
+    int PlayerAScore = 0;
+
+    Nz::Vector2f HOZIRONTAL_BORD_SIZE = Nz::Vector2f(1280.f, 10.f);
+    Nz::Vector2f VERTICAL_BORD_SIZE = Nz::Vector2f(10.f, 720.f);
+
+    const Nz::Vector2f m_windowSize;
+
+    Nz::EnttWorld& m_world;
+};
---
+++ examples/Pong/xmake.lua
@@ -0,0 +1,8 @@
+set_languages("c++20")
+
+target("Pong")
+	add_deps("NazaraGraphics")
+	add_packages("entt")
+	add_files("**.cpp")
+    add_headerfiles("**.hpp")
+	add_defines("NAZARA_ENTT")
