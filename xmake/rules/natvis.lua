da49b3946501a71f1c980916782b92dfb3dafaf9
SirLynix <lynix680@gmail.com>
2023-11-17 13:16:05 +0100

    XMake: Improve natvis rules

--- xmake/rules/natvis.lua
+++ xmake/rules/natvis.lua
@@ -1,12 +1,12 @@
 rule("natvis")
     on_config(function (target)
-        for name, pkg in pairs(target:pkgs()) do
+        for name, pkg in table.orderpairs(target:pkgs()) do
             local includedir = path.join(pkg:installdir(), "include")
             local natvis = os.files(path.join(includedir, "**.natvis"))
             if #natvis > 0 then
                 local groups = table.wrap(target:get("filegroups"))
                 if not table.find(groups, name) then
-                    target:add("headerfiles", natvis, { install = false })
+                    target:add("extrafiles", natvis)
                     target:add("filegroups", path.join("nativs", name), { rootdir = includedir })
                 end
             end
