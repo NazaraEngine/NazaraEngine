NazaraEngine/NazaraEngine

commit 9fb308a531704f25115ab7fdb24f1414345f8e64

Graphics/FramePipelinePass: Replace pointer + size by span

src/Nazara/Graphics/PipelinePassList.cpp
@@ -83,10 +83,8 @@ namespace Nz
 			FramePipelinePass::PassInputOuputs passInputOuputs;
 			passInputOuputs.depthStencilInput = GetAttachmentIndex(passData.depthStencilInput);
 			passInputOuputs.depthStencilOutput = GetAttachmentIndex(passData.depthStencilOutput);
-			passInputOuputs.inputAttachments = inputs.data();
-			passInputOuputs.inputCount = passData.inputs.size();
-			passInputOuputs.outputAttachments = outputs.data();
-			passInputOuputs.outputCount = passData.outputs.size();
+			passInputOuputs.inputAttachments = inputs;
+			passInputOuputs.outputAttachments = outputs;
 
 			FramePass& framePass = passes[passIndex]->RegisterToFrameGraph(frameGraph, passInputOuputs);
 			if (passCallback)

include/Nazara/Graphics/FramePipelinePass.hpp
@@ -14,6 +14,7 @@
 #include <Nazara/Math/Rect.hpp>
 #include <NazaraUtils/Bitset.hpp>
 #include <limits>
+#include <span>
 
 namespace Nz
 {
@@ -74,13 +75,10 @@ namespace Nz
 
 			struct PassInputOuputs
 			{
-				// TODO: Add Nz::View / Nz::Span
-				const std::size_t* inputAttachments;
-				const std::size_t* outputAttachments;
+				std::span<const std::size_t> inputAttachments;
+				std::span<const std::size_t> outputAttachments;
 				std::size_t depthStencilInput = InvalidAttachmentIndex;
 				std::size_t depthStencilOutput = InvalidAttachmentIndex;
-				std::size_t inputCount = 0;
-				std::size_t outputCount = 0;
 			};
 
 			struct VisibleRenderable

src/Nazara/Graphics/DebugDrawPipelinePass.cpp
@@ -21,10 +21,10 @@ namespace Nz
 
 	FramePass& DebugDrawPipelinePass::RegisterToFrameGraph(FrameGraph& frameGraph, const PassInputOuputs& inputOuputs)
 	{
-		if (inputOuputs.inputCount != 1)
+		if (inputOuputs.inputAttachments.size() != 1)
 			throw std::runtime_error("one input expected");
 
-		if (inputOuputs.outputCount != 1)
+		if (inputOuputs.outputAttachments.size() != 1)
 			throw std::runtime_error("one output expected");
 
 		FramePass& debugDrawPass = frameGraph.AddPass("Debug draw pass");

src/Nazara/Graphics/DepthPipelinePass.cpp
@@ -119,10 +119,10 @@ namespace Nz
 
 	FramePass& DepthPipelinePass::RegisterToFrameGraph(FrameGraph& frameGraph, const PassInputOuputs& inputOuputs)
 	{
-		if (inputOuputs.inputCount > 0)
+		if (inputOuputs.inputAttachments.size() > 0)
 			throw std::runtime_error("no input expected");
 
-		if (inputOuputs.outputCount > 0)
+		if (inputOuputs.outputAttachments.size() > 0)
 			throw std::runtime_error("no output expected");
 
 		if (inputOuputs.depthStencilInput != InvalidAttachmentIndex)

src/Nazara/Graphics/ForwardPipelinePass.cpp
@@ -146,10 +146,10 @@ namespace Nz
 
 	FramePass& ForwardPipelinePass::RegisterToFrameGraph(FrameGraph& frameGraph, const PassInputOuputs& inputOuputs)
 	{
-		if (inputOuputs.inputCount > 0)
+		if (inputOuputs.inputAttachments.size() > 0)
 			throw std::runtime_error("no input expected");
 
-		if (inputOuputs.outputCount != 1)
+		if (inputOuputs.outputAttachments.size() != 1)
 			throw std::runtime_error("one output expected");
 
 		if (inputOuputs.depthStencilOutput == InvalidAttachmentIndex)

src/Nazara/Graphics/PostProcessPipelinePass.cpp
@@ -52,10 +52,10 @@ namespace Nz
 
 	FramePass& PostProcessPipelinePass::RegisterToFrameGraph(FrameGraph& frameGraph, const PassInputOuputs& inputOuputs)
 	{
-		if (inputOuputs.inputCount != 1)
+		if (inputOuputs.inputAttachments.size() != 1)
 			throw std::runtime_error("one input expected");
 
-		if (inputOuputs.outputCount != 1)
+		if (inputOuputs.outputAttachments.size() != 1)
 			throw std::runtime_error("one output expected");
 
 		if (inputOuputs.depthStencilInput != InvalidAttachmentIndex)

commit 0d3a9547fcad7e2509b6fb4f5335ce303dbd7edb

Graphics/PipelinePassList: Fix error introduced by 9fb308a531704f25115ab7fdb24f1414345f8e64

src/Nazara/Graphics/PipelinePassList.cpp
@@ -83,8 +83,8 @@ namespace Nz
 			FramePipelinePass::PassInputOuputs passInputOuputs;
 			passInputOuputs.depthStencilInput = GetAttachmentIndex(passData.depthStencilInput);
 			passInputOuputs.depthStencilOutput = GetAttachmentIndex(passData.depthStencilOutput);
-			passInputOuputs.inputAttachments = inputs;
-			passInputOuputs.outputAttachments = outputs;
+			passInputOuputs.inputAttachments = std::span(inputs.data(), passData.inputs.size());
+			passInputOuputs.outputAttachments = std::span(outputs.data(), passData.outputs.size());
 
 			FramePass& framePass = passes[passIndex]->RegisterToFrameGraph(frameGraph, passInputOuputs);
 			if (passCallback)