350531dcebe752d42ad1c58a52101f9fe124e64f
SirLynix <lynix680@gmail.com>
2024-01-03 23:23:39 +0100

    Update Context.cpp

--- src/Nazara/OpenGLRenderer/Wrapper/Context.cpp
+++ src/Nazara/OpenGLRenderer/Wrapper/Context.cpp
@@ -618,14 +618,14 @@ namespace Nz::GL
 #endif
 				glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 
-			// Explicit casting is required for MinGW 32bits to add calling convention
-			GLDEBUGPROC callback = [](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+			glDebugMessageCallback([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+#ifdef NAZARA_COMPILER_MINGW
+				GL_APIENTRY // required on MinGW 32bits
+#endif
 			{
 				const Context* context = static_cast<const Context*>(userParam);
 				context->HandleDebugMessage(source, type, id, severity, length, message);
-			};
-
-			glDebugMessageCallback(callback, this);
+			}, this);
 
 			if (glDebugMessageControl)
 			{
