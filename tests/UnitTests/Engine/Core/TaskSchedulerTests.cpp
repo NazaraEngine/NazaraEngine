adc6a5c0a59b66b3ed56b74af70361e04d821909
SirLynix <lynix680@gmail.com>
2024-02-02 16:20:51 +0100

    Tests: Add task scheduler tests

---
+++ tests/UnitTests/Engine/Core/TaskSchedulerTests.cpp
@@ -0,0 +1,48 @@
+#include <Nazara/Core/TaskScheduler.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <atomic>
+
+SCENARIO("TaskScheduler", "[CORE][TaskScheduler]")
+{
+	for (std::size_t workerCount : { 0, 1, 2, 4 })
+	{
+		GIVEN("A task scheduler with " << workerCount << " workers")
+		{
+			Nz::TaskScheduler scheduler(4);
+
+			WHEN("We add a single task and wait for it")
+			{
+				bool executed = false;
+				scheduler.AddTask([&] { executed = true; });
+				scheduler.WaitForTasks();
+
+				CHECK(executed);
+			}
+
+			WHEN("We add a lot of tasks and wait for all of them")
+			{
+				constexpr std::size_t taskCount = 512;
+
+				std::vector<Nz::UInt8> completionBuffer(taskCount, 0);
+				std::atomic_uint count = 0;
+				for (std::size_t i = 0; i < taskCount; ++i)
+				{
+					scheduler.AddTask([&, i]
+					{
+						completionBuffer[i]++;
+						count++;
+					});
+				}
+				scheduler.WaitForTasks();
+
+				CHECK(count == taskCount);
+
+				for (std::size_t i = 0; i < taskCount; ++i)
+				{
+					INFO("checking that task " << i << " was executed once");
+					CHECK(completionBuffer[i] == 1);
+				}
+			}
+		}
+	}
+}
