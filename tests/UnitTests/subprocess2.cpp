c6f87f9cd2cfd2931445de531264fbff02fb4a9c
SirLynix <lynix680@gmail.com>
2024-02-11 22:06:34 +0100

    UnitTests: Increase process test delay

--- tests/UnitTests/subprocess2.cpp
+++ tests/UnitTests/subprocess2.cpp
@@ -57,7 +57,7 @@ int main(int argc, char* argv[])
 	successFile5.Flush();
 
 	// Wait until parent dies
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::this_thread::sleep_for(std::chrono::seconds(2));
 
 	result = Nz::Process::Exists(pid);
 	if (!result)
