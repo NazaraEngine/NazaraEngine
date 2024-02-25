92d6e3e3b1f7f65dd8a7fab0408fa5d33af410c4
SirLynix <lynix680@gmail.com>
2024-02-25 21:04:50 +0100

    Core: Add TaskSchedulerAppComponent

---
+++ include/Nazara/Core/TaskSchedulerAppComponent.hpp
@@ -0,0 +1,42 @@
+// Copyright (C) 2024 Jérôme "SirLynix" Leclercq (lynix680@gmail.com)
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_CORE_TASKSCHEDULERAPPCOMPONENT_HPP
+#define NAZARA_CORE_TASKSCHEDULERAPPCOMPONENT_HPP
+
+#include <NazaraUtils/Prerequisites.hpp>
+#include <Nazara/Core/ApplicationComponent.hpp>
+#include <Nazara/Core/TaskScheduler.hpp>
+
+namespace Nz
+{
+	class TaskSchedulerAppComponent : public ApplicationComponent
+	{
+		public:
+			inline TaskSchedulerAppComponent(ApplicationBase& app, unsigned int workerCount = 0);
+			TaskSchedulerAppComponent(const TaskSchedulerAppComponent&) = delete;
+			TaskSchedulerAppComponent(TaskSchedulerAppComponent&&) = delete;
+			~TaskSchedulerAppComponent() = default;
+
+			inline void AddTask(TaskScheduler::Task&& task);
+
+			inline TaskScheduler& GetScheduler();
+			inline const TaskScheduler& GetScheduler() const;
+			inline unsigned int GetWorkerCount() const;
+
+			inline void WaitForTasks();
+
+			TaskSchedulerAppComponent& operator=(const TaskSchedulerAppComponent&) = delete;
+			TaskSchedulerAppComponent& operator=(TaskSchedulerAppComponent&&) = delete;
+
+		private:
+			TaskScheduler m_scheduler;
+	};
+}
+
+#include <Nazara/Core/TaskSchedulerAppComponent.inl>
+
+#endif // NAZARA_CORE_TASKSCHEDULERAPPCOMPONENT_HPP
---
+++ include/Nazara/Core/TaskSchedulerAppComponent.inl
@@ -0,0 +1,38 @@
+// Copyright (C) 2024 Jérôme "SirLynix" Leclercq (lynix680@gmail.com)
+// This file is part of the "Nazara Engine - Core module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+namespace Nz
+{
+	inline TaskSchedulerAppComponent::TaskSchedulerAppComponent(ApplicationBase& app, unsigned int workerCount) :
+	ApplicationComponent(app),
+	m_scheduler(workerCount)
+	{
+	}
+
+	inline void TaskSchedulerAppComponent::AddTask(TaskScheduler::Task&& task)
+	{
+		return m_scheduler.AddTask(std::move(task));
+	}
+
+	inline TaskScheduler& TaskSchedulerAppComponent::GetScheduler()
+	{
+		return m_scheduler;
+	}
+
+	inline const TaskScheduler& TaskSchedulerAppComponent::GetScheduler() const
+	{
+		return m_scheduler;
+	}
+
+	inline unsigned int TaskSchedulerAppComponent::GetWorkerCount() const
+	{
+		return m_scheduler.GetWorkerCount();
+	}
+
+	inline void TaskSchedulerAppComponent::WaitForTasks()
+	{
+		return m_scheduler.WaitForTasks();
+	}
+}
+
