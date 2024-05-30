f18435f4c340f8165b470565d3757f54fe7fe896
SirLynix <lynix680@gmail.com>
2024-05-30 09:33:34 +0200

    Add MoltenVK dependency on macOS for examples and tests

--- examples/xmake.lua
+++ examples/xmake.lua
@@ -1,6 +1,11 @@
 option("examples", { description = "Build examples", default = true })
 
 if has_config("examples") then
+	if is_plat("macosx") then
+		add_requires("moltenvk", { configs = { shared = true }})
+		add_packages("moltenvk", { links = {} })
+	end
+
 	set_group("Examples")
 	includes("*/xmake.lua")
 end
--- tests/xmake.lua
+++ tests/xmake.lua
@@ -1,7 +1,11 @@
 option("tests", { description = "Build tests", default = true })
 
 if has_config("tests") then
-	set_group("Tests")
+	if is_plat("macosx") then
+		add_requires("moltenvk", { configs = { shared = true }})
+		add_packages("moltenvk", { links = {} })
+	end
 
+	set_group("Tests")
 	includes("*/xmake.lua")
 end
