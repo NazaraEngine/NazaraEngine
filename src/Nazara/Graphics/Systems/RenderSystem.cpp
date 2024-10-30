98e21d8154bcf804a0ca95dfd0d6b4b252164212
SirLynix <lynix680@gmail.com>
2024-10-30 17:49:44 +0100

    Graphics/RenderSystem: Fix crash on destruction

--- src/Nazara/Graphics/Systems/RenderSystem.cpp
+++ src/Nazara/Graphics/Systems/RenderSystem.cpp
@@ -46,6 +46,8 @@ namespace Nz
 		m_cameraConstructObserver.disconnect();
 		m_graphicsConstructObserver.disconnect();
 		m_lightConstructObserver.disconnect();
+		// Destroy swapchains before pipeline (to free release queues which may point to resources owned by the pipeline)
+		m_windowSwapchains.clear();
 		m_pipeline.reset();
 	}
 
