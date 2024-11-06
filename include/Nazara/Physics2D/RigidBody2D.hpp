NazaraEngine/NazaraEngine

commit 298799c1b669e2ca3d10396c3ba971840ad1731b

Physics*D/RigidBody*D: Rename geom to collider

include/Nazara/Physics2D/RigidBody2D.hpp
@@ -55,9 +55,9 @@ namespace Nz
 			Rectf GetAABB() const;
 			RadianAnglef GetAngularVelocity() const;
 			inline UInt32 GetBodyIndex() const;
+			inline const std::shared_ptr<Collider2D>& GetCollider() const;
 			float GetElasticity(std::size_t shapeIndex = 0) const;
 			float GetFriction(std::size_t shapeIndex = 0) const;
-			inline const std::shared_ptr<Collider2D>& GetGeom() const;
 			inline cpBody* GetHandle() const;
 			inline float GetMass() const;
 			Vector2f GetMassCenter(CoordSys coordSys = CoordSys::Local) const;
@@ -84,7 +84,7 @@ namespace Nz
 			void SetElasticity(std::size_t shapeIndex, float elasticity);
 			void SetFriction(float friction);
 			void SetFriction(std::size_t shapeIndex, float friction);
-			void SetGeom(std::shared_ptr<Collider2D> geom, bool recomputeMoment = true, bool recomputeMassCenter = true);
+			void SetCollider(std::shared_ptr<Collider2D> collider, bool recomputeMoment = true, bool recomputeMassCenter = true);
 			void SetMass(float mass, bool recomputeMoment = true);
 			void SetMassCenter(const Vector2f& center, CoordSys coordSys = CoordSys::Local);
 			void SetMomentOfInertia(float moment);
@@ -116,7 +116,7 @@ namespace Nz
 
 			struct CommonSettings
 			{
-				std::shared_ptr<Collider2D> geom;
+				std::shared_ptr<Collider2D> collider;
 				RadianAnglef rotation = RadianAnglef::Zero();
 				Vector2f position = Vector2f::Zero();
 				bool initiallySleeping = false;
@@ -129,7 +129,7 @@ namespace Nz
 				DynamicSettings(std::shared_ptr<Collider2D> collider, float mass_) :
 				mass(mass_)
 				{
-					geom = std::move(collider);
+					collider = std::move(collider);
 				}
 
 				RadianAnglef angularVelocity = RadianAnglef::Zero();
@@ -143,7 +143,7 @@ namespace Nz
 				StaticSettings() = default;
 				StaticSettings(std::shared_ptr<Collider2D> collider)
 				{
-					geom = std::move(collider);
+					collider = std::move(collider);
 				}
 			};
 
@@ -162,7 +162,7 @@ namespace Nz
 			static void CopyShapeData(cpShape* from, cpShape* to);
 
 			std::vector<cpShape*> m_shapes;
-			std::shared_ptr<Collider2D> m_geom;
+			std::shared_ptr<Collider2D> m_collider;
 			MovablePtr<cpBody> m_handle;
 			MovablePtr<PhysWorld2D> m_world;
 			UInt32 m_bodyIndex;

include/Nazara/Physics3D/RigidBody3D.hpp
@@ -58,7 +58,7 @@ namespace Nz
 			inline JPH::Body* GetBody();
 			inline const JPH::Body* GetBody() const;
 			UInt32 GetBodyIndex() const override;
-			inline const std::shared_ptr<Collider3D>& GetGeom() const;
+			inline const std::shared_ptr<Collider3D>& GetCollider() const;
 			std::pair<Vector3f, Vector3f> GetLinearAndAngularVelocity() const;
 			float GetLinearDamping() const;
 			Vector3f GetLinearVelocity() const;
@@ -79,7 +79,7 @@ namespace Nz
 
 			void SetAngularDamping(float angularDamping);
 			void SetAngularVelocity(const Vector3f& angularVelocity);
-			void SetGeom(std::shared_ptr<Collider3D> geom, bool recomputeInertia = true);
+			void SetCollider(std::shared_ptr<Collider3D> collider, bool recomputeInertia = true);
 			void SetLinearAndAngularVelocity(const Vector3f& linearVelocity, const Vector3f& angularVelocity);
 			void SetLinearDamping(float damping);
 			void SetLinearVelocity(const Vector3f& velocity);
@@ -102,7 +102,7 @@ namespace Nz
 
 			struct CommonSettings
 			{
-				std::shared_ptr<Collider3D> geom;
+				std::shared_ptr<Collider3D> collider;
 				PhysObjectLayer3D objectLayer = 0;
 				Quaternionf rotation = Quaternionf::Identity();
 				Vector3f position = Vector3f::Zero();
@@ -117,7 +117,7 @@ namespace Nz
 				DynamicSettings(std::shared_ptr<Collider3D> collider, float mass_) :
 				mass(mass_)
 				{
-					geom = std::move(collider);
+					collider = std::move(collider);
 				}
 
 				// Default values from Jolt
@@ -140,7 +140,7 @@ namespace Nz
 				StaticSettings() = default;
 				StaticSettings(std::shared_ptr<Collider3D> collider)
 				{
-					geom = std::move(collider);
+					collider = std::move(collider);
 				}
 			};
 
@@ -157,9 +157,7 @@ namespace Nz
 
 			bool ShouldActivate() const;
 
-			static const JPH::Shape* GetEmptyShape();
-
-			std::shared_ptr<Collider3D> m_geom;
+			std::shared_ptr<Collider3D> m_collider;
 			MovablePtr<JPH::Body> m_body;
 			MovablePtr<PhysWorld3D> m_world;
 			UInt32 m_bodyIndex;

src/Nazara/Physics2D/RigidBody2D.cpp
@@ -14,7 +14,7 @@
 namespace Nz
 {
 	RigidBody2D::RigidBody2D(const RigidBody2D& object) :
-	m_geom(object.m_geom),
+	m_collider(object.m_collider),
 	m_world(object.m_world),
 	m_positionOffset(object.m_positionOffset),
 	m_isRegistered(false),
@@ -24,13 +24,13 @@ namespace Nz
 	m_mass(object.GetMass())
 	{
 		NazaraAssert(m_world, "Invalid world");
-		NazaraAssert(m_geom, "Invalid geometry");
+		NazaraAssert(m_collider, "Invalid geometry");
 
 		m_bodyIndex = m_world->RegisterBody(*this);
 		m_handle = cpBodyNew(m_mass, object.GetMomentOfInertia());
 		cpBodySetUserData(m_handle, this);
 
-		SetGeom(object.GetGeom(), false, false);
+		SetCollider(object.GetCollider(), false, false);
 		SetVelocityFunction(object.m_velocityFunc);
 
 		CopyBodyData(object.GetHandle(), m_handle);
@@ -44,7 +44,7 @@ namespace Nz
 
 	RigidBody2D::RigidBody2D(RigidBody2D&& object) noexcept :
 	m_shapes(std::move(object.m_shapes)),
-	m_geom(std::move(object.m_geom)),
+	m_collider(std::move(object.m_collider)),
 	m_handle(object.m_handle),
 	m_world(object.m_world),
 	m_bodyIndex(object.m_bodyIndex),
@@ -286,11 +286,11 @@ namespace Nz
 		cpShapeSetFriction(m_shapes[shapeIndex], cpFloat(friction));
 	}
 
-	void RigidBody2D::SetGeom(std::shared_ptr<Collider2D> geom, bool recomputeMoment, bool recomputeMassCenter)
+	void RigidBody2D::SetCollider(std::shared_ptr<Collider2D> collider, bool recomputeMoment, bool recomputeMassCenter)
 	{
-		// We have no public way of getting rid of an existing geom without removing the whole body
+		// We have no public way of getting rid of an existing collider without removing the whole body
 		// So let's save some attributes of the body, destroy it and rebuild it
-		if (m_geom)
+		if (m_collider)
 		{
 			cpFloat mass = cpBodyGetMass(m_handle);
 			cpFloat moment = cpBodyGetMoment(m_handle);
@@ -311,12 +311,12 @@ namespace Nz
 			m_handle = newHandle;
 		}
 
-		if (geom)
-			m_geom = std::move(geom);
+		if (collider)
+			m_collider = std::move(collider);
 		else
-			m_geom = std::make_shared<NullCollider2D>();
+			m_collider = std::make_shared<NullCollider2D>();
 
-		m_geom->GenerateShapes(m_handle, &m_shapes);
+		m_collider->GenerateShapes(m_handle, &m_shapes);
 
 		for (cpShape* shape : m_shapes)
 			cpShapeSetUserData(shape, this);
@@ -327,11 +327,11 @@ namespace Nz
 		if (recomputeMoment)
 		{
 			if (!IsStatic() && !IsKinematic())
-				cpBodySetMoment(m_handle, m_geom->ComputeMomentOfInertia(m_mass));
+				cpBodySetMoment(m_handle, m_collider->ComputeMomentOfInertia(m_mass));
 		}
 
 		if (recomputeMassCenter)
-			SetMassCenter(m_geom->ComputeCenterOfMass());
+			SetMassCenter(m_collider->ComputeCenterOfMass());
 	}
 
 	void RigidBody2D::SetMass(float mass, bool recomputeMoment)
@@ -345,7 +345,7 @@ namespace Nz
 					cpBodySetMass(body->GetHandle(), mass);
 
 					if (recomputeMoment)
-						cpBodySetMoment(body->GetHandle(), body->GetGeom()->ComputeMomentOfInertia(mass));
+						cpBodySetMoment(body->GetHandle(), body->GetCollider()->ComputeMomentOfInertia(mass));
 				});
 			}
 			else
@@ -361,7 +361,7 @@ namespace Nz
 					cpBodySetMass(body->GetHandle(), mass);
 
 					if (recomputeMoment)
-						cpBodySetMoment(body->GetHandle(), body->GetGeom()->ComputeMomentOfInertia(mass));
+						cpBodySetMoment(body->GetHandle(), body->GetCollider()->ComputeMomentOfInertia(mass));
 				}
 			});
 		}
@@ -545,7 +545,7 @@ namespace Nz
 		m_isRegistered        = object.m_isRegistered;
 		m_isSimulationEnabled = object.m_isSimulationEnabled;
 		m_isStatic            = object.m_isStatic;
-		m_geom                = std::move(object.m_geom);
+		m_collider                = std::move(object.m_collider);
 		m_gravityFactor       = object.m_gravityFactor;
 		m_mass                = object.m_mass;
 		m_positionOffset      = object.m_positionOffset;
@@ -583,7 +583,7 @@ namespace Nz
 		m_handle = (m_mass > 0.f) ? cpBodyNew(m_mass, 0.f) : cpBodyNewKinematic(); // moment will be recomputed by SetGeom
 		cpBodySetUserData(m_handle, this);
 
-		SetGeom(settings.geom);
+		SetCollider(settings.collider);
 		SetAngularVelocity(settings.angularVelocity);
 		SetPosition(settings.position);
 		SetRotation(settings.rotation);
@@ -605,7 +605,7 @@ namespace Nz
 		m_handle = cpBodyNewStatic();
 		cpBodySetUserData(m_handle, this);
 
-		SetGeom(settings.geom);
+		SetCollider(settings.collider);
 		SetPosition(settings.position);
 		SetRotation(settings.rotation);
 	}

examples/Physics2DDemo/main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char* argv[])
 
 	Nz::RigidBody2DComponent::DynamicSettings boxSettings;
 	boxSettings.mass = 50.f;
-	boxSettings.geom = std::make_shared<Nz::BoxCollider2D>(Nz::Vector2f(32.f, 32.f));
+	boxSettings.collider = std::make_shared<Nz::BoxCollider2D>(Nz::Vector2f(32.f, 32.f));
 
 	std::shared_ptr<Nz::Sprite> boxSprite = std::make_shared<Nz::Sprite>(spriteMaterial);
 	boxSprite->SetSize({ 32.f, 32.f });
@@ -98,7 +98,7 @@ int main(int argc, char* argv[])
 		}
 
 		Nz::RigidBody2DComponent::StaticSettings groundSettings;
-		groundSettings.geom = std::make_shared<Nz::BoxCollider2D>(tilemap->GetSize());
+		groundSettings.collider = std::make_shared<Nz::BoxCollider2D>(tilemap->GetSize());
 
 		groundEntity.emplace<Nz::NodeComponent>().SetPosition({ windowSize.x * 0.5f, -windowSize.y * 0.2f });
 		groundEntity.emplace<Nz::GraphicsComponent>().AttachRenderable(tilemap, 1);

examples/PhysicsDemo/main.cpp
@@ -136,7 +136,7 @@ int main(int argc, char* argv[])
 		entityNode.SetPosition(Nz::Vector3f(12.5f, 0.f, 25.f));
 
 		Nz::RigidBody3D::DynamicSettings settings;
-		settings.geom = shipCollider;
+		settings.collider = shipCollider;
 		settings.mass = 100.f;
 
 		auto& entityPhys = playerEntity.emplace<Nz::RigidBody3DComponent>(settings);
@@ -179,7 +179,7 @@ int main(int argc, char* argv[])
 				entityNode.SetRotation(Nz::EulerAnglesf(0.f, Nz::TurnAnglef(0.5f), 0.f));
 
 				Nz::RigidBody3D::DynamicSettings settings;
-				settings.geom = shipCollider;
+				settings.collider = shipCollider;
 				settings.mass = 100.f;
 
 				auto& entityPhys = entity.emplace<Nz::RigidBody3DComponent>(settings);
@@ -239,7 +239,7 @@ int main(int argc, char* argv[])
 			entity.emplace<Nz::NodeComponent>();
 
 			Nz::RigidBody3D::DynamicSettings settings;
-			settings.geom = shipCollider;
+			settings.collider = shipCollider;
 			settings.mass = 100.f;
 
 			auto& entityPhys = entity.emplace<Nz::RigidBody3DComponent>(settings);

examples/PhysicsPlayground/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char* argv[])
 		std::shared_ptr<Nz::CompoundCollider3D> boxCollider = std::make_shared<Nz::CompoundCollider3D>(std::move(colliders));
 
 		Nz::RigidBody3D::StaticSettings settings;
-		settings.geom = boxCollider;
+		settings.collider = boxCollider;
 
 		boxColliderEntity.emplace<Nz::RigidBody3DComponent>(settings);
 
@@ -139,7 +139,7 @@ int main(int argc, char* argv[])
 		ballNode.SetScale(radius);
 
 		Nz::RigidBody3D::DynamicSettings settings;
-		settings.geom = sphereCollider;
+		settings.collider = sphereCollider;
 		settings.mass = 4.f / 3.f * Nz::Pi<float>() * Nz::IntegralPow(radius, 3);
 
 		ballEntity.emplace<Nz::RigidBody3DComponent>(settings);
@@ -176,7 +176,7 @@ int main(int argc, char* argv[])
 		std::shared_ptr<Nz::BoxCollider3D> boxCollider = std::make_shared<Nz::BoxCollider3D>(Nz::Vector3f(width, height, depth));
 
 		Nz::RigidBody3D::DynamicSettings settings;
-		settings.geom = boxCollider;
+		settings.collider = boxCollider;
 		settings.mass = width * height * depth;
 
 		boxEntity.emplace<Nz::RigidBody3DComponent>(settings);
@@ -243,7 +243,7 @@ int main(int argc, char* argv[])
 			shipNode.SetPosition({ xRandom(rd), yRandom(rd), zRandom(rd) });
 
 			Nz::RigidBody3D::DynamicSettings settings;
-			settings.geom = shipCollider;
+			settings.collider = shipCollider;
 			settings.mass = 100.f;
 
 			shipEntity.emplace<Nz::RigidBody3DComponent>(settings);

examples/Showcase/main.cpp
@@ -362,7 +362,7 @@ int main(int argc, char* argv[])
 		auto translatedFloorCollider = std::make_shared<Nz::TranslatedRotatedCollider3D>(std::move(floorCollider), Nz::Vector3f::Down() * 0.5f);
 
 		Nz::RigidBody3D::StaticSettings floorSettings;
-		floorSettings.geom = translatedFloorCollider;
+		floorSettings.collider = translatedFloorCollider;
 
 		floorEntity.emplace<Nz::RigidBody3DComponent>(floorSettings);
 

include/Nazara/Physics2D/Collider2D.hpp
@@ -127,14 +127,14 @@ namespace Nz
 	class NAZARA_PHYSICS2D_API CompoundCollider2D : public Collider2D
 	{
 		public:
-			CompoundCollider2D(std::vector<std::shared_ptr<Collider2D>> geoms);
+			CompoundCollider2D(std::vector<std::shared_ptr<Collider2D>> colliders);
 
 			Nz::Vector2f ComputeCenterOfMass() const override;
 			float ComputeMomentOfInertia(float mass) const override;
 
 			inline bool DoesOverrideCollisionProperties() const;
 
-			inline const std::vector<std::shared_ptr<Collider2D>>& GetGeoms() const;
+			inline const std::vector<std::shared_ptr<Collider2D>>& GetColliders() const;
 			ColliderType2D GetType() const override;
 
 			inline void OverridesCollisionProperties(bool shouldOverride);
@@ -143,7 +143,7 @@ namespace Nz
 			std::size_t CreateShapes(cpBody* body, std::vector<cpShape*>* shapes) const override;
 			std::size_t GenerateShapes(cpBody* body, std::vector<cpShape*>* shapes) const override;
 
-			std::vector<std::shared_ptr<Collider2D>> m_geoms;
+			std::vector<std::shared_ptr<Collider2D>> m_colliders;
 			bool m_doesOverrideCollisionProperties;
 	};
 

commit 4981d20c05f26fcdcc24f078113b657938cde9d2

Physics*D:/RigidBody*D: Fix bug introduced in 298799c

include/Nazara/Physics2D/RigidBody2D.hpp
@@ -126,10 +126,10 @@ namespace Nz
 			struct DynamicSettings : CommonSettings
 			{
 				DynamicSettings() = default;
-				DynamicSettings(std::shared_ptr<Collider2D> collider, float mass_) :
+				DynamicSettings(std::shared_ptr<Collider2D> collider_, float mass_) :
 				mass(mass_)
 				{
-					collider = std::move(collider);
+					collider = std::move(collider_);
 				}
 
 				RadianAnglef angularVelocity = RadianAnglef::Zero();
@@ -141,9 +141,9 @@ namespace Nz
 			struct StaticSettings : CommonSettings
 			{
 				StaticSettings() = default;
-				StaticSettings(std::shared_ptr<Collider2D> collider)
+				StaticSettings(std::shared_ptr<Collider2D> collider_)
 				{
-					collider = std::move(collider);
+					collider = std::move(collider_);
 				}
 			};
 

include/Nazara/Physics3D/RigidBody3D.hpp
@@ -114,10 +114,10 @@ namespace Nz
 			struct DynamicSettings : CommonSettings
 			{
 				DynamicSettings() = default;
-				DynamicSettings(std::shared_ptr<Collider3D> collider, float mass_) :
+				DynamicSettings(std::shared_ptr<Collider3D> collider_, float mass_) :
 				mass(mass_)
 				{
-					collider = std::move(collider);
+					collider = std::move(collider_);
 				}
 
 				// Default values from Jolt
@@ -138,9 +138,9 @@ namespace Nz
 			struct StaticSettings : CommonSettings
 			{
 				StaticSettings() = default;
-				StaticSettings(std::shared_ptr<Collider3D> collider)
+				StaticSettings(std::shared_ptr<Collider3D> collider_)
 				{
-					collider = std::move(collider);
+					collider = std::move(collider_);
 				}
 			};
 

src/Nazara/Physics2D/RigidBody2D.cpp
@@ -545,7 +545,7 @@ namespace Nz
 		m_isRegistered        = object.m_isRegistered;
 		m_isSimulationEnabled = object.m_isSimulationEnabled;
 		m_isStatic            = object.m_isStatic;
-		m_collider                = std::move(object.m_collider);
+		m_collider            = std::move(object.m_collider);
 		m_gravityFactor       = object.m_gravityFactor;
 		m_mass                = object.m_mass;
 		m_positionOffset      = object.m_positionOffset;