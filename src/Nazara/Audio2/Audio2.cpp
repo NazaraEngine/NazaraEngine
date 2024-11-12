91a2687d9b784f78e6b3d691b0bbffbce4fafbfa
SirLynix <lynix680@gmail.com>
2024-11-12 11:34:12 +0100

    Start working on miniaudio

---
+++ src/Nazara/Audio2/Audio2.cpp
@@ -0,0 +1,188 @@
+// Copyright (C) 2024 Jérôme "SirLynix" Leclercq (lynix680@gmail.com)
+// This file is part of the "Nazara Engine - Audio2 module"
+// For conditions of distribution and use, see copyright notice in Export.hpp
+
+#include <Nazara/Audio2/Audio2.hpp>
+#include <NazaraUtils/CallOnExit.hpp>
+#include <Nazara/Audio2/AudioDeviceInfo.hpp>
+/*#include <Nazara/Audio2/Formats/drmp3Loader.hpp>
+#include <Nazara/Audio2/Formats/drwavLoader.hpp>
+#include <Nazara/Audio2/Formats/libflacLoader.hpp>
+#include <Nazara/Audio2/Formats/libvorbisLoader.hpp>*/
+#include <Nazara/Core/CommandLineParameters.hpp>
+#include <Nazara/Core/EnvironmentVariables.hpp>
+#include <Nazara/Core/Error.hpp>
+#include <Nazara/Core/Format.hpp>
+#include <miniaudio.h>
+#include <cstring>
+#include <stdexcept>
+#include <utility>
+
+namespace Nz
+{
+	/*!
+	* \ingroup audio
+	* \class Nz::Audio2
+	* \brief Audio class that represents the module initializer of Audio2
+	*/
+
+	Audio2::Audio2(Config config) :
+	ModuleBase("Audio2", this)
+	{
+		// Setup miniaudio
+		std::unique_ptr<ma_log> maLog = std::make_unique<ma_log>();
+		if (ma_result result = ma_log_init(nullptr, maLog.get()); result != MA_SUCCESS)
+			throw std::runtime_error(Format("failed to initialize miniaudio logger: {}", ma_result_description(result)));
+
+		CallOnExit uninitLog([&]
+		{
+			ma_log_uninit(maLog.get());
+		});
+
+		ma_log_register_callback(maLog.get(), ma_log_callback_init([](void* /*pUserData*/, ma_uint32 level, const char* pMessage)
+		{
+			std::string_view message(pMessage);
+			if NAZARA_LIKELY(!message.empty() && message.ends_with('\n'))
+				message.remove_suffix(1);
+
+			switch (level)
+			{
+				case MA_LOG_LEVEL_DEBUG:
+					NazaraDebug("[MiniAudio] {}", message);
+					break;
+
+				case MA_LOG_LEVEL_INFO:
+					NazaraNotice("[MiniAudio] {}", message);
+					break;
+
+				case MA_LOG_LEVEL_WARNING:
+					NazaraWarningFmt("[MiniAudio] {}", message);
+					break;
+
+				case MA_LOG_LEVEL_ERROR:
+					NazaraErrorFmt("[MiniAudio] {}", message);
+					break;
+			}
+		}, nullptr));
+
+		ma_context_config contextConfig = ma_context_config_init();
+		contextConfig.pLog = maLog.get();
+
+		ma_backend nullBackend = ma_backend_null;
+
+		std::unique_ptr<ma_context> maContext = std::make_unique<ma_context>();
+		ma_result result = ma_context_init((config.noAudio) ? &nullBackend : nullptr, (config.noAudio) ? 1 : 0, &contextConfig, maContext.get());
+		if (result != MA_SUCCESS)
+			throw std::runtime_error(Format("failed to initialize miniaudio: {}", ma_result_description(result)));
+
+		CallOnExit releaseContext([&] { ma_context_uninit(maContext.get()); });
+
+		if (maContext->backend == ma_backend_null && !config.allowNullDevice)
+			throw std::runtime_error("no audio backend");
+
+		// Loaders
+		/*m_soundBufferLoader.RegisterLoader(Loaders::GetSoundBufferLoader_drmp3());
+		m_soundStreamLoader.RegisterLoader(Loaders::GetSoundStreamLoader_drmp3());
+		m_soundBufferLoader.RegisterLoader(Loaders::GetSoundBufferLoader_drwav());
+		m_soundStreamLoader.RegisterLoader(Loaders::GetSoundStreamLoader_drwav());
+		m_soundBufferLoader.RegisterLoader(Loaders::GetSoundBufferLoader_libflac());
+		m_soundStreamLoader.RegisterLoader(Loaders::GetSoundStreamLoader_libflac());
+		m_soundBufferLoader.RegisterLoader(Loaders::GetSoundBufferLoader_libvorbis());
+		m_soundStreamLoader.RegisterLoader(Loaders::GetSoundStreamLoader_libvorbis());*/
+
+		releaseContext.Reset();
+		m_maContext = maContext.release();
+
+		uninitLog.Reset();
+		m_maLog = maLog.release();
+	}
+
+	Audio2::~Audio2()
+	{
+		if (ma_result result = ma_context_uninit(m_maContext))
+			NazaraWarningFmt("failed to uninit context: {}", ma_result_description(result));
+
+		delete m_maContext;
+
+		ma_log* maLog = static_cast<ma_log*>(m_maLog);
+		ma_log_uninit(maLog);
+		delete maLog;
+	}
+
+#if 0
+	/*!
+	* \brief Gets the default SoundBuffer loader
+	* \return A reference to the default SoundBuffer loader
+	*/
+	SoundBufferLoader& Audio2::GetSoundBufferLoader()
+	{
+		return m_soundBufferLoader;
+	}
+
+	/*!
+	* \brief Gets the default SoundBuffer loader
+	* \return A constant reference to the default SoundBuffer loader
+	*/
+	const SoundBufferLoader& Audio2::GetSoundBufferLoader() const
+	{
+		return m_soundBufferLoader;
+	}
+
+	/*!
+	* \brief Gets the default SoundStream loader
+	* \return A reference to the default SoundStream loader
+	*/
+	SoundStreamLoader& Audio2::GetSoundStreamLoader()
+	{
+		return m_soundStreamLoader;
+	}
+
+	/*!
+	* \brief Gets the default SoundStream loader
+	* \return A constant reference to the default SoundStream loader
+	*/
+	const SoundStreamLoader& Audio2::GetSoundStreamLoader() const
+	{
+		return m_soundStreamLoader;
+	}
+#endif
+
+	std::vector<AudioDeviceInfo> Audio2::QueryDevices() const
+	{
+		std::vector<AudioDeviceInfo> devices;
+	
+		ma_context_enumerate_devices(m_maContext, [](ma_context* /*context*/, ma_device_type deviceType, const ma_device_info* info, void* userdata) -> ma_bool32
+		{
+			std::vector<AudioDeviceInfo>& deviceList = *reinterpret_cast<std::vector<AudioDeviceInfo>*>(userdata);
+
+			static_assert(sizeof(AudioDeviceId) >= sizeof(ma_device_id));
+			static_assert(alignof(AudioDeviceId) >= alignof(ma_device_id));
+			static_assert(std::tuple_size_v<decltype(AudioDeviceInfo::deviceName)> >= MA_MAX_DEVICE_NAME_LENGTH + 1);
+
+			auto& device = deviceList.emplace_back();
+			std::memcpy(&device.deviceId.data[0], &info->id, sizeof(ma_device_id));
+			std::strcpy(&device.deviceName[0], &info->name[0]);
+
+			device.isDefault = info->isDefault == MA_TRUE;
+			if (deviceType == ma_device_type_playback)
+				device.deviceType = AudioDeviceType::Playback;
+			else
+			{
+				assert(deviceType == ma_device_type_capture);
+				device.deviceType = AudioDeviceType::Capture;
+			}
+
+			return MA_TRUE;
+		}, &devices);
+
+		return devices;
+	}
+
+	Audio2* Audio2::s_instance = nullptr;
+
+	void Audio2::Config::Override(const CommandLineParameters& parameters)
+	{
+		if (parameters.HasFlag("no-audio") || TestEnvironmentVariable("NAZARA_NO_AUDIO"))
+			noAudio = true;
+	}
+}
