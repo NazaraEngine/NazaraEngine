209ad69a0c7284963a5178f12f20a3641db098d5
SirLynix <lynix680@gmail.com>
2025-01-03 00:25:34 +0100

    Core/Uuid: Remove FCG copyright
    Full Cycle Games was my employer at the time I made those classes for their game project, unfortunately it doesn't exist anymore today

--- src/Nazara/Core/Uuid.cpp
+++ src/Nazara/Core/Uuid.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Full Cycle Games
+// Copyright (C) 2025 Jérôme "SirLynix" Leclercq (lynix680@gmail.com)
 // This file is part of the "Nazara Engine - Core module"
 // For conditions of distribution and use, see copyright notice in Export.hpp
 
