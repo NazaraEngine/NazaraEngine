4ec23e23e3c834db83845bfa848db06f4bcf1105
SirLynix <lynix680@gmail.com>
2026-02-09 20:35:50 +0100

    Core/DynLib: Better handling for path on Windows

--- src/Nazara/Core/Win32/DynLibImpl.cpp
+++ src/Nazara/Core/Win32/DynLibImpl.cpp
@@ -28,15 +28,35 @@ namespace Nz::PlatformImpl
 
 	bool DynLibImpl::Load(const std::filesystem::path& libraryPath, std::string* errorMessage)
 	{
-		m_handle = LoadLibraryExW(PathToWideTemp(libraryPath).data(), nullptr, (libraryPath.is_absolute()) ? LOAD_WITH_ALTERED_SEARCH_PATH : 0);
+		std::wstring path = libraryPath.wstring();
 
-		if (m_handle)
-			return true;
-		else
+		std::size_t lastDirSep = std::wstring::npos;
+		if (std::size_t sepOffset = path.find_first_of(L"\\/"); sepOffset != std::wstring::npos)
+		{
+			do 
+			{
+				path[sepOffset] = L'\\';
+				lastDirSep = sepOffset;
+			}
+			while ((sepOffset = path.find_first_of(L"\\/", sepOffset + 1)) != std::wstring::npos);
+
+			path[lastDirSep] = L'\0';
+			SetDllDirectoryW(path.data());
+			path[lastDirSep] = L'\\';
+		}
+
+		m_handle = LoadLibraryExW(path.data(), nullptr, (libraryPath.is_absolute()) ? LOAD_WITH_ALTERED_SEARCH_PATH : 0);
+
+		if (lastDirSep != std::wstring::npos)
+			SetDllDirectoryW(nullptr);
+
+		if (!m_handle)
 		{
 			*errorMessage = Error::GetLastSystemError();
 			return false;
 		}
+
+		return true;
 	}
 }
 
