NazaraEngine/NazaraEngine

commit da938651641f7c727ad3787347197d392e515673

Core/Image: Fix HasAlpha reading out of bounds if no alpha is found

src/Nazara/Core/Image.cpp
@@ -960,16 +960,19 @@ namespace Nz
 				return false;
 
 			auto seq = workingBitset.Write(GetConstPixels(), info.bitsPerPixel);
-			do
+			for (;;)
 			{
 				workingBitset &= info.alphaMask;
-				if (workingBitset.Count() != info.alphaMask.Count()) //< Means that at least one bit of the alpha mask of this pixel is disabled
+				workingBitset ^= info.alphaMask;
+				if (workingBitset.TestAny()) //< Means that at least one bit of the alpha mask of this pixel is disabled
 					return true;
+
+				if (--pixelCount > 0)
+					break;
 
 				workingBitset.Clear();
 				seq = workingBitset.Write(seq, info.bitsPerPixel);
 			}
-			while (--pixelCount > 0);
 
 			return false;
 		}

commit 9d62e52f3930dc8e070130fc1dc06242218b15de

Core/Image: Fix HasAlpha, broken in da938651641f7c727ad3787347197d392e515673

src/Nazara/Core/Image.cpp
@@ -967,7 +967,7 @@ namespace Nz
 				if (workingBitset.TestAny()) //< Means that at least one bit of the alpha mask of this pixel is disabled
 					return true;
 
-				if (--pixelCount > 0)
+				if (--pixelCount == 0)
 					break;
 
 				workingBitset.Clear();